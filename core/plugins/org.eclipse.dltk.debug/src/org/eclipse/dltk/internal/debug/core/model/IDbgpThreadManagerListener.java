d68501f61087e12d7ddd125e673a29e5918b41bd
dkovalev <dkovalev>
2007-06-08 13:01:31 +0000

    Debugging actions update, debugging packages refactoring, added ruby search open action

--- core/plugins/org.eclipse.dltk.debug/src/org/eclipse/dltk/internal/debug/core/model/IDbgpThreadManagerListener.java
+++ core/plugins/org.eclipse.dltk.debug/src/org/eclipse/dltk/internal/debug/core/model/IDbgpThreadManagerListener.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2005, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,17 +7,12 @@
  *
  
  *******************************************************************************/
-package org.eclipse.dltk.debug.core.model;
+package org.eclipse.dltk.internal.debug.core.model;
 
-import org.eclipse.debug.core.DebugException;
-import org.eclipse.debug.core.model.IStackFrame;
+import org.eclipse.dltk.debug.core.model.IScriptThread;
 
-public interface IScriptStackFrame extends IStackFrame {
-	String getSourceString();
-	
-	int getLevel();
-		
-	IScriptVariable findVariable(String varName) throws DebugException;
-	
-	IScriptThread getScriptThread();
+public interface IDbgpThreadManagerListener {
+	void threadAccepted(IScriptThread thread, boolean first);
+
+	void allThreadsTerminated();
 }
--- core/plugins/org.eclipse.dltk.debug/src/org/eclipse/dltk/internal/debug/core/model/ScriptModelConstants.java
+++ core/plugins/org.eclipse.dltk.debug/src/org/eclipse/dltk/internal/debug/core/model/ScriptModelConstants.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2005, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,17 +7,13 @@
  *
  
  *******************************************************************************/
-package org.eclipse.dltk.debug.core.model;
+package org.eclipse.dltk.internal.debug.core.model;
 
-import org.eclipse.debug.core.DebugException;
-import org.eclipse.debug.core.model.IStackFrame;
+public class ScriptModelConstants {
+	private ScriptModelConstants() {
 
-public interface IScriptStackFrame extends IStackFrame {
-	String getSourceString();
-	
-	int getLevel();
-		
-	IScriptVariable findVariable(String varName) throws DebugException;
-	
-	IScriptThread getScriptThread();
+	}
+
+	// TODO: should remove this, only used for breakpoints
+	public static final String MODEL_ID = "org.eclipse.dltk.debug.core.model";
 }
