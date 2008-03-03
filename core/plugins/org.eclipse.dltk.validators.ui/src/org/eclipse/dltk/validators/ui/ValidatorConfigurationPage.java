f6235be321724e7799881926018215130fb24a38
asobolev <asobolev>
2008-03-03 13:00:23 +0000

    *fixes + applied patch from Michael Spector <spektom@gmail.com> for basic type hierarchy support.

--- core/plugins/org.eclipse.dltk.validators.ui/src/org/eclipse/dltk/validators/ui/ValidatorConfigurationPage.java
+++ core/plugins/org.eclipse.dltk.validators.ui/src/org/eclipse/dltk/validators/ui/ValidatorConfigurationPage.java
@@ -9,24 +9,50 @@
  *******************************************************************************/
 package org.eclipse.dltk.validators.ui;
 
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.dltk.validators.core.IValidator;
 import org.eclipse.swt.widgets.Composite;
 
 public abstract class ValidatorConfigurationPage {
 	protected IValidator validator;
+	private IStatusHandler statusHandler;
+
+	public interface IStatusHandler {
+		public void updateStatus();
+	}
+
 	public void setValidator(IValidator validator) {
 		this.validator = validator;
 	}
+
 	public IValidator getValidator() {
 		return this.validator;
 	}
+
 	public abstract void applyChanges();
-	public abstract void createControl( Composite parent, int columns );
-	
+
+	public abstract void createControl(Composite parent, int columns);
+
+	public String getErrorMessage() {
+		return null;
+	}
+
 	/**
 	 * Called from new validator dialog, to switch configurations.
 	 */
 	public void dispose() {
-		
+	}
+
+	public IStatus getStatus() {
+		return null;
+	}
+
+	public void setStatusHandler(IStatusHandler handler) {
+		this.statusHandler = handler;
+	}
+	public void updateStatus() {
+		if( this.statusHandler != null ) {
+			this.statusHandler.updateStatus();
+		}
 	}
 }
