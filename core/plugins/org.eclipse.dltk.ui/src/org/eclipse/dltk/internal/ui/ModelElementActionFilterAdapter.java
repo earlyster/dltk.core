76f66d1e3245f9049ce6a5a97c4b697d4df798c1
asobolev <asobolev>
2008-04-02 14:52:29 +0000

    * fixes, some refactoring

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/ModelElementActionFilterAdapter.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/ModelElementActionFilterAdapter.java
@@ -1,46 +1,13 @@
 package org.eclipse.dltk.internal.ui;
 
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.dltk.core.PriorityDLTKExtensionManager;
 import org.eclipse.dltk.core.DLTKCore;
 import org.eclipse.dltk.ui.actions.IActionFilterTester;
 import org.eclipse.ui.IActionFilter;
 
 public class ModelElementActionFilterAdapter implements IActionFilter {
 	private final static String ACTION_FILTER_TESTER = "org.eclipse.dltk.ui.actionFilterTester"; //$NON-NLS-1$
-	private static class IdBasedExtensionManager extends PriorityDLTKExtensionManager {
-		private static final String CLASS_ATTR = "class"; //$NON-NLS-1$
-		public IdBasedExtensionManager(String extension) {
-			super(extension, "id"); //$NON-NLS-1$
-		}
-		public IActionFilterTester getObject(String id) throws CoreException {
-			ElementInfo ext = this.getElementInfo(id);
-
-			return (IActionFilterTester)getInitObject(ext);
-		}
-
-		public Object getInitObject(ElementInfo ext) throws CoreException {
-			if (ext != null) {
-				if (ext.object != null) {
-					return ext.object;
-				}
-
-				IConfigurationElement cfg = (IConfigurationElement) ext.getConfig();
-				Object object = createObject(cfg);
-				ext.object = object;
-				return object;
-			}
-			return null;
-		}
-
-		protected Object createObject(IConfigurationElement cfg) throws CoreException {
-			return cfg.createExecutableExtension(CLASS_ATTR);
-		}
-	}
-	private static IdBasedExtensionManager actionFilterTesters = new IdBasedExtensionManager(ACTION_FILTER_TESTER) {
-		
-	};
+	private static IdBasedExtensionManager actionFilterTesters = new IdBasedExtensionManager(ACTION_FILTER_TESTER);
 	public boolean testAttribute(Object target, String name, String value) {
 		try {
 			IActionFilterTester tester = actionFilterTesters.getObject(name);
