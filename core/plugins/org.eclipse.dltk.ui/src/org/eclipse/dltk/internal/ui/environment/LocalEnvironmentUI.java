92ff363aa4339271fbca9e1184627f4e1c2aede8
asobolev <asobolev>
2008-04-04 10:35:01 +0000

    *fix

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/environment/LocalEnvironmentUI.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/environment/LocalEnvironmentUI.java
@@ -1,5 +1,13 @@
 package org.eclipse.dltk.internal.ui.environment;
 
-public class LocalEnvironmentUI {
+import org.eclipse.dltk.ui.environment.IEnvironmentUI;
+import org.eclipse.swt.widgets.Shell;
+
+public class LocalEnvironmentUI implements IEnvironmentUI {
+
+	public String selectFolder(Shell shell) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
