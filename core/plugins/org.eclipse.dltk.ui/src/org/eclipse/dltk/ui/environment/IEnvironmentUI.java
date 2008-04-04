02113b931d9eb29c148959b65fb3a0722f30aae5
asobolev <asobolev>
2008-04-04 10:35:10 +0000

    *fix

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/environment/IEnvironmentUI.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/environment/IEnvironmentUI.java
@@ -1,5 +1,12 @@
 package org.eclipse.dltk.ui.environment;
 
+import org.eclipse.swt.widgets.Shell;
+
 public interface IEnvironmentUI {
-	
+	/**
+	 * Open directory selection dialog. Dialog allow creation of new
+	 * directories.
+	 */
+	String selectFolder(Shell shell);
+
 }
