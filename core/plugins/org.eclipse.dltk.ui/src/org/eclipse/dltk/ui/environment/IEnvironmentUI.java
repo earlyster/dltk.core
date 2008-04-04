7ef7675e291da9e36f7cb99d4162abf8ea337945
asobolev <asobolev>
2008-04-04 13:21:39 +0000

    * tcl interpreter selection dialog

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/environment/IEnvironmentUI.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/environment/IEnvironmentUI.java
@@ -3,10 +3,18 @@ package org.eclipse.dltk.ui.environment;
 import org.eclipse.swt.widgets.Shell;
 
 public interface IEnvironmentUI {
+	public static final int DEFAULT = 0;
+	public static final int EXECUTABLE = 1;
+
 	/**
 	 * Open directory selection dialog. Dialog allow creation of new
 	 * directories.
 	 */
 	String selectFolder(Shell shell);
+
+	/**
+	 * Open file selection dialog.
+	 */
+	String selectFile(Shell shell, int executable2);
 
 }
