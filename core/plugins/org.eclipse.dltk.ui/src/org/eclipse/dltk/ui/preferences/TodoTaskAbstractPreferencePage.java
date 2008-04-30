2ca457abb5f8e5c9ddb2142505d3705d6b255608
asobolev <asobolev>
2008-04-30 05:57:13 +0000

    * applied patch 185488 with some changes

---
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/preferences/TodoTaskAbstractPreferencePage.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2008 xored software, Inc.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     xored software, Inc. - initial API and Implementation (Alex Panchenko)
+ *******************************************************************************/
+package org.eclipse.dltk.ui.preferences;
+
+import org.eclipse.core.runtime.Preferences;
+import org.eclipse.dltk.ui.PreferencesAdapter;
+
+public abstract class TodoTaskAbstractPreferencePage extends
+		AbstractConfigurationBlockPreferencePage {
+
+	protected abstract Preferences getPluginPreferences();
+
+	protected IPreferenceConfigurationBlock createConfigurationBlock(
+			OverlayPreferenceStore overlayPreferenceStore) {
+		return new TodoTaskConfigurationBlock(getPluginPreferences(),
+				overlayPreferenceStore, this);
+	}
+
+	protected void setPreferenceStore() {
+		setPreferenceStore(new PreferencesAdapter(getPluginPreferences()));
+	}
+
+}
