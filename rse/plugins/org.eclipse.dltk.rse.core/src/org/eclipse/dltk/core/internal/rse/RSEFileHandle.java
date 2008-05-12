a87aa0a1c3fd33ff367d9334031635ef64b12854
asobolev <asobolev>
2008-05-12 11:06:36 +0000

    * some fixes * RSE 3.0M7 compatibility fix

--- rse/plugins/org.eclipse.dltk.rse.core/src/org/eclipse/dltk/core/internal/rse/RSEFileHandle.java
+++ rse/plugins/org.eclipse.dltk.rse.core/src/org/eclipse/dltk/core/internal/rse/RSEFileHandle.java
@@ -23,6 +23,56 @@ import org.eclipse.rse.internal.efs.RSEFileSystem;
 
 public class RSEFileHandle extends EFSFileHandle {
 
+	private final class CountStream extends InputStream {
+		private InputStream stream;
+
+		public CountStream(InputStream stream) {
+			this.stream = stream;
+		}
+
+		public int read() throws IOException {
+			int read = stream.read();
+			if (read != -1) {
+				RSEPerfomanceStatistics
+						.inc(RSEPerfomanceStatistics.TOTAL_BYTES_RECEIVED);
+			}
+			return read;
+		}
+
+		public int available() throws IOException {
+			return this.stream.available();
+		}
+
+		public void close() throws IOException {
+			this.stream.close();
+		}
+
+		public synchronized void mark(int readlimit) {
+			this.stream.mark(readlimit);
+		}
+
+		public boolean markSupported() {
+			return this.stream.markSupported();
+		}
+
+		public int read(byte[] b, int off, int len) throws IOException {
+			return this.stream.read(b, off, len);
+		}
+
+		public int read(byte[] b) throws IOException {
+			return this.stream.read(b);
+		}
+
+		public synchronized void reset() throws IOException {
+			this.stream.reset();
+		}
+
+		public long skip(long n) throws IOException {
+			return this.stream.skip(n);
+		}
+
+	}
+
 	public RSEFileHandle(IEnvironment env, URI locationURI) {
 		super(env, RSEFileSystem.getInstance().getStore(locationURI));
 	}
@@ -35,18 +85,7 @@ public class RSEFileHandle extends EFSFileHandle {
 	public InputStream openInputStream(IProgressMonitor monitor)
 			throws IOException {
 		if (RSEPerfomanceStatistics.PERFOMANCE_TRACING) {
-
-			final InputStream stream = super.openInputStream(monitor);
-			return new InputStream() {
-				public int read() throws IOException {
-					int read = stream.read();
-					if (read != -1) {
-						RSEPerfomanceStatistics
-								.inc(RSEPerfomanceStatistics.TOTAL_BYTES_RECEIVED);
-					}
-					return read;
-				}
-			};
+			return new CountStream(super.openInputStream(monitor));
 		}
 		return super.openInputStream(monitor);
 	}
