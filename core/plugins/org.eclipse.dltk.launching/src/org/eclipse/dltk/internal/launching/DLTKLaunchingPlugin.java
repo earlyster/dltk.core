6f5eaaa4254332946a99ae7db45fdf0d3534dbaf
asobolev <asobolev>
2008-05-29 07:33:55 +0000

    * RSE lazy loading code

--- core/plugins/org.eclipse.dltk.launching/src/org/eclipse/dltk/internal/launching/DLTKLaunchingPlugin.java
+++ core/plugins/org.eclipse.dltk.launching/src/org/eclipse/dltk/internal/launching/DLTKLaunchingPlugin.java
@@ -151,7 +151,7 @@ public class DLTKLaunchingPlugin extends Plugin implements
 	 * 
 	 * @return the Document
 	 * @throws ParserConfigurationException
-	 * 		if an exception occurs creating the document builder
+	 *             if an exception occurs creating the document builder
 	 */
 	public static Document getDocument() throws ParserConfigurationException {
 		DocumentBuilderFactory dfactory = DocumentBuilderFactory.newInstance();
@@ -165,7 +165,7 @@ public class DLTKLaunchingPlugin extends Plugin implements
 	 * platform line separators.
 	 * 
 	 * @param doc
-	 * 		document to serialize
+	 *            document to serialize
 	 * @return the document as a string
 	 */
 	public static String serializeDocument(Document doc) throws IOException,
@@ -189,7 +189,7 @@ public class DLTKLaunchingPlugin extends Plugin implements
 	 * 
 	 * @return an XML parser
 	 * @throws CoreException
-	 * 		if unable to create a parser
+	 *             if unable to create a parser
 	 * 
 	 */
 	public static DocumentBuilder getParser() throws CoreException {
@@ -211,9 +211,9 @@ public class DLTKLaunchingPlugin extends Plugin implements
 	 * Throws an exception with the given message and underlying exception.
 	 * 
 	 * @param message
-	 * 		error message
+	 *            error message
 	 * @param exception
-	 * 		underlying exception or <code>null</code> if none
+	 *            underlying exception or <code>null</code> if none
 	 * @throws CoreException
 	 */
 	protected static void abort(String message, Throwable exception)
@@ -227,10 +227,10 @@ public class DLTKLaunchingPlugin extends Plugin implements
 	 * Returns a new runtime buildpath entry of the specified type.
 	 * 
 	 * @param id
-	 * 		extension type id
+	 *            extension type id
 	 * @return new uninitialized runtime buildpath entry
 	 * @throws CoreException
-	 * 		if unable to create an entry
+	 *             if unable to create an entry
 	 */
 	public IRuntimeBuildpathEntry2 newRuntimeBuildpathEntry(String id)
 			throws CoreException {
@@ -341,8 +341,7 @@ public class DLTKLaunchingPlugin extends Plugin implements
 	 * Save preferences whenever the connect timeout changes. Process changes to
 	 * the list of installed InterpreterEnvironments.
 	 * 
-	 * @see org.eclipse.core.runtime.Preferences.IPropertyChangeListener#
-	 * 	propertyChange(PropertyChangeEvent)
+	 * @see org.eclipse.core.runtime.Preferences.IPropertyChangeListener#propertyChange(PropertyChangeEvent)
 	 */
 	public void propertyChange(PropertyChangeEvent event) {
 		String property = event.getProperty();
@@ -427,7 +426,7 @@ public class DLTKLaunchingPlugin extends Plugin implements
 			}
 
 			// Fire change notification for added and changed Interpreters. The
-			//' convertToRealInterpreter'
+			// ' convertToRealInterpreter'
 			// fires the appropriate notification.
 			Iterator iter = current.iterator();
 			while (iter.hasNext()) {
@@ -436,7 +435,7 @@ public class DLTKLaunchingPlugin extends Plugin implements
 			}
 
 			// set the new default Interpreter installs. This will fire a
-			//' defaultInterpreterChanged',
+			// ' defaultInterpreterChanged',
 			// if it in fact changed
 			String[] newDefaultId = newResults
 					.getDefaultInterpreterInstallCompositeID();
@@ -601,6 +600,7 @@ public class DLTKLaunchingPlugin extends Plugin implements
 
 		// old container ids to new
 		private HashMap fRenamedContainerIds = new HashMap();
+		private Job job;
 
 		public void defaultInterpreterInstallChanged(
 				IInterpreterInstall previous, IInterpreterInstall current) {
@@ -640,9 +640,20 @@ public class DLTKLaunchingPlugin extends Plugin implements
 		 * InterpreterEnvironment changes.
 		 */
 		public void process() throws CoreException {
-			Job job = new InterpreterUpdateJob(this);
-			job.setName("Rebind interpreters");
-			job.schedule();
+			if (job == null) {
+				job = new InterpreterUpdateJob(this);
+				job.setName("Rebind interpreters");
+				job.schedule();
+			} else {
+				try {
+					job.join();
+					job.schedule();
+				} catch (InterruptedException e) {
+					if (DLTKCore.DEBUG) {
+						e.printStackTrace();
+					}
+				}
+			}
 		}
 
 		protected void doit(IProgressMonitor monitor) throws CoreException {
