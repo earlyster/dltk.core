c3f69f5e8186c43c1efc763b23936a97d257874f
apanchenk <apanchenk>
2008-06-06 09:48:46 +0000

    TabStyle is final with private constructor

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/text/util/TabStyle.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/text/util/TabStyle.java
@@ -14,46 +14,42 @@ import java.util.Map;
 
 import org.eclipse.dltk.ui.CodeFormatterConstants;
 
-public abstract class TabStyle {
-	
+public final class TabStyle {
+
 	private final String name;
 
-	public TabStyle(String name) {
+	private TabStyle(String name) {
 		this.name = name;
 	}
-	
+
 	public String toString() {
 		return name;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
-	public static final TabStyle TAB = new TabStyle(CodeFormatterConstants.TAB) {
-		
-	};
-	
-	public static final TabStyle SPACES = new TabStyle(CodeFormatterConstants.SPACE) {
-		
-	};
-	
-	public static final TabStyle MIXED = new TabStyle(CodeFormatterConstants.MIXED) {
-		
-	};
-	
+
+	public static final TabStyle TAB = new TabStyle(CodeFormatterConstants.TAB);
+
+	public static final TabStyle SPACES = new TabStyle(
+			CodeFormatterConstants.SPACE);
+
+	public static final TabStyle MIXED = new TabStyle(
+			CodeFormatterConstants.MIXED);
+
 	private static final Map byName = new HashMap();
-	
+
 	static {
 		byName.put(TAB.getName(), TAB);
 		byName.put(SPACES.getName(), SPACES);
 		byName.put(MIXED.getName(), MIXED);
 	}
-	
+
 	public static TabStyle forName(String name) {
 		return (TabStyle) byName.get(name);
 	}
-	
+
 	public static TabStyle forName(String name, TabStyle deflt) {
 		TabStyle result = forName(name);
 		if (result == null)
@@ -61,5 +57,5 @@ public abstract class TabStyle {
 		else
 			return result;
 	}
-	
+
 }
