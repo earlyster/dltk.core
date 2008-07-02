97a82a9aa5b8b887cf0e7d5f84a3b91c42893955
apanchenk <apanchenk>
2008-07-02 05:40:00 +0000

    NON-NLS comments

--- core/plugins/org.eclipse.dltk.debug/src/org/eclipse/dltk/debug/core/model/IScriptTypeFactory.java
+++ core/plugins/org.eclipse.dltk.debug/src/org/eclipse/dltk/debug/core/model/IScriptTypeFactory.java
@@ -2,9 +2,9 @@ package org.eclipse.dltk.debug.core.model;
 
 public interface IScriptTypeFactory {
 
-	static String STRING = "string";
-	static String ARRAY = "array";
-	static String HASH = "hash";
+	static String STRING = "string"; //$NON-NLS-1$
+	static String ARRAY = "array"; //$NON-NLS-1$
+	static String HASH = "hash"; //$NON-NLS-1$
 
 	IScriptType buildType(String type);
 }
