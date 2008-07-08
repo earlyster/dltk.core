157064fdc5926423a36566a4946dafcffe30a77e
apanchenk <apanchenk>
2008-07-08 05:42:45 +0000

    check that there are validators configured for the matching validator types

--- core/plugins/org.eclipse.dltk.validators.ui/src/org/eclipse/dltk/validators/internal/ui/ValidatorsVisibilityTester.java
+++ core/plugins/org.eclipse.dltk.validators.ui/src/org/eclipse/dltk/validators/internal/ui/ValidatorsVisibilityTester.java
@@ -4,6 +4,7 @@ import org.eclipse.dltk.core.DLTKLanguageManager;
 import org.eclipse.dltk.core.IDLTKLanguageToolkit;
 import org.eclipse.dltk.core.IModelElement;
 import org.eclipse.dltk.ui.actions.IActionFilterTester;
+import org.eclipse.dltk.validators.core.IValidator;
 import org.eclipse.dltk.validators.core.IValidatorType;
 import org.eclipse.dltk.validators.core.ValidatorRuntime;
 
@@ -13,15 +14,22 @@ public class ValidatorsVisibilityTester implements IActionFilterTester {
 	}
 
 	public boolean test(Object target, String name, String value) {
-		if( target instanceof IModelElement ) {
-			IDLTKLanguageToolkit languageToolkit = DLTKLanguageManager.getLanguageToolkit((IModelElement)target);
-			if( languageToolkit != null ) {
-				IValidatorType[] validatorTypes = ValidatorRuntime.getValidatorTypes(languageToolkit.getNatureId());
-				if( validatorTypes != null && validatorTypes.length > 0 ) {
-					return true;
+		if (target instanceof IModelElement) {
+			final IDLTKLanguageToolkit toolkit = DLTKLanguageManager
+					.getLanguageToolkit((IModelElement) target);
+			if (toolkit != null) {
+				final IValidatorType[] types = ValidatorRuntime
+						.getValidatorTypes(toolkit.getNatureId());
+				if (types != null && types.length > 0) {
+					for (int i = 0, size = types.length; i < size; ++i) {
+						final IValidator[] validators = types[i]
+								.getValidators();
+						if (validators != null && validators.length != 0) {
+							return true;
+						}
+					}
 				}
 			}
-			return false;
 		}
 		return false;
 	}
