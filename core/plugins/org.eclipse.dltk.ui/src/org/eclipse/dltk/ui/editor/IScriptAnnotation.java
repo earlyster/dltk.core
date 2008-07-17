14fe56e908e1fff2727ea9ff48a73563aa85249d
apanchenk <apanchenk>
2008-07-17 13:10:19 +0000

    formatting

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/editor/IScriptAnnotation.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/editor/IScriptAnnotation.java
@@ -14,12 +14,9 @@ import java.util.Iterator;
 import org.eclipse.dltk.compiler.problem.CategorizedProblem;
 import org.eclipse.dltk.core.ISourceModule;
 
-
-
 /**
- * Interface of annotations representing markers
- * and problems.
- *
+ * Interface of annotations representing markers and problems.
+ * 
  * @see org.eclipse.core.resources.IMarker
  * @see org.eclipse.jdt.core.compiler.IProblem
  */
@@ -47,77 +44,82 @@ public interface IScriptAnnotation {
 
 	/**
 	 * Returns whether this annotation is overlaid.
-	 *
+	 * 
 	 * @return <code>true</code> if overlaid
 	 */
 	boolean hasOverlay();
 
 	/**
 	 * Returns the overlay of this annotation.
-	 *
+	 * 
 	 * @return the annotation's overlay
 	 */
 	IScriptAnnotation getOverlay();
 
 	/**
-	 * Returns an iterator for iterating over the
- 	 * annotation which are overlaid by this annotation.
-	 *
+	 * Returns an iterator for iterating over the annotation which are overlaid
+	 * by this annotation.
+	 * 
 	 * @return an iterator over the overlaid annotations
 	 */
 	Iterator getOverlaidIterator();
 
 	/**
-	 * Adds the given annotation to the list of
-	 * annotations which are overlaid by this annotations.
-	 *
-	 * @param annotation the problem annotation
+	 * Adds the given annotation to the list of annotations which are overlaid
+	 * by this annotations.
+	 * 
+	 * @param annotation
+	 *            the problem annotation
 	 */
 	void addOverlaid(IScriptAnnotation annotation);
 
 	/**
-	 * Removes the given annotation from the list of
-	 * annotations which are overlaid by this annotation.
-	 *
-	 * @param annotation the problem annotation
+	 * Removes the given annotation from the list of annotations which are
+	 * overlaid by this annotation.
+	 * 
+	 * @param annotation
+	 *            the problem annotation
 	 */
 	void removeOverlaid(IScriptAnnotation annotation);
 
 	/**
-	 * Tells whether this annotation is a problem
-	 * annotation.
-	 *
+	 * Tells whether this annotation is a problem annotation.
+	 * 
 	 * @return <code>true</code> if it is a problem annotation
 	 */
 	boolean isProblem();
 
 	/**
-	 * Returns the compilation unit corresponding to the document on which the annotation is set
-	 * or <code>null</code> if no corresponding compilation unit exists.
+	 * Returns the compilation unit corresponding to the document on which the
+	 * annotation is set or <code>null</code> if no corresponding compilation
+	 * unit exists.
 	 */
 	ISourceModule getSourceModule();
 
 	/**
-	 * Returns the problem arguments or <code>null</code> if no problem arguments can be evaluated.
+	 * Returns the problem arguments or <code>null</code> if no problem
+	 * arguments can be evaluated.
 	 * 
 	 * @return returns the problem arguments or <code>null</code> if no problem
-	 *  arguments can be evaluated.
+	 *         arguments can be evaluated.
 	 */
 	String[] getArguments();
 
 	/**
-	 * Returns the problem id or <code>-1</code> if no problem id can be evaluated.
+	 * Returns the problem id or <code>-1</code> if no problem id can be
+	 * evaluated.
 	 * 
 	 * @return returns the problem id or <code>-1</code>
 	 */
 	int getId();
 
 	/**
-	 * Returns the marker type associated to this problem or <code>null<code> if no marker type
+	 * Returns the marker type associated to this problem or
+	 * <code>null<code> if no marker type
 	 * can be evaluated. See also {@link CategorizedProblem#getMarkerType()}.
 	 * 
-	 * @return the type of the marker which would be associated to the problem or
-	 * <code>null<code> if no marker type can be evaluated. 
+	 * @return the type of the marker which would be associated to the problem
+	 *         or <code>null<code> if no marker type can be evaluated.
 	 */
 	String getMarkerType();
 }
