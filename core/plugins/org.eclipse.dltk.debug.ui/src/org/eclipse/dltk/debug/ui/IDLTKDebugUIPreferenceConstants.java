2db29bbd843aab1478d283b0f46347458609e31c
jgangemi <jgangemi>
2008-07-24 13:52:37 +0000

    - d'oh, didn't mean to check in DLTKDebugUIPluginPreferenceInitializer last night and that depends on changes here

--- core/plugins/org.eclipse.dltk.debug.ui/src/org/eclipse/dltk/debug/ui/IDLTKDebugUIPreferenceConstants.java
+++ core/plugins/org.eclipse.dltk.debug.ui/src/org/eclipse/dltk/debug/ui/IDLTKDebugUIPreferenceConstants.java
@@ -11,4 +11,24 @@ public interface IDLTKDebugUIPreferenceConstants {
 			+ ".alert_hot_code_replace_failed"; //$NON-NLS-1$
 	String PREF_ALERT_HCR_NOT_SUPPORTED = DLTKDebugUIPlugin.PLUGIN_ID
 			+ ".alert_hot_code_replace_not_supported"; //$NON-NLS-1$
+
+	/**
+	 * String preference indication when and where variable details should
+	 * appear. Valid values include:
+	 * <ul>
+	 * <li><code>INLINE_ALL</code> to show inline details for all variables
+	 * <li><code>INLINE_FORMATTERS</code> to show inline details for variables
+	 * with formatters
+	 * <li><code>DETAIL_PANE</code> to show details only in the detail pane
+	 * </ul>
+	 */
+	public static final String PREF_SHOW_DETAILS = DLTKDebugUIPlugin.PLUGIN_ID
+			+ ".show_details"; //$NON-NLS-1$
+
+	/**
+	 * "Show detail" preference values.
+	 */
+	public static final String INLINE_ALL = "INLINE_ALL"; //$NON-NLS-1$
+	public static final String INLINE_FORMATTERS = "INLINE_FORMATTERS"; //$NON-NLS-1$
+	public static final String DETAIL_PANE = "DETAIL_PANE"; //$NON-NLS-1$
 }
