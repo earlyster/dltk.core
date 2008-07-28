93b675e144d78e008e05bc33c9fb51eb3692dd20
apanchenk <apanchenk>
2008-07-28 13:33:05 +0000

    added getModelPresentation(IPresentationContext context, String modelId) method for 3.3 compatibility

--- core/plugins/org.eclipse.dltk.debug.ui/src/org/eclipse/dltk/internal/debug/ui/variables/ScriptVariableLabelProvider.java
+++ core/plugins/org.eclipse.dltk.debug.ui/src/org/eclipse/dltk/internal/debug/ui/variables/ScriptVariableLabelProvider.java
@@ -3,8 +3,10 @@ package org.eclipse.dltk.internal.debug.ui.variables;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.model.IValue;
 import org.eclipse.debug.core.model.IVariable;
+import org.eclipse.debug.internal.ui.DelegatingModelPresentation;
 import org.eclipse.debug.internal.ui.model.elements.VariableLabelProvider;
 import org.eclipse.debug.internal.ui.viewers.model.provisional.IPresentationContext;
+import org.eclipse.debug.internal.ui.views.DebugModelPresentationContext;
 import org.eclipse.debug.ui.IDebugModelPresentation;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.util.IPropertyChangeListener;
@@ -23,6 +25,27 @@ public class ScriptVariableLabelProvider extends VariableLabelProvider
 	public void dispose() {
 		store.removePropertyChangeListener(this);
 		store = null;
+	}
+
+	/**
+	 * XXX 3.3 compatibility getModelPresentation(IPresentationContext,String)
+	 * 
+	 * @param context
+	 * @param modelId
+	 * @return
+	 */
+	protected IDebugModelPresentation getModelPresentation(
+			IPresentationContext context, String modelId) {
+		if (context instanceof DebugModelPresentationContext) {
+			DebugModelPresentationContext debugContext = (DebugModelPresentationContext) context;
+			IDebugModelPresentation presentation = debugContext
+					.getModelPresentation();
+			if (presentation instanceof DelegatingModelPresentation) {
+				return ((DelegatingModelPresentation) presentation)
+						.getPresentation(modelId);
+			}
+		}
+		return null;
 	}
 
 	protected String getValueText(IVariable variable, IValue value,
