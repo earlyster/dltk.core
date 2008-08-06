b7ad6a1a9eadf5f654b4d56893d232dc8de72bcc
ssanders <ssanders>
2008-08-06 04:03:34 +0000

    Changed getScriptBuilders() to add generic builders first, because they may be depended on by specific, but not vice-versa.

--- core/plugins/org.eclipse.dltk.core/model/org/eclipse/dltk/internal/core/builder/ScriptBuilderManager.java
+++ core/plugins/org.eclipse.dltk.core/model/org/eclipse/dltk/internal/core/builder/ScriptBuilderManager.java
@@ -43,18 +43,19 @@ public class ScriptBuilderManager {
 		for (int i = 0; i < cfg.length; i++) {
 			String nature = cfg[i].getAttribute(NATURE_ATTR);
 			if (builders.get(nature) != null) {
-				List elements = (List)builders.get(nature);
+				List elements = (List) builders.get(nature);
 				elements.add(cfg[i]);
-			}
-			else {
+			} else {
 				List elements = new ArrayList();
-				elements.add( cfg[i] );
+				elements.add(cfg[i]);
 				builders.put(nature, elements);
 			}
 		}
 	}
+
 	/**
 	 * Return merged with all elements with nature #
+	 * 
 	 * @param natureId
 	 * @return
 	 * @throws CoreException
@@ -64,42 +65,42 @@ public class ScriptBuilderManager {
 		initialize();
 
 		List results = new ArrayList();
-		processNature(natureId, results);
 		// Add from # nature.
-		processNature( "#", results ); //$NON-NLS-1$
-		return (IScriptBuilder[])results.toArray(new IScriptBuilder[results.size()]);
+		processNature("#", results); //$NON-NLS-1$
+		processNature(natureId, results);
+		return (IScriptBuilder[]) results.toArray(new IScriptBuilder[results
+				.size()]);
 	}
+
 	private static void processNature(String natureId, List results)
 			throws CoreException {
 		Object ext = builders.get(natureId);
-		
+
 		if (ext != null) {
-			if( ext instanceof IScriptBuilder[]) {
-				IScriptBuilder[] b = (IScriptBuilder[])ext;
+			if (ext instanceof IScriptBuilder[]) {
+				IScriptBuilder[] b = (IScriptBuilder[]) ext;
 				for (int i = 0; i < b.length; i++) {
-					if( !results.contains(b[i])) {
+					if (!results.contains(b[i])) {
 						results.add(b[i]);
 					}
 				}
-			}
-			else if ( ext instanceof List ) {
-				List elements = (List)ext;
+			} else if (ext instanceof List) {
+				List elements = (List) ext;
 				IScriptBuilder[] result = new IScriptBuilder[elements.size()];
-				for( int i = 0; i < elements.size(); ++i ) {
+				for (int i = 0; i < elements.size(); ++i) {
 					Object e = elements.get(i);
-					if( e instanceof IScriptBuilder ) {
-						result[i] = (IScriptBuilder)e;
-					}
-					else {
+					if (e instanceof IScriptBuilder) {
+						result[i] = (IScriptBuilder) e;
+					} else {
 						IConfigurationElement cfg = (IConfigurationElement) e;
 						IScriptBuilder builder = (IScriptBuilder) cfg
 								.createExecutableExtension("class"); //$NON-NLS-1$
 						result[i] = builder;
 					}
 				}
-				builders.put(natureId, result) ;
+				builders.put(natureId, result);
 				for (int i = 0; i < result.length; i++) {
-					if( !results.contains(result[i])) {
+					if (!results.contains(result[i])) {
 						results.add(result[i]);
 					}
 				}
@@ -108,17 +109,18 @@ public class ScriptBuilderManager {
 	}
 
 	public static IScriptBuilder[] getAllScriptBuilders() throws CoreException {
-		
+
 		initialize();
 		List result = new ArrayList();
 		Iterator iterator = builders.keySet().iterator();
-		while( iterator.hasNext() ) {
-			String nature = (String)iterator.next();
+		while (iterator.hasNext()) {
+			String nature = (String) iterator.next();
 			IScriptBuilder[] b = getScriptBuilders(nature);
-			for( int i = 0; i < b.length; ++i ) {
+			for (int i = 0; i < b.length; ++i) {
 				result.add(b[i]);
 			}
 		}
-		return (IScriptBuilder[])result.toArray(new IScriptBuilder[result.size()]);
+		return (IScriptBuilder[]) result.toArray(new IScriptBuilder[result
+				.size()]);
 	}
 }
