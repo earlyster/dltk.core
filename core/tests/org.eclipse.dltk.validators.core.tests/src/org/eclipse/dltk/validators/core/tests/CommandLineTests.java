20aca804a9353af63ad0e5237b10794822694f7b
apanchenk <apanchenk>
2008-08-15 10:59:50 +0000

    more tests added

--- core/tests/org.eclipse.dltk.validators.core.tests/src/org/eclipse/dltk/validators/core/tests/CommandLineTests.java
+++ core/tests/org.eclipse.dltk.validators.core.tests/src/org/eclipse/dltk/validators/core/tests/CommandLineTests.java
@@ -17,10 +17,61 @@ import junit.framework.TestCase;
 
 public class CommandLineTests extends TestCase {
 
+	public void testConstructor() {
+		assertEquals("", new CommandLine().toString());
+		CommandLine commandLine = new CommandLine("A B");
+		assertEquals("A B", commandLine.toString());
+	}
+
+	public void testAdd() {
+		CommandLine commandLine = new CommandLine();
+		commandLine.add("A");
+		commandLine.add("B");
+		assertEquals("A B", commandLine.toString());
+	}
+
 	public void testAddArray() {
 		CommandLine commandLine = new CommandLine();
 		commandLine.add(new String[] { "A", "B" });
 		assertEquals("A B", commandLine.toString());
 	}
 
+	public void testToArray() {
+		CommandLine commandLine = new CommandLine();
+		commandLine.add(new String[] { "A", "B" });
+		String[] args = commandLine.toArray();
+		assertEquals(2, args.length);
+		assertEquals("A", args[0]);
+		assertEquals("B", args[1]);
+	}
+
+	public void testContains() {
+		CommandLine commandLine = new CommandLine();
+		commandLine.add(new String[] { "A", "B" });
+		assertTrue(commandLine.contains("A"));
+		assertFalse(commandLine.contains("AA"));
+	}
+
+	public void testAddCommandLine() {
+		CommandLine commandLine = new CommandLine("A B");
+		commandLine.add(new CommandLine("C D"));
+		assertEquals("A B C D", commandLine.toString());
+	}
+
+	public void testClear() {
+		CommandLine commandLine = new CommandLine();
+		commandLine.add("A");
+		commandLine.add("B");
+		assertEquals("A B", commandLine.toString());
+		commandLine.clear();
+		assertEquals("", commandLine.toString());
+	}
+
+	public void testReplace() {
+		CommandLine commandLine = new CommandLine("A B %c");
+		commandLine.replaceSequence('x', "XX");
+		assertEquals("A B %c", commandLine.toString());
+		commandLine.replaceSequence('c', "CC");
+		assertEquals("A B CC", commandLine.toString());
+	}
 }
