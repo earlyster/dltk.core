38f8a5243a49078f7bc1bb12d6a846ce47097223
asobolev <asobolev>
2008-09-04 07:32:25 +0000

    * lazy file handle merged

--- core/plugins/org.eclipse.dltk.launching/src/org/eclipse/dltk/internal/launching/LazyFileHandle.java
+++ core/plugins/org.eclipse.dltk.launching/src/org/eclipse/dltk/internal/launching/LazyFileHandle.java
@@ -209,20 +209,22 @@ public class LazyFileHandle implements IFileHandle {
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (obj == null)
+		if (obj == null || !(obj instanceof IFileHandle))
 			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		LazyFileHandle other = (LazyFileHandle) obj;
+		IFileHandle other = (IFileHandle) obj;
 		if (environment == null) {
-			if (other.environment != null)
+			if (other.getEnvironment() != null)
 				return false;
-		} else if (!environment.equals(other.environment))
+		} else {
+			final IEnvironment otherEnvironment = other.getEnvironment();
+			if (otherEnvironment == null
+					|| !environment.equals(otherEnvironment.getId()))
 			return false;
+		}
 		if (path == null) {
-			if (other.path != null)
+			if (other.getPath() != null)
 				return false;
-		} else if (!path.equals(other.path))
+		} else if (!path.equals(other.getPath()))
 			return false;
 		return true;
 	}
