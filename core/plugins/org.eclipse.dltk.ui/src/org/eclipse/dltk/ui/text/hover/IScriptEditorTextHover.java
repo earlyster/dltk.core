b992040c45a0f044b1425a43ba2cbf587d90742a
apanchenk <apanchenk>
2008-09-14 12:23:03 +0000

    formatting

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/text/hover/IScriptEditorTextHover.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/text/hover/IScriptEditorTextHover.java
@@ -13,32 +13,29 @@ import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.text.ITextHover;
 import org.eclipse.ui.IEditorPart;
 
-
 /**
  * Provides a hover popup which appears on top of an editor with relevant
- * display information. If the text hover does not provide information no
- * hover popup is shown.
+ * display information. If the text hover does not provide information no hover
+ * popup is shown.
  * <p>
  * Clients may implement this interface.
  * </p>
- *
+ * 
  * @see org.eclipse.ui.IEditorPart
  * @see org.eclipse.jface.text.ITextHover
- *
-	 *
  */
 public interface IScriptEditorTextHover extends ITextHover {
 
 	/**
 	 * Sets the editor on which the hover is shown.
-	 *
-	 * @param editor the editor on which the hover popup should be shown
+	 * 
+	 * @param editor
+	 *            the editor on which the hover popup should be shown
 	 */
 	void setEditor(IEditorPart editor);
-	
+
 	/**
 	 * Sets the preference store from which hover can get settings.
 	 */
 	void setPreferenceStore(IPreferenceStore store);
 }
-
