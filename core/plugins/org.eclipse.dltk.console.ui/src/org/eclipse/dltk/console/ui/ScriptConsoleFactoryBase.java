3b211ce2a2edbc48ff1c270a3c27ecd810891213
apanchenk <apanchenk>
2008-09-21 03:06:41 +0000

    register console only if it is created

--- core/plugins/org.eclipse.dltk.console.ui/src/org/eclipse/dltk/console/ui/ScriptConsoleFactoryBase.java
+++ core/plugins/org.eclipse.dltk.console.ui/src/org/eclipse/dltk/console/ui/ScriptConsoleFactoryBase.java
@@ -11,20 +11,22 @@ package org.eclipse.dltk.console.ui;
 
 import org.eclipse.ui.console.IConsoleFactory;
 
-
 public abstract class ScriptConsoleFactoryBase implements IConsoleFactory {
-	public ScriptConsoleFactoryBase(){	
+	public ScriptConsoleFactoryBase() {
 	}
-	
-	protected void registerAndOpenConsole(ScriptConsole console){
+
+	protected void registerAndOpenConsole(ScriptConsole console) {
 		ScriptConsoleManager manager = ScriptConsoleManager.getInstance();
 		manager.add(console);
-		manager.showConsole(console);		
+		manager.showConsole(console);
 	}
-			
+
 	public void openConsole() {
-		registerAndOpenConsole(createConsoleInstance());
+		ScriptConsole console = createConsoleInstance();
+		if (console != null) {
+			registerAndOpenConsole(console);
+		}
 	}
-	
+
 	protected abstract ScriptConsole createConsoleInstance();
 }
