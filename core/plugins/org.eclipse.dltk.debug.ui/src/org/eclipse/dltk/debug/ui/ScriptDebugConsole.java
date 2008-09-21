d526436a470ffde2ace9b9010c8bf90915ca91fa
apanchenk <apanchenk>
2008-09-21 02:27:49 +0000

    formatting

--- core/plugins/org.eclipse.dltk.debug.ui/src/org/eclipse/dltk/debug/ui/ScriptDebugConsole.java
+++ core/plugins/org.eclipse.dltk.debug.ui/src/org/eclipse/dltk/debug/ui/ScriptDebugConsole.java
@@ -6,22 +6,28 @@ import org.eclipse.ui.console.IOConsole;
 
 public class ScriptDebugConsole extends IOConsole {
 	private ILaunch launch;
+
 	public ILaunch getLaunch() {
 		return launch;
 	}
+
 	public void setLaunch(ILaunch launch) {
 		this.launch = launch;
 	}
-	public ScriptDebugConsole(String name, ImageDescriptor imageDescriptor, String encoding) {
+
+	public ScriptDebugConsole(String name, ImageDescriptor imageDescriptor,
+			String encoding) {
 		super(name, null, imageDescriptor, encoding, true);
-		
+
 		this.addPatternMatchListener(new ScriptDebugConsoleTraceTracker());
 	}
+
 	public void matcherFinished() {
 		super.matcherFinished();
 	}
+
 	public void partitionerFinished() {
 		super.partitionerFinished();
 	}
-	
+
 }
