05c35978d8c6d561f4c806a0af1e862c7fb3ffe1
asobolev <asobolev>
2008-10-08 04:32:55 +0000

    * unzip method added

--- core/tests/org.eclipse.dltk.core.tests/src/org/eclipse/dltk/core/tests/util/Util.java
+++ core/tests/org.eclipse.dltk.core.tests/src/org/eclipse/dltk/core/tests/util/Util.java
@@ -9,19 +9,29 @@
  *******************************************************************************/
 package org.eclipse.dltk.core.tests.util;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
 public class Util {
 
 	/**
 	 * Generate a display string from the given String.
-	 * @param inputString the given input string
-	 *
-  	 */
-	public static String displayString(String inputString){
+	 * 
+	 * @param inputString
+	 *            the given input string
+	 * 
+	 */
+	public static String displayString(String inputString) {
 		return displayString(inputString, 0);
 	}
+
 	/**
-	 * Generate a display string from the given String.
-	 * It converts:
+	 * Generate a display string from the given String. It converts:
 	 * <ul>
 	 * <li>\t to \t</li>
 	 * <li>\r to \\r</li>
@@ -33,55 +43,67 @@ public class Util {
 	 * <li>\\ to \\\\</li>
 	 * <li>All other characters are unchanged.</li>
 	 * </ul>
-	 * This method doesn't convert \r\n to \n. 
+	 * This method doesn't convert \r\n to \n.
 	 * <p>
-	 * Example of use:
-	 * <o>
+	 * Example of use: <o>
 	 * <li>
+	 * 
 	 * <pre>
-	 * input string = "abc\ndef\tghi",
+	 * input string = &quot;abc\ndef\tghi&quot;,
 	 * indent = 3
-	 * result = "\"\t\t\tabc\\n" +
-	 * 			"\t\t\tdef\tghi\""
+	 * result = &quot;\&quot;\t\t\tabc\\n&quot; +
+	 * 			&quot;\t\t\tdef\tghi\&quot;&quot;
 	 * </pre>
+	 * 
 	 * </li>
 	 * <li>
+	 * 
 	 * <pre>
-	 * input string = "abc\ndef\tghi\n",
+	 * input string = &quot;abc\ndef\tghi\n&quot;,
 	 * indent = 3
-	 * result = "\"\t\t\tabc\\n" +
-	 * 			"\t\t\tdef\tghi\\n\""
+	 * result = &quot;\&quot;\t\t\tabc\\n&quot; +
+	 * 			&quot;\t\t\tdef\tghi\\n\&quot;&quot;
 	 * </pre>
+	 * 
 	 * </li>
 	 * <li>
+	 * 
 	 * <pre>
-	 * input string = "abc\r\ndef\tghi\r\n",
+	 * input string = &quot;abc\r\ndef\tghi\r\n&quot;,
 	 * indent = 3
-	 * result = "\"\t\t\tabc\\r\\n" +
-	 * 			"\t\t\tdef\tghi\\r\\n\""
+	 * result = &quot;\&quot;\t\t\tabc\\r\\n&quot; +
+	 * 			&quot;\t\t\tdef\tghi\\r\\n\&quot;&quot;
 	 * </pre>
+	 * 
 	 * </li>
 	 * </ol>
 	 * </p>
 	 * 
-	 * @param inputString the given input string
-	 * @param indent number of tabs are added at the begining of each line.
-	 *
+	 * @param inputString
+	 *            the given input string
+	 * @param indent
+	 *            number of tabs are added at the begining of each line.
+	 * 
 	 * @return the displayed string
-	*/
+	 */
 	public static String displayString(String inputString, int indent) {
 		return displayString(inputString, indent, false);
 	}
-	public static String displayString(String inputString, int indent, boolean shift) {
+
+	public static String displayString(String inputString, int indent,
+			boolean shift) {
 		if (inputString == null)
 			return "null";
 		int length = inputString.length();
 		StringBuffer buffer = new StringBuffer(length);
-		java.util.StringTokenizer tokenizer = new java.util.StringTokenizer(inputString, "\n\r", true);
-		for (int i = 0; i < indent; i++) buffer.append("\t");
-		if (shift) indent++;
+		java.util.StringTokenizer tokenizer = new java.util.StringTokenizer(
+				inputString, "\n\r", true);
+		for (int i = 0; i < indent; i++)
+			buffer.append("\t");
+		if (shift)
+			indent++;
 		buffer.append("\"");
-		while (tokenizer.hasMoreTokens()){
+		while (tokenizer.hasMoreTokens()) {
 
 			String token = tokenizer.nextToken();
 			if (token.equals("\r")) {
@@ -92,13 +114,15 @@ public class Util {
 						buffer.append("\\n");
 						if (tokenizer.hasMoreTokens()) {
 							buffer.append("\" + \n");
-							for (int i = 0; i < indent; i++) buffer.append("\t");
+							for (int i = 0; i < indent; i++)
+								buffer.append("\t");
 							buffer.append("\"");
 						}
 						continue;
 					}
 					buffer.append("\" + \n");
-					for (int i = 0; i < indent; i++) buffer.append("\t");
+					for (int i = 0; i < indent; i++)
+						buffer.append("\t");
 					buffer.append("\"");
 				} else {
 					continue;
@@ -107,42 +131,43 @@ public class Util {
 				buffer.append("\\n");
 				if (tokenizer.hasMoreTokens()) {
 					buffer.append("\" + \n");
-					for (int i = 0; i < indent; i++) buffer.append("\t");
+					for (int i = 0; i < indent; i++)
+						buffer.append("\t");
 					buffer.append("\"");
 				}
 				continue;
-			}	
+			}
 
 			StringBuffer tokenBuffer = new StringBuffer();
-			for (int i = 0; i < token.length(); i++){ 
+			for (int i = 0; i < token.length(); i++) {
 				char c = token.charAt(i);
 				switch (c) {
-					case '\r' :
-						tokenBuffer.append("\\r");
-						break;
-					case '\n' :
-						tokenBuffer.append("\\n");
-						break;
-					case '\b' :
-						tokenBuffer.append("\\b");
-						break;
-					case '\t' :
-						tokenBuffer.append("\t");
-						break;
-					case '\f' :
-						tokenBuffer.append("\\f");
-						break;
-					case '\"' :
-						tokenBuffer.append("\\\"");
-						break;
-					case '\'' :
-						tokenBuffer.append("\\'");
-						break;
-					case '\\' :
-						tokenBuffer.append("\\\\");
-						break;
-					default :
-						tokenBuffer.append(c);
+				case '\r':
+					tokenBuffer.append("\\r");
+					break;
+				case '\n':
+					tokenBuffer.append("\\n");
+					break;
+				case '\b':
+					tokenBuffer.append("\\b");
+					break;
+				case '\t':
+					tokenBuffer.append("\t");
+					break;
+				case '\f':
+					tokenBuffer.append("\\f");
+					break;
+				case '\"':
+					tokenBuffer.append("\\\"");
+					break;
+				case '\'':
+					tokenBuffer.append("\\'");
+					break;
+				case '\\':
+					tokenBuffer.append("\\\\");
+					break;
+				default:
+					tokenBuffer.append(c);
 				}
 			}
 			buffer.append(tokenBuffer.toString());
@@ -150,15 +175,16 @@ public class Util {
 		buffer.append("\"");
 		return buffer.toString();
 	}
-	
+
 	public static String convertToIndependantLineDelimiter(String source) {
-		if (source.indexOf('\n') == -1 && source.indexOf('\r') == -1) return source;
+		if (source.indexOf('\n') == -1 && source.indexOf('\r') == -1)
+			return source;
 		StringBuffer buffer = new StringBuffer();
 		for (int i = 0, length = source.length(); i < length; i++) {
 			char car = source.charAt(i);
 			if (car == '\r') {
 				buffer.append('\n');
-				if (i < length-1 && source.charAt(i+1) == '\n') {
+				if (i < length - 1 && source.charAt(i + 1) == '\n') {
 					i++; // skip \n after \r
 				}
 			} else {
@@ -167,5 +193,60 @@ public class Util {
 		}
 		return buffer.toString();
 	}
-	
+
+	/**
+	 * Unzip the contents of the given zip in the given directory (create it if
+	 * it doesn't exist)
+	 */
+	public static void unzip(String zipPath, String destDirPath)
+			throws IOException {
+
+		InputStream zipIn = new FileInputStream(zipPath);
+		byte[] buf = new byte[8192];
+		File destDir = new File(destDirPath);
+		ZipInputStream zis = new ZipInputStream(zipIn);
+		FileOutputStream fos = null;
+		try {
+			ZipEntry zEntry;
+			while ((zEntry = zis.getNextEntry()) != null) {
+				// if it is empty directory, create it
+				if (zEntry.isDirectory()) {
+					new File(destDir, zEntry.getName()).mkdirs();
+					continue;
+				}
+				// if it is a file, extract it
+				String filePath = zEntry.getName();
+				int lastSeparator = filePath.lastIndexOf("/"); //$NON-NLS-1$
+				String fileDir = ""; //$NON-NLS-1$
+				if (lastSeparator >= 0) {
+					fileDir = filePath.substring(0, lastSeparator);
+				}
+				// create directory for a file
+				new File(destDir, fileDir).mkdirs();
+				// write file
+				File outFile = new File(destDir, filePath);
+				fos = new FileOutputStream(outFile);
+				int n = 0;
+				while ((n = zis.read(buf)) >= 0) {
+					fos.write(buf, 0, n);
+				}
+				fos.close();
+			}
+		} catch (IOException ioe) {
+			if (fos != null) {
+				try {
+					fos.close();
+				} catch (IOException ioe2) {
+				}
+			}
+		} finally {
+			try {
+				zipIn.close();
+				if (zis != null)
+					zis.close();
+			} catch (IOException ioe) {
+			}
+		}
+	}
+
 }
