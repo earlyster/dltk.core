b9cccacca714c52ba7af431d240ccf9af067af7d
apanchenk <apanchenk>
2008-10-20 07:12:46 +0000

    formatting

--- core/plugins/org.eclipse.dltk.core/model/org/eclipse/dltk/internal/core/DiscardWorkingCopyOperation.java
+++ core/plugins/org.eclipse.dltk.core/model/org/eclipse/dltk/internal/core/DiscardWorkingCopyOperation.java
@@ -15,21 +15,23 @@ import org.eclipse.dltk.core.IModelElementDelta;
 import org.eclipse.dltk.core.ModelException;
 
 /**
- * Discards a working copy (decrement its use count and remove its working copy info if the use count is 0)
- * and signal its removal through a delta.
+ * Discards a working copy (decrement its use count and remove its working copy
+ * info if the use count is 0) and signal its removal through a delta.
  */
 public class DiscardWorkingCopyOperation extends ModelOperation {
-	
+
 	public DiscardWorkingCopyOperation(IModelElement workingCopy) {
-		super(new IModelElement[] {workingCopy});
+		super(new IModelElement[] { workingCopy });
 	}
+
 	protected void executeOperation() throws ModelException {
 		SourceModule workingCopy = getWorkingCopy();
 		ModelManager manager = ModelManager.getModelManager();
 		int useCount = manager.discardPerWorkingCopyInfo(workingCopy);
 		if (useCount == 0) {
 			IScriptProject scriptProject = workingCopy.getScriptProject();
-			if (ExternalScriptProject.EXTERNAL_PROJECT_NAME.equals(scriptProject.getElementName())) {
+			if (ExternalScriptProject.EXTERNAL_PROJECT_NAME
+					.equals(scriptProject.getElementName())) {
 				manager.removePerProjectInfo((ScriptProject) scriptProject);
 				manager.containerRemove(scriptProject);
 			}
@@ -41,25 +43,32 @@ public class DiscardWorkingCopyOperation extends ModelOperation {
 				removeReconcileDelta(workingCopy);
 			} else {
 				if (workingCopy.getResource().isAccessible()) {
-					// report a F_PRIMARY_WORKING_COPY change delta for a primary working copy
-					ModelElementDelta delta = new ModelElementDelta(this.getModel());
-					delta.changed(workingCopy, IModelElementDelta.F_PRIMARY_WORKING_COPY);
+					// report a F_PRIMARY_WORKING_COPY change delta for a
+					// primary working copy
+					ModelElementDelta delta = new ModelElementDelta(this
+							.getModel());
+					delta.changed(workingCopy,
+							IModelElementDelta.F_PRIMARY_WORKING_COPY);
 					addDelta(delta);
 				} else {
 					// report a REMOVED delta
-					ModelElementDelta delta = new ModelElementDelta(this.getModel());
-					delta.removed(workingCopy, IModelElementDelta.F_PRIMARY_WORKING_COPY);
+					ModelElementDelta delta = new ModelElementDelta(this
+							.getModel());
+					delta.removed(workingCopy,
+							IModelElementDelta.F_PRIMARY_WORKING_COPY);
 					addDelta(delta);
 				}
 			}
 		}
 	}
+
 	/**
 	 * Returns the working copy this operation is working on.
 	 */
 	protected SourceModule getWorkingCopy() {
-		return (SourceModule)getElementToProcess();
+		return (SourceModule) getElementToProcess();
 	}
+
 	/**
 	 * @see ModelOperation#isReadOnly
 	 */
