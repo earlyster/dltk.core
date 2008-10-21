db9d6f6a88917187207332a15149203dc4961ea5
apanchenk <apanchenk>
2008-10-21 13:47:05 +0000

    change api, IScriptInterpreter.exec(String) now returns IScriptExecResult, so we can return output and error flag

--- core/plugins/org.eclipse.dltk.console/src/org/eclipse/dltk/console/IScriptExecResult.java
+++ core/plugins/org.eclipse.dltk.console/src/org/eclipse/dltk/console/IScriptExecResult.java
@@ -1,22 +1,20 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2007 IBM Corporation and others.
+ * Copyright (c) 2008 xored software, Inc.
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- 
+ * Contributors:
+ *     xored software, Inc. - initial API and Implementation (Alex Panchenko)
  *******************************************************************************/
 package org.eclipse.dltk.console;
 
-import java.io.IOException;
+public interface IScriptExecResult {
 
-public interface IScriptConsoleInterpreter {
-	int WAIT_NEW_COMMAND = 0;
-	int WAIT_CONTINUE_COMMAND = 1;
-	int WAIT_USER_INPUT = 2;
+	String getOutput();
 
-	IScriptExecResult exec(String command) throws IOException;
+	boolean isError();
 
-	int getState();
 }
---
+++ core/plugins/org.eclipse.dltk.console/src/org/eclipse/dltk/console/ScriptExecResult.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2008 xored software, Inc.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     xored software, Inc. - initial API and Implementation (Alex Panchenko)
+ *******************************************************************************/
+package org.eclipse.dltk.console;
+
+public class ScriptExecResult implements IScriptExecResult {
+
+	private final String output;
+	private final boolean error;
+
+	public ScriptExecResult(String output) {
+		this(output, false);
+	}
+
+	/**
+	 * @param output
+	 * @param error
+	 */
+	public ScriptExecResult(String output, boolean error) {
+		this.output = output;
+		this.error = error;
+	}
+
+	public String getOutput() {
+		return output;
+	}
+
+	public boolean isError() {
+		return error;
+	}
+
+}
