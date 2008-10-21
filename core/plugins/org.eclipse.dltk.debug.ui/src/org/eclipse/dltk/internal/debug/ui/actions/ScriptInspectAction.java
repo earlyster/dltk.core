1cba9da80b77a53224662ed69b907a19ebfd171d
apanchenk <apanchenk>
2008-10-21 08:12:52 +0000

    fixed typo in class name

--- core/plugins/org.eclipse.dltk.debug.ui/src/org/eclipse/dltk/internal/debug/ui/actions/ScriptInspectAction.java
+++ core/plugins/org.eclipse.dltk.debug.ui/src/org/eclipse/dltk/internal/debug/ui/actions/ScriptInspectAction.java
@@ -12,7 +12,7 @@ package org.eclipse.dltk.internal.debug.ui.actions;
 
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.dltk.debug.core.eval.IScriptEvaluationResult;
-import org.eclipse.dltk.debug.core.eval.InsepctEvaluatedScriptExpression;
+import org.eclipse.dltk.debug.core.eval.InspectEvaluatedScriptExpression;
 import org.eclipse.dltk.debug.ui.DLTKDebugUIPlugin;
 import org.eclipse.swt.widgets.Display;
 
@@ -26,7 +26,7 @@ public class ScriptInspectAction extends ScriptEvaluationAction {
 			public void run() {
 				if (!display.isDisposed()) {
 					showExpressionView();
-					InsepctEvaluatedScriptExpression expression = new InsepctEvaluatedScriptExpression(
+					InspectEvaluatedScriptExpression expression = new InspectEvaluatedScriptExpression(
 							result);
 					DebugPlugin.getDefault().getExpressionManager()
 							.addExpression(expression);
