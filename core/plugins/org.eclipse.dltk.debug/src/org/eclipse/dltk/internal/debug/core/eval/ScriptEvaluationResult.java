d12ee2eaf09366c9f774f9c94ac8fc3d5d0f6090
apanchenk <apanchenk>
2008-10-21 08:02:03 +0000

    use CharOperation.NO_STRINGS instead of own NO_MESSAGES

--- core/plugins/org.eclipse.dltk.debug/src/org/eclipse/dltk/internal/debug/core/eval/ScriptEvaluationResult.java
+++ core/plugins/org.eclipse.dltk.debug/src/org/eclipse/dltk/internal/debug/core/eval/ScriptEvaluationResult.java
@@ -1,13 +1,12 @@
 package org.eclipse.dltk.internal.debug.core.eval;
 
 import org.eclipse.debug.core.DebugException;
+import org.eclipse.dltk.compiler.CharOperation;
 import org.eclipse.dltk.debug.core.eval.IScriptEvaluationResult;
 import org.eclipse.dltk.debug.core.model.IScriptThread;
 import org.eclipse.dltk.debug.core.model.IScriptValue;
 
 public class ScriptEvaluationResult implements IScriptEvaluationResult {
-	private static final String[] NO_MESSAGES = new String[0];
-
 	private final IScriptThread thread;
 	private final String snippet;
 	private final IScriptValue value;
@@ -32,7 +31,7 @@ public class ScriptEvaluationResult implements IScriptEvaluationResult {
 	}
 
 	public String[] getErrorMessages() {
-		return NO_MESSAGES;
+		return CharOperation.NO_STRINGS;
 	}
 
 	public DebugException getException() {
