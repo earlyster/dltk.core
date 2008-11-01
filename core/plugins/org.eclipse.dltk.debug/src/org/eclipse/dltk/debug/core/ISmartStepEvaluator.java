a26d31df5ec9bfa28e23d84563bce4377cd42bf4
apanchenk <apanchenk>
2008-11-01 09:49:25 +0000

    method renamed and documented

--- core/plugins/org.eclipse.dltk.debug/src/org/eclipse/dltk/debug/core/ISmartStepEvaluator.java
+++ core/plugins/org.eclipse.dltk.debug/src/org/eclipse/dltk/debug/core/ISmartStepEvaluator.java
@@ -3,5 +3,15 @@ package org.eclipse.dltk.debug.core;
 import org.eclipse.dltk.debug.core.model.IScriptThread;
 
 public interface ISmartStepEvaluator {
-	boolean skipSuspend(String[] filters, IScriptThread thread);
+
+	/**
+	 * Tests if the stack of the specified thread should be skipped. Returns
+	 * <code>true</code> if execution should automatically continue or
+	 * <code>false</code> if not.
+	 * 
+	 * @param filters
+	 * @param thread
+	 * @return
+	 */
+	boolean isFiltered(String[] filters, IScriptThread thread);
 }
