d0bb57071e39e39210e6bb77fa11ea6978d95360
apanchenk <apanchenk>
2008-11-10 06:21:45 +0000

    formatting

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/documentation/IScriptDocumentationProvider.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/documentation/IScriptDocumentationProvider.java
@@ -13,30 +13,34 @@ import java.io.Reader;
 
 import org.eclipse.dltk.core.IMember;
 
-
 /**
- * Interface for org.eclipse.dltk.ui.scriptDocumentationProviders extension point.
- * 
+ * Interface for org.eclipse.dltk.ui.scriptDocumentationProviders extension
+ * point.
  */
 public interface IScriptDocumentationProvider {
-	
+
 	/**
-	 * Fetches info for a member like proc or variable. Also it
-	 * may look into parents, if child contains no documentaton. 
+	 * Fetches info for a member like proc or variable. Also it may look into
+	 * parents, if child contains no documentaton.
 	 * 
-	 * @param element Element to get info for.
-	 * @param lookIntoParents Allows to look into parents for a doc.
-	 * @param lookIntoExternal Allows to look into external resources.
+	 * @param element
+	 *            Element to get info for.
+	 * @param lookIntoParents
+	 *            Allows to look into parents for a doc.
+	 * @param lookIntoExternal
+	 *            Allows to look into external resources.
 	 * @return Reader object, providing doc.
 	 */
-	public Reader getInfo (IMember element, boolean lookIntoParents, boolean lookIntoExternal);
-		
+	public Reader getInfo(IMember element, boolean lookIntoParents,
+			boolean lookIntoExternal);
+
 	/**
 	 * Fetches some info about provided keyword.
 	 * 
-	 * @param content Keyword to get info for.
-	 * @return  Reader object, providing doc.
+	 * @param content
+	 *            Keyword to get info for.
+	 * @return Reader object, providing doc.
 	 */
-	public Reader getInfo (String content);
-	
+	public Reader getInfo(String content);
+
 }
