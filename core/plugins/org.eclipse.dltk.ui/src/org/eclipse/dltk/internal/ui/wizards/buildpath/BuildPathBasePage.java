bbb15b7e22f94aafdd3ac17c71254a88419fb380
apeled <apeled>
2008-11-12 14:43:09 +0000

    fixed formatting

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/wizards/buildpath/BuildPathBasePage.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/wizards/buildpath/BuildPathBasePage.java
@@ -14,50 +14,53 @@ import java.util.List;
 import java.util.Set;
 
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.dltk.core.ScriptModelUtil;
 import org.eclipse.dltk.core.IBuildpathEntry;
 import org.eclipse.dltk.core.IScriptProject;
+import org.eclipse.dltk.core.ScriptModelUtil;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 
-
 public abstract class BuildPathBasePage {
-	
+
 	public abstract List getSelection();
+
 	public abstract void setSelection(List selection, boolean expand);
-	
+
 	public void addElement(BPListElement element) {
-		
+
 	}
-	
+
 	public abstract boolean isEntryKind(int kind);
-			
+
 	protected void filterAndSetSelection(List list) {
-		ArrayList res= new ArrayList(list.size());
-		for (int i= list.size()-1; i >= 0; i--) {
-			Object curr= list.get(i);
+		ArrayList res = new ArrayList(list.size());
+		for (int i = list.size() - 1; i >= 0; i--) {
+			Object curr = list.get(i);
 			if (curr instanceof BPListElement) {
-				BPListElement elem= (BPListElement) curr;
-				if (elem.getParentContainer() == null && isEntryKind(elem.getEntryKind())) {
+				BPListElement elem = (BPListElement) curr;
+				if (elem.getParentContainer() == null
+						&& isEntryKind(elem.getEntryKind())) {
 					res.add(curr);
 				}
 			}
 		}
 		setSelection(res, false);
 	}
-	
-	public static void fixNestingConflicts(BPListElement[] newEntries, BPListElement[] existing, Set modifiedSourceEntries) {
-		for (int i= 0; i < newEntries.length; i++) {
+
+	public static void fixNestingConflicts(BPListElement[] newEntries,
+			BPListElement[] existing, Set modifiedSourceEntries) {
+		for (int i = 0; i < newEntries.length; i++) {
 			addExclusionPatterns(newEntries[i], existing, modifiedSourceEntries);
 		}
 	}
-	
-	private static void addExclusionPatterns(BPListElement newEntry, BPListElement[] existing, Set modifiedEntries) {
-		IPath entryPath= newEntry.getPath();
-		for (int i= 0; i < existing.length; i++) {
-			BPListElement curr= existing[i];
+
+	private static void addExclusionPatterns(BPListElement newEntry,
+			BPListElement[] existing, Set modifiedEntries) {
+		IPath entryPath = newEntry.getPath();
+		for (int i = 0; i < existing.length; i++) {
+			BPListElement curr = existing[i];
 			if (curr.getEntryKind() == IBuildpathEntry.BPE_SOURCE) {
-				IPath currPath= curr.getPath();
+				IPath currPath = curr.getPath();
 				if (!currPath.equals(entryPath)) {
 					if (currPath.isPrefixOf(entryPath)) {
 						if (addToExclusions(entryPath, curr)) {
@@ -72,26 +75,29 @@ public abstract class BuildPathBasePage {
 			}
 		}
 	}
-	
+
 	private static boolean addToExclusions(IPath entryPath, BPListElement curr) {
-		IPath[] exclusionFilters= (IPath[]) curr.getAttribute(BPListElement.EXCLUSION);
+		IPath[] exclusionFilters = (IPath[]) curr
+				.getAttribute(BPListElement.EXCLUSION);
 		if (!ScriptModelUtil.isExcludedPath(entryPath, exclusionFilters)) {
-			IPath pathToExclude= entryPath.removeFirstSegments(curr.getPath().segmentCount()).addTrailingSeparator();
-			IPath[] newExclusionFilters= new IPath[exclusionFilters.length + 1];
-			System.arraycopy(exclusionFilters, 0, newExclusionFilters, 0, exclusionFilters.length);
-			newExclusionFilters[exclusionFilters.length]= pathToExclude;
+			IPath pathToExclude = entryPath.removeFirstSegments(
+					curr.getPath().segmentCount()).addTrailingSeparator();
+			IPath[] newExclusionFilters = new IPath[exclusionFilters.length + 1];
+			System.arraycopy(exclusionFilters, 0, newExclusionFilters, 0,
+					exclusionFilters.length);
+			newExclusionFilters[exclusionFilters.length] = pathToExclude;
 			curr.setAttribute(BPListElement.EXCLUSION, newExclusionFilters);
 			return true;
 		}
 		return false;
 	}
-	
+
 	protected boolean containsOnlyTopLevelEntries(List selElements) {
 		if (selElements.size() == 0) {
 			return true;
 		}
-		for (int i= 0; i < selElements.size(); i++) {
-			Object elem= selElements.get(i);
+		for (int i = 0; i < selElements.size(); i++) {
+			Object elem = selElements.get(i);
 			if (elem instanceof BPListElement) {
 				if (((BPListElement) elem).getParentContainer() != null) {
 					return false;
@@ -106,5 +112,5 @@ public abstract class BuildPathBasePage {
 	public abstract void init(IScriptProject scriptProject);
 
 	public abstract Control getControl(Composite parent);
-	
+
 }
