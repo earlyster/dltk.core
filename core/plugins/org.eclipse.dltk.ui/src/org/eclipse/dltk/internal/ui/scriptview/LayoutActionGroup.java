3cec8f9701ec8205ddf644cdebe2f049f46b4830
rganor <rganor>
2008-11-30 07:01:15 +0000

    [LayoutGroup] making this class public enables extender to update layouts (remove/add  hierarchical/flat modes)

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/scriptview/LayoutActionGroup.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/scriptview/LayoutActionGroup.java
@@ -12,47 +12,46 @@ package org.eclipse.dltk.internal.ui.scriptview;
 import org.eclipse.dltk.core.DLTKCore;
 import org.eclipse.dltk.internal.ui.actions.MultiActionGroup;
 import org.eclipse.dltk.ui.DLTKPluginImages;
-import org.eclipse.jface.action.Action;
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.action.IMenuManager;
-import org.eclipse.jface.action.MenuManager;
-import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.action.*;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IWorkbenchActionConstants;
-
 
 /**
  * Adds view menus to switch between flat and hierarchical layout.
  * 
-	 *
+ * 
  */
-class LayoutActionGroup extends MultiActionGroup {
+public class LayoutActionGroup extends MultiActionGroup {
 
 	LayoutActionGroup(ScriptExplorerPart packageExplorer) {
 		super(createActions(packageExplorer), getSelectedState(packageExplorer));
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see ActionGroup#fillActionBars(IActionBars)
 	 */
 	public void fillActionBars(IActionBars actionBars) {
 		super.fillActionBars(actionBars);
 		contributeToViewMenu(actionBars.getMenuManager());
 	}
-	
+
 	private void contributeToViewMenu(IMenuManager viewMenu) {
 		viewMenu.add(new Separator());
 
 		// Create layout sub menu
-		
-		IMenuManager layoutSubMenu= new MenuManager(ScriptMessages.LayoutActionGroup_label); 
-		final String layoutGroupName= "layout"; //$NON-NLS-1$
-		Separator marker= new Separator(layoutGroupName);
+
+		IMenuManager layoutSubMenu = new MenuManager(
+				ScriptMessages.LayoutActionGroup_label);
+		final String layoutGroupName = "layout"; //$NON-NLS-1$
+		Separator marker = new Separator(layoutGroupName);
 
 		viewMenu.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
 		viewMenu.add(marker);
 		viewMenu.appendToGroup(layoutGroupName, layoutSubMenu);
-		viewMenu.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS+"-end"));//$NON-NLS-1$		
+		viewMenu.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS
+				+ "-end"));//$NON-NLS-1$		
 		addActions(layoutSubMenu);
 	}
 
@@ -62,16 +61,20 @@ class LayoutActionGroup extends MultiActionGroup {
 		else
 			return 1;
 	}
-	
+
 	static IAction[] createActions(ScriptExplorerPart packageExplorer) {
-		IAction flatLayoutAction= new LayoutAction(packageExplorer, true);
-		flatLayoutAction.setText(ScriptMessages.LayoutActionGroup_flatLayoutAction_label); 
-		DLTKPluginImages.setLocalImageDescriptors(flatLayoutAction, "flatLayout.gif"); //$NON-NLS-1$
-		IAction hierarchicalLayout= new LayoutAction(packageExplorer, false);
-		hierarchicalLayout.setText(ScriptMessages.LayoutActionGroup_hierarchicalLayoutAction_label);	  
-		DLTKPluginImages.setLocalImageDescriptors(hierarchicalLayout, "hierarchicalLayout.gif"); //$NON-NLS-1$
-		
-		return new IAction[]{flatLayoutAction, hierarchicalLayout};
+		IAction flatLayoutAction = new LayoutAction(packageExplorer, true);
+		flatLayoutAction
+				.setText(ScriptMessages.LayoutActionGroup_flatLayoutAction_label);
+		DLTKPluginImages.setLocalImageDescriptors(flatLayoutAction,
+				"flatLayout.gif"); //$NON-NLS-1$
+		IAction hierarchicalLayout = new LayoutAction(packageExplorer, false);
+		hierarchicalLayout
+				.setText(ScriptMessages.LayoutActionGroup_hierarchicalLayoutAction_label);
+		DLTKPluginImages.setLocalImageDescriptors(hierarchicalLayout,
+				"hierarchicalLayout.gif"); //$NON-NLS-1$
+
+		return new IAction[] { flatLayoutAction, hierarchicalLayout };
 	}
 }
 
@@ -83,15 +86,17 @@ class LayoutAction extends Action implements IAction {
 	public LayoutAction(ScriptExplorerPart packageExplorer, boolean flat) {
 		super("", AS_RADIO_BUTTON); //$NON-NLS-1$
 
-		fIsFlatLayout= flat;
-		fPackageExplorer= packageExplorer;
+		fIsFlatLayout = flat;
+		fPackageExplorer = packageExplorer;
 		if (DLTKCore.DEBUG) {
 			System.err.println("Add help support here..."); //$NON-NLS-1$
-		}		
-//		if (fIsFlatLayout)
-//			PlatformUI.getWorkbench().getHelpSystem().setHelp(this, IScriptHelpContextIds.LAYOUT_FLAT_ACTION);
-//		else
-//			PlatformUI.getWorkbench().getHelpSystem().setHelp(this, IScriptHelpContextIds.LAYOUT_HIERARCHICAL_ACTION);
+		}
+		// if (fIsFlatLayout)
+		// PlatformUI.getWorkbench().getHelpSystem().setHelp(this,
+		// IScriptHelpContextIds.LAYOUT_FLAT_ACTION);
+		// else
+		// PlatformUI.getWorkbench().getHelpSystem().setHelp(this,
+		// IScriptHelpContextIds.LAYOUT_HIERARCHICAL_ACTION);
 	}
 
 	/*
