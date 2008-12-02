ce122e2ab957c0369ec244685b159f9ea6cfd312
apanchenk <apanchenk>
2008-12-02 19:16:09 +0000

    deleted constructor with DLTKToken arguments

--- core/plugins/org.eclipse.dltk.core/ast/org/eclipse/dltk/ast/declarations/MethodDeclaration.java
+++ core/plugins/org.eclipse.dltk.core/ast/org/eclipse/dltk/ast/declarations/MethodDeclaration.java
@@ -19,7 +19,6 @@ import java.util.List;
 
 import org.eclipse.dltk.ast.ASTListNode;
 import org.eclipse.dltk.ast.ASTVisitor;
-import org.eclipse.dltk.ast.DLTKToken;
 import org.eclipse.dltk.ast.statements.Block;
 import org.eclipse.dltk.internal.compiler.lookup.MethodScope;
 import org.eclipse.dltk.utils.CorePrinter;
@@ -34,12 +33,6 @@ public class MethodDeclaration extends Declaration {
 	private List decorators;
 
 	private String declaringTypeName;
-
-	public MethodDeclaration(DLTKToken function_t, DLTKToken name) {
-
-		super(name, function_t.getColumn(), name.getColumn()
-				+ name.getText().length());
-	}
 
 	public MethodDeclaration(String name, int nameStart, int nameEnd,
 			int declStart, int declEnd) {
@@ -68,14 +61,12 @@ public class MethodDeclaration extends Declaration {
 	public void traverse(ASTVisitor visitor) throws Exception {
 
 		if (visitor.visit(this)) {
-			// Deocrators
+			// Decorators
 			if (this.decorators != null) {
-				if (this.decorators != null) {
-					Iterator it = this.decorators.iterator();
-					while (it.hasNext()) {
-						Decorator dec = (Decorator) it.next();
-						dec.traverse(visitor);
-					}
+				Iterator it = this.decorators.iterator();
+				while (it.hasNext()) {
+					Decorator dec = (Decorator) it.next();
+					dec.traverse(visitor);
 				}
 			}
 
@@ -196,8 +187,8 @@ public class MethodDeclaration extends Declaration {
 				&& (this.declaringTypeName == null || this.declaringTypeName
 						.equals(d.declaringTypeName));
 	}
-	
-	/* (non-Javadoc)
+
+	/*
 	 * @see org.eclipse.dltk.ast.ASTNode#matchStart()
 	 */
 	public int matchStart() {
@@ -205,8 +196,6 @@ public class MethodDeclaration extends Declaration {
 	}
 
 	/*
-	 * (non-Javadoc)
-	 * 
 	 * @see org.eclipse.dltk.ast.ASTNode#matchLength()
 	 */
 	public int matchLength() {
