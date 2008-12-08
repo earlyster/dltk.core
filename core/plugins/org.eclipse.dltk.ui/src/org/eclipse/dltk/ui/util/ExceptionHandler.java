6851f04ae78ad93c50a707ed817b62bfe22e091e
apanchenk <apanchenk>
2008-12-08 09:33:30 +0000

    formatting

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/util/ExceptionHandler.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/util/ExceptionHandler.java
@@ -22,80 +22,100 @@ import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
 
-
 /**
- * The default exception handler shows an error dialog when one of its handle methods
- * is called. If the passed exception is a <code>CoreException</code> an error dialog
- * pops up showing the exception's status information. For a <code>InvocationTargetException</code>
- * a normal message dialog pops up showing the exception's message. Additionally the exception
- * is written to the platform log.
+ * The default exception handler shows an error dialog when one of its handle
+ * methods is called. If the passed exception is a <code>CoreException</code> an
+ * error dialog pops up showing the exception's status information. For a
+ * <code>InvocationTargetException</code> a normal message dialog pops up
+ * showing the exception's message. Additionally the exception is written to the
+ * platform log.
  */
 public class ExceptionHandler {
 
-	private static ExceptionHandler fgInstance= new ExceptionHandler();
-	
+	private static ExceptionHandler fgInstance = new ExceptionHandler();
+
 	/**
-	 * Logs the given exception using the platform's logging mechanism. The exception is
-	 * logged as an error with the error code <code>ModelStatusConstants.INTERNAL_ERROR</code>.
+	 * Logs the given exception using the platform's logging mechanism. The
+	 * exception is logged as an error with the error code
+	 * <code>ModelStatusConstants.INTERNAL_ERROR</code>.
 	 */
 	public static void log(Throwable t, String message) {
-		DLTKUIPlugin.log(new Status(IStatus.ERROR, DLTKUIPlugin.PLUGIN_ID, 
-			IDLTKStatusConstants.INTERNAL_ERROR, message, t));
+		DLTKUIPlugin.log(new Status(IStatus.ERROR, DLTKUIPlugin.PLUGIN_ID,
+				IDLTKStatusConstants.INTERNAL_ERROR, message, t));
 	}
-	
+
 	/**
-	 * Handles the given <code>CoreException</code>. The workbench shell is used as a parent
-	 * for the dialog window.
+	 * Handles the given <code>CoreException</code>. The workbench shell is used
+	 * as a parent for the dialog window.
 	 * 
-	 * @param e the <code>CoreException</code> to be handled
-	 * @param title the dialog window's window title
-	 * @param message message to be displayed by the dialog window
+	 * @param e
+	 *            the <code>CoreException</code> to be handled
+	 * @param title
+	 *            the dialog window's window title
+	 * @param message
+	 *            message to be displayed by the dialog window
 	 */
 	public static void handle(CoreException e, String title, String message) {
 		handle(e, DLTKUIPlugin.getActiveWorkbenchShell(), title, message);
 	}
-	
+
 	/**
-	 * Handles the given <code>CoreException</code>. 
+	 * Handles the given <code>CoreException</code>.
 	 * 
-	 * @param e the <code>CoreException</code> to be handled
-	 * @param parent the dialog window's parent shell
-	 * @param title the dialog window's window title
-	 * @param message message to be displayed by the dialog window
+	 * @param e
+	 *            the <code>CoreException</code> to be handled
+	 * @param parent
+	 *            the dialog window's parent shell
+	 * @param title
+	 *            the dialog window's window title
+	 * @param message
+	 *            message to be displayed by the dialog window
 	 */
-	public static void handle(CoreException e, Shell parent, String title, String message) {
-		fgInstance.perform(e, parent, title, message);
-	}
-	
-	/**
-	 * Handles the given <code>InvocationTargetException</code>. The workbench shell is used 
-	 * as a parent for the dialog window.
-	 * 
-	 * @param e the <code>InvocationTargetException</code> to be handled
-	 * @param title the dialog window's window title
-	 * @param message message to be displayed by the dialog window
-	 */
-	public static void handle(InvocationTargetException e, String title, String message) {
-		handle(e, DLTKUIPlugin.getActiveWorkbenchShell(), title, message);
-	}
-	
-	/**
-	 * Handles the given <code>InvocationTargetException</code>. 
-	 * 
-	 * @param e the <code>InvocationTargetException</code> to be handled
-	 * @param parent the dialog window's parent shell
-	 * @param title the dialog window's window title
-	 * @param message message to be displayed by the dialog window
-	 */
-	public static void handle(InvocationTargetException e, Shell parent, String title, String message) {
+	public static void handle(CoreException e, Shell parent, String title,
+			String message) {
 		fgInstance.perform(e, parent, title, message);
 	}
 
-	//---- Hooks for subclasses to control exception handling ------------------------------------
-	
-	protected void perform(CoreException e, Shell shell, String title, String message) {
+	/**
+	 * Handles the given <code>InvocationTargetException</code>. The workbench
+	 * shell is used as a parent for the dialog window.
+	 * 
+	 * @param e
+	 *            the <code>InvocationTargetException</code> to be handled
+	 * @param title
+	 *            the dialog window's window title
+	 * @param message
+	 *            message to be displayed by the dialog window
+	 */
+	public static void handle(InvocationTargetException e, String title,
+			String message) {
+		handle(e, DLTKUIPlugin.getActiveWorkbenchShell(), title, message);
+	}
+
+	/**
+	 * Handles the given <code>InvocationTargetException</code>.
+	 * 
+	 * @param e
+	 *            the <code>InvocationTargetException</code> to be handled
+	 * @param parent
+	 *            the dialog window's parent shell
+	 * @param title
+	 *            the dialog window's window title
+	 * @param message
+	 *            message to be displayed by the dialog window
+	 */
+	public static void handle(InvocationTargetException e, Shell parent,
+			String title, String message) {
+		fgInstance.perform(e, parent, title, message);
+	}
+
+	// ---- Hooks for subclasses to control exception handling
+	// ------------------------------------
+
+	protected void perform(CoreException e, Shell shell, String title,
+			String message) {
 		DLTKUIPlugin.log(e);
-		IStatus status= e.getStatus();
+		IStatus status = e.getStatus();
 		if (status != null) {
 			ErrorDialog.openError(shell, title, message, status);
 		} else {
@@ -103,32 +123,36 @@ public class ExceptionHandler {
 		}
 	}
 
-	protected void perform(InvocationTargetException e, Shell shell, String title, String message) {
-		Throwable target= e.getTargetException();
+	protected void perform(InvocationTargetException e, Shell shell,
+			String title, String message) {
+		Throwable target = e.getTargetException();
 		if (target instanceof CoreException) {
-			perform((CoreException)target, shell, title, message);
+			perform((CoreException) target, shell, title, message);
 		} else {
 			DLTKUIPlugin.log(e);
 			if (e.getMessage() != null && e.getMessage().length() > 0) {
 				displayMessageDialog(e, e.getMessage(), shell, title, message);
 			} else {
-				displayMessageDialog(e, target.getMessage(), shell, title, message);
+				displayMessageDialog(e, target.getMessage(), shell, title,
+						message);
 			}
 		}
 	}
 
-	//---- Helper methods -----------------------------------------------------------------------
-	
-	private void displayMessageDialog(Throwable t, String exceptionMessage, Shell shell, String title, String message) {
-		StringWriter msg= new StringWriter();
+	// ---- Helper methods
+	// -----------------------------------------------------------------------
+
+	private void displayMessageDialog(Throwable t, String exceptionMessage,
+			Shell shell, String title, String message) {
+		StringWriter msg = new StringWriter();
 		if (message != null) {
 			msg.write(message);
 			msg.write("\n\n"); //$NON-NLS-1$
 		}
 		if (exceptionMessage == null || exceptionMessage.length() == 0)
-			msg.write(DLTKUIMessages.ExceptionDialog_seeErrorLogMessage); 
+			msg.write(DLTKUIMessages.ExceptionDialog_seeErrorLogMessage);
 		else
 			msg.write(exceptionMessage);
-		MessageDialog.openError(shell, title, msg.toString());			
-	}	
+		MessageDialog.openError(shell, title, msg.toString());
+	}
 }
