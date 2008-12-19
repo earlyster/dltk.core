65e2975e2e9c7b9da992e0cb2910dcd5522a6e3b
mspector <mspector>
2008-12-19 11:40:22 +0000

    Fixed #259356

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/text/completion/ScriptCompletionProposal.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/text/completion/ScriptCompletionProposal.java
@@ -16,39 +16,58 @@ import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.contentassist.ContextInformation;
 import org.eclipse.swt.graphics.Image;
 
-
-public abstract class ScriptCompletionProposal extends AbstractScriptCompletionProposal {
+public abstract class ScriptCompletionProposal extends
+		AbstractScriptCompletionProposal {
 
 	/**
-	 * Creates a new completion proposal. All fields are initialized based on the provided
-	 * information.
+	 * Creates a new completion proposal. All fields are initialized based on
+	 * the provided information.
 	 * 
-	 * @param replacementString the actual string to be inserted into the document
-	 * @param replacementOffset the offset of the text to be replaced
-	 * @param replacementLength the length of the text to be replaced
-	 * @param image the image to display for this proposal
-	 * @param displayString the string to be displayed for the proposal If set to <code>null</code>,
-	 *        the replacement string will be taken as display string.
+	 * @param replacementString
+	 *            the actual string to be inserted into the document
+	 * @param replacementOffset
+	 *            the offset of the text to be replaced
+	 * @param replacementLength
+	 *            the length of the text to be replaced
+	 * @param image
+	 *            the image to display for this proposal
+	 * @param displayString
+	 *            the string to be displayed for the proposal If set to
+	 *            <code>null</code>, the replacement string will be taken as
+	 *            display string.
 	 */
-	public ScriptCompletionProposal(String replacementString, int replacementOffset, int replacementLength, Image image, String displayString, int relevance) {
-		this(replacementString, replacementOffset, replacementLength, image, displayString, relevance, false);
+	public ScriptCompletionProposal(String replacementString,
+			int replacementOffset, int replacementLength, Image image,
+			String displayString, int relevance) {
+		this(replacementString, replacementOffset, replacementLength, image,
+				displayString, relevance, false);
 	}
-	
+
 	/**
-	 * Creates a new completion proposal. All fields are initialized based on the provided
-	 * information.
+	 * Creates a new completion proposal. All fields are initialized based on
+	 * the provided information.
 	 * 
-	 * @param replacementString the actual string to be inserted into the document
-	 * @param replacementOffset the offset of the text to be replaced
-	 * @param replacementLength the length of the text to be replaced
-	 * @param image the image to display for this proposal
-	 * @param displayString the string to be displayed for the proposal If set to <code>null</code>,
-	 *        the replacement string will be taken as display string.
-	 * @param relevance the relevance
-	 * @param indoc <code>true</code> for a javadoc proposal
-	 *
+	 * @param replacementString
+	 *            the actual string to be inserted into the document
+	 * @param replacementOffset
+	 *            the offset of the text to be replaced
+	 * @param replacementLength
+	 *            the length of the text to be replaced
+	 * @param image
+	 *            the image to display for this proposal
+	 * @param displayString
+	 *            the string to be displayed for the proposal If set to
+	 *            <code>null</code>, the replacement string will be taken as
+	 *            display string.
+	 * @param relevance
+	 *            the relevance
+	 * @param indoc
+	 *            <code>true</code> for a javadoc proposal
+	 * 
 	 */
-	public ScriptCompletionProposal(String replacementString, int replacementOffset, int replacementLength, Image image, String displayString, int relevance, boolean indoc) {
+	public ScriptCompletionProposal(String replacementString,
+			int replacementOffset, int replacementLength, Image image,
+			String displayString, int relevance, boolean indoc) {
 		Assert.isNotNull(replacementString);
 		Assert.isTrue(replacementOffset >= 0);
 		Assert.isTrue(replacementLength >= 0);
@@ -57,42 +76,46 @@ public abstract class ScriptCompletionProposal extends AbstractScriptCompletionP
 		setReplacementOffset(replacementOffset);
 		setReplacementLength(replacementLength);
 		setImage(image);
-		setDisplayString(displayString == null ? replacementString : displayString);
+		setDisplayString(displayString == null ? replacementString
+				: displayString);
 		setRelevance(relevance);
 		setCursorPosition(replacementString.length());
 		setInDoc(indoc);
 		setSortString(displayString == null ? replacementString : displayString);
-		
+
 		// TODO: temporary solution - should be removed
-		setContextInformation(new ContextInformation(getDisplayString(), getDisplayString()));
+		setContextInformation(new ContextInformation(getDisplayString(),
+				getDisplayString()));
 	}
 
 	protected boolean isValidPrefix(String prefix) {
-		String word= getDisplayString();
+		String word = getReplacementString();
 		if (isInScriptdoc()) {
 			int idx = word.indexOf("{@link "); //$NON-NLS-1$
-			if (idx==0) {
+			if (idx == 0) {
 				word = word.substring(7);
 			} else {
 				idx = word.indexOf("{@value "); //$NON-NLS-1$
-				if (idx==0) {
+				if (idx == 0) {
 					word = word.substring(8);
 				}
 			}
 		}
 		return isPrefix(prefix, word);
 	}
-	
-	public CharSequence getPrefixCompletionText(IDocument document, int completionOffset) {
-		String string= getReplacementString();
-		int pos= string.indexOf('(');
+
+	public CharSequence getPrefixCompletionText(IDocument document,
+			int completionOffset) {
+		String string = getReplacementString();
+		int pos = string.indexOf('(');
 		if (pos > 0)
 			return string.subSequence(0, pos);
 		else
 			return string;
 	}
 
-	protected void handleSmartTrigger(IDocument document, char trigger, int referenceOffset) throws BadLocationException {
+	protected void handleSmartTrigger(IDocument document, char trigger,
+			int referenceOffset) throws BadLocationException {
 		// TODO Auto-generated method stub
-	}	
+	}
 }
