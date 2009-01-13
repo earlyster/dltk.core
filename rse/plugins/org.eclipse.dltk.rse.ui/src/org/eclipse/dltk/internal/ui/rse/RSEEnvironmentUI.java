941a18c23e1296af50f508f5b2f1e1d2f02ad899
apanchenk <apanchenk>
2009-01-13 11:45:14 +0000

    "String selectFolder(Shell shell, String initialFolder)" method added

--- rse/plugins/org.eclipse.dltk.rse.ui/src/org/eclipse/dltk/internal/ui/rse/RSEEnvironmentUI.java
+++ rse/plugins/org.eclipse.dltk.rse.ui/src/org/eclipse/dltk/internal/ui/rse/RSEEnvironmentUI.java
@@ -1,9 +1,16 @@
 package org.eclipse.dltk.internal.ui.rse;
 
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.dltk.core.internal.rse.DLTKRSEPlugin;
 import org.eclipse.dltk.core.internal.rse.RSEEnvironment;
 import org.eclipse.dltk.ui.environment.IEnvironmentUI;
+import org.eclipse.jface.window.Window;
 import org.eclipse.rse.files.ui.dialogs.SystemRemoteFileDialog;
 import org.eclipse.rse.files.ui.dialogs.SystemRemoteFolderDialog;
+import org.eclipse.rse.services.clientserver.messages.SystemMessageException;
+import org.eclipse.rse.subsystems.files.core.model.RemoteFileUtility;
+import org.eclipse.rse.subsystems.files.core.subsystems.IRemoteFile;
+import org.eclipse.rse.subsystems.files.core.subsystems.IRemoteFileSubSystem;
 import org.eclipse.rse.subsystems.files.core.subsystems.RemoteFile;
 import org.eclipse.swt.widgets.Shell;
 
@@ -15,9 +22,28 @@ public class RSEEnvironmentUI implements IEnvironmentUI {
 	}
 
 	public String selectFolder(Shell shell) {
+		return selectFolder(shell, null);
+	}
+
+	public String selectFolder(Shell shell, String initialFolder) {
 		SystemRemoteFolderDialog dialog = new SystemRemoteFolderDialog(shell);
 		dialog.setDefaultSystemConnection(this.environment.getHost(), true);
-		if (dialog.open() == SystemRemoteFolderDialog.OK) {
+		if (initialFolder != null) {
+			final IRemoteFileSubSystem fs = RemoteFileUtility
+					.getFileSubSystem(environment.getHost());
+			if (fs != null) {
+				try {
+					final IRemoteFile remoteFile = fs.getRemoteFileObject(
+							initialFolder, new NullProgressMonitor());
+					if (remoteFile != null) {
+						dialog.setPreSelection(remoteFile);
+					}
+				} catch (SystemMessageException e) {
+					DLTKRSEPlugin.log(e);
+				}
+			}
+		}
+		if (dialog.open() == Window.OK) {
 			Object selectedObject = dialog.getSelectedObject();
 			if (selectedObject instanceof RemoteFile) {
 				RemoteFile file = (RemoteFile) selectedObject;
@@ -30,7 +56,7 @@ public class RSEEnvironmentUI implements IEnvironmentUI {
 	public String selectFile(Shell shell, int executable2) {
 		SystemRemoteFileDialog dialog = new SystemRemoteFileDialog(shell);
 		dialog.setDefaultSystemConnection(this.environment.getHost(), true);
-		if( dialog.open() == SystemRemoteFileDialog.OK ) {
+		if (dialog.open() == Window.OK) {
 			Object selectedObject = dialog.getSelectedObject();
 			if (selectedObject instanceof RemoteFile) {
 				RemoteFile file = (RemoteFile) selectedObject;
