d07bfa81f6b42ea8bb780b338cd0d57522f13f7a
apanchenk <apanchenk>
2009-01-21 07:59:28 +0000

    restore getTreeViewer() method

--- core/tests/org.eclipse.dltk.ui.tests/src/org/eclipse/dltk/ui/tests/navigator/scriptexplorer/MockPluginView.java
+++ core/tests/org.eclipse.dltk.ui.tests/src/org/eclipse/dltk/ui/tests/navigator/scriptexplorer/MockPluginView.java
@@ -91,6 +91,11 @@ public class MockPluginView extends ScriptExplorerPart {
 	public void setFocus() {
 	}
 	
+	public TreeViewer getTreeViewer(){
+		return fViewer;
+	}
+	
+
 	protected IModelElement findElementToSelect(IModelElement je) {
 		return null;
 	}
