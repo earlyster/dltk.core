2da3055e6d9da350d2715df1f1ba5c318e6a91fe
apanchenk <apanchenk>
2009-02-16 09:36:42 +0000

    re-commit last changes

--- core/plugins/org.eclipse.dltk.core/utils/org/eclipse/dltk/utils/PlatformFileUtils.java
+++ core/plugins/org.eclipse.dltk.core/utils/org/eclipse/dltk/utils/PlatformFileUtils.java
@@ -2,38 +2,31 @@ package org.eclipse.dltk.utils;
 
 import java.io.File;
 import java.io.IOException;
-import java.security.InvalidParameterException;
 
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.dltk.core.DLTKCore;
-import org.eclipse.dltk.core.environment.EnvironmentManager;
 import org.eclipse.dltk.core.environment.IEnvironment;
 import org.eclipse.dltk.core.environment.IFileHandle;
-import org.eclipse.dltk.core.internal.environment.LocalEnvironment;
 import org.eclipse.osgi.service.datalocation.Location;
 
 public class PlatformFileUtils {
 	public static IFileHandle findAbsoluteOrEclipseRelativeFile(
 			IEnvironment env, IPath path) {
 		if (path == null || path.isEmpty()) {
-			throw new InvalidParameterException(Messages.PlatformFileUtils_pathMustNotBeEmpty);
+			throw new IllegalArgumentException(
+					Messages.PlatformFileUtils_pathMustNotBeEmpty);
 		}
 		IFileHandle file = env.getFile(path);
-		if (!env.getId().equals(LocalEnvironment.ENVIRONMENT_ID)) {
-			return file;
-		}
-		if (EnvironmentManager.isLocal(env) && !file.exists()
-				&& !path.isAbsolute()) {
-			String loc;
+		if (env.isLocal() && !file.exists() && !path.isAbsolute()) {
 			Location location = Platform.getInstallLocation();
 			if (location != null) {
 				try {
-					loc = FileLocator.resolve(location.getURL()).getPath();
-					IFileHandle nfile = env.getFile(new Path(loc
-							+ env.getSeparator() + path.toOSString()));
+					String loc = FileLocator.resolve(location.getURL())
+							.getPath();
+					IFileHandle nfile = env.getFile(new Path(loc).append(path));
 					if (nfile.exists()) {
 						return nfile;
 					}
@@ -47,9 +40,9 @@ public class PlatformFileUtils {
 			location = Platform.getInstanceLocation();
 			if (location != null) {
 				try {
-					loc = FileLocator.resolve(location.getURL()).getPath();
-					IFileHandle nfile = env.getFile(new Path(loc
-							+ env.getSeparator() + path.toOSString()));
+					String loc = FileLocator.resolve(location.getURL())
+							.getPath();
+					IFileHandle nfile = env.getFile(new Path(loc).append(path));
 					if (nfile.exists()) {
 						return nfile;
 					}
