f0ef2747d18ecb508e45cee5e8b1c7bd11d36c50
apanchenk <apanchenk>
2009-03-18 08:40:35 +0000

    update to the EnvironmentManager changes

--- rse/plugins/org.eclipse.dltk.rse.ui/src/org/eclipse/dltk/internal/ui/rse/RemoteProjectLabelDecorator.java
+++ rse/plugins/org.eclipse.dltk.rse.ui/src/org/eclipse/dltk/internal/ui/rse/RemoteProjectLabelDecorator.java
@@ -14,8 +14,6 @@ package org.eclipse.dltk.internal.ui.rse;
 import java.net.URI;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.dltk.core.DLTKCore;
 import org.eclipse.dltk.core.environment.EnvironmentManager;
 import org.eclipse.dltk.core.environment.IEnvironment;
 import org.eclipse.dltk.core.internal.rse.RSEEnvironment;
@@ -29,9 +27,6 @@ import org.eclipse.jface.viewers.ILightweightLabelDecorator;
  */
 public class RemoteProjectLabelDecorator extends BaseLabelProvider implements
 		ILightweightLabelDecorator {
-
-	private static final String DECORATION_BEGIN = " ("; //$NON-NLS-1$
-	private static final String DECORATION_END = ")"; //$NON-NLS-1$
 
 	public void decorate(Object element, IDecoration decoration) {
 		if (element instanceof IProject) {
@@ -49,30 +44,27 @@ public class RemoteProjectLabelDecorator extends BaseLabelProvider implements
 		if (uri != null
 				&& RSEEnvironmentProvider.RSE_SCHEME.equalsIgnoreCase(uri
 						.getScheme()) && uri.getHost() != null) {
-			decoration.addSuffix(DECORATION_BEGIN + uri.getHost().toLowerCase()
-					+ DECORATION_END);
+			addHostNameSuffix(decoration, uri.getHost());
 		} else if (project.isOpen()) {
-			try {
-				final String envId = project
-						.getPersistentProperty(EnvironmentManager.PROJECT_ENVIRONMENT);
-				if (envId != null) {
-					final IEnvironment environment = EnvironmentManager
-							.getEnvironmentById(envId);
-					if (environment instanceof RSEEnvironment) {
-						final String hostName = ((RSEEnvironment) environment)
-								.getHost().getHostName();
-						if (hostName != null) {
-							decoration.addSuffix(DECORATION_BEGIN
-									+ hostName.toLowerCase() + DECORATION_END);
-						}
+			final String envId = EnvironmentManager.getEnvironmentId(project,
+					false);
+			if (envId != null) {
+				final IEnvironment environment = EnvironmentManager
+						.getEnvironmentById(envId);
+				if (environment instanceof RSEEnvironment) {
+					final String hostName = ((RSEEnvironment) environment)
+							.getHost().getHostName();
+					if (hostName != null) {
+						addHostNameSuffix(decoration, hostName);
 					}
-				}
-			} catch (CoreException e) {
-				if (DLTKCore.DEBUG) {
-					e.printStackTrace();
 				}
 			}
 		}
+	}
+
+	private static void addHostNameSuffix(IDecoration decoration,
+			final String hostName) {
+		decoration.addSuffix(" (" + hostName.toLowerCase() + ")"); //$NON-NLS-1$ //$NON-NLS-2$
 	}
 
 }
