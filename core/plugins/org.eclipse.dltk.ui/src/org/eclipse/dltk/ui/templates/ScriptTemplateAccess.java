717306b5014e21104ce32cbd117c56002f1538e4
apanchenk <apanchenk>
2009-04-01 16:58:54 +0000

    createContextTypeRegistry() method extracted

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/templates/ScriptTemplateAccess.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/templates/ScriptTemplateAccess.java
@@ -1,50 +1,57 @@
 package org.eclipse.dltk.ui.templates;
 
 import java.io.IOException;
-import java.text.MessageFormat;
 
 import org.eclipse.dltk.ui.DLTKUIPlugin;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.text.templates.ContextTypeRegistry;
 import org.eclipse.jface.text.templates.persistence.TemplateStore;
+import org.eclipse.osgi.util.NLS;
 import org.eclipse.ui.editors.text.templates.ContributionContextTypeRegistry;
 import org.eclipse.ui.editors.text.templates.ContributionTemplateStore;
 
 public abstract class ScriptTemplateAccess {
 
-	private ContributionContextTypeRegistry fRegistry;
+	private ContextTypeRegistry fRegistry;
 	private TemplateStore fStore;
 
-	public TemplateStore getTemplateStore()
-	{
-		if (fStore == null) 
-		{
+	public TemplateStore getTemplateStore() {
+		if (fStore == null) {
 			fStore = new ContributionTemplateStore(getContextTypeRegistry(),
 					getPreferenceStore(), getCustomTemplatesKey());
 			loadTemplates();
 		}
-		
 		return fStore;
 	}
-	
+
 	public ContextTypeRegistry getContextTypeRegistry() {
 		if (fRegistry == null) {
-			fRegistry = new ContributionContextTypeRegistry();
-			fRegistry.addContextType(getContextTypeId());
+			fRegistry = createContextTypeRegistry();
 		}
-		
 		return fRegistry;
 	}
-	
+
+	protected ContextTypeRegistry createContextTypeRegistry() {
+		final ContributionContextTypeRegistry registry = new ContributionContextTypeRegistry();
+		registry.addContextType(getContextTypeId());
+		return registry;
+	}
+
 	protected abstract String getContextTypeId();
+
 	protected abstract String getCustomTemplatesKey();
+
 	protected abstract IPreferenceStore getPreferenceStore();
-	
+
 	private void loadTemplates() {
 		try {
 			fStore.load();
 		} catch (IOException e) {
-			DLTKUIPlugin.logErrorMessage(MessageFormat.format(TemplateMessages.ScriptTemplateAccess_unableToLoadTemplateStore, new Object[] { e }));
+			final String msg = NLS
+					.bind(
+							TemplateMessages.ScriptTemplateAccess_unableToLoadTemplateStore,
+							e);
+			DLTKUIPlugin.logErrorMessage(msg, e);
 		}
 	}
 }
