4da731fc90098513b45715415e22f5d15737f31d
apanchenk <apanchenk>
2009-05-15 19:23:01 +0000

    formatting

--- rse/plugins/org.eclipse.dltk.rse.ui/src/org/eclipse/dltk/rse/ui/Activator.java
+++ rse/plugins/org.eclipse.dltk.rse.ui/src/org/eclipse/dltk/rse/ui/Activator.java
@@ -10,11 +10,11 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.dltk.rse.ui";
+	public static final String PLUGIN_ID = "org.eclipse.dltk.rse.ui"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
-	
+
 	/**
 	 * The constructor
 	 */
@@ -22,8 +22,7 @@ public class Activator extends AbstractUIPlugin {
 	}
 
 	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * @see AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
@@ -32,8 +31,7 @@ public class Activator extends AbstractUIPlugin {
 	}
 
 	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * @see AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
@@ -42,7 +40,7 @@ public class Activator extends AbstractUIPlugin {
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
