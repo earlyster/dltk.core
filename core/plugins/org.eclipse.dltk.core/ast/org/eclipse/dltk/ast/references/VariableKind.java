52e74f6295e89e304956326d3dcbb5838ce1ca38
apanchenk <apanchenk>
2009-06-11 15:00:56 +0000

    simplify implementation

--- core/plugins/org.eclipse.dltk.core/ast/org/eclipse/dltk/ast/references/VariableKind.java
+++ core/plugins/org.eclipse.dltk.core/ast/org/eclipse/dltk/ast/references/VariableKind.java
@@ -10,96 +10,53 @@
 package org.eclipse.dltk.ast.references;
 
 public interface VariableKind {
-	
+
 	public static final int FIRST_VARIABLE_ID = 0;
-	
+
 	int getId();
-	
-	public class Unknown implements VariableKind {
-		
-		public static final int ID = FIRST_VARIABLE_ID + 0;
 
-		public int getId() {
-			return ID;
-		}
-	}
+	public static class Implementation implements VariableKind {
 
-	public class Local implements VariableKind {
-		
-		public static final int ID = FIRST_VARIABLE_ID + 1;
+		private final int id;
 
-		public int getId() {
-			return ID;
+		public Implementation(int id) {
+			this.id = id;
 		}
 
-	}
-
-	public class Global implements VariableKind {
-		
-		public static final int ID = FIRST_VARIABLE_ID + 2;
-
 		public int getId() {
-			return ID;
+			return id;
+		}
+
+		@Override
+		public String toString() {
+			return "VariableKind-" + id; //$NON-NLS-1$
 		}
 
 	}
 
-	public class Instance implements VariableKind {
-		
-		public static final int ID = FIRST_VARIABLE_ID + 3;
-
-		public int getId() {
-			return ID;
-		}
-
-	}
-
-	public class Class implements VariableKind {
-		
-		public static final int ID = FIRST_VARIABLE_ID + 4;
-
-		public int getId() {
-			return ID;
-		}
-		
-	}
-	
-	public class Mixin implements VariableKind {
-		
-		public static final int ID = FIRST_VARIABLE_ID + 5;
-
-		public int getId() {
-			return ID;
-		}
-		
-	}
-	
-	public class Argument implements VariableKind {
-		
-		public static final int ID = FIRST_VARIABLE_ID + 6;
-
-		public int getId() {
-			return ID;
-		}
-		
-	}
-	
 	public static final int LAST_CORE_VARIABLE_ID = FIRST_VARIABLE_ID + 50;
-	
+
 	public static final int LAST_VARIABLE_ID = LAST_CORE_VARIABLE_ID + 50;
 
-	public static final VariableKind UNKNOWN = new Unknown();
-	
-	public static final VariableKind LOCAL = new Local();
+	public static final VariableKind UNKNOWN = new Implementation(
+			FIRST_VARIABLE_ID + 0);
 
-	public static final VariableKind GLOBAL = new Global();
+	public static final VariableKind LOCAL = new Implementation(
+			FIRST_VARIABLE_ID + 1);
 
-	public static final VariableKind INSTANCE = new Instance();
+	public static final VariableKind GLOBAL = new Implementation(
+			FIRST_VARIABLE_ID + 2);
 
-	public static final VariableKind CLASS = new Class();
-	
-	public static final VariableKind MIXIN = new Mixin();
-	
-	public static final VariableKind ARGUMENT = new Argument();
+	public static final VariableKind INSTANCE = new Implementation(
+			FIRST_VARIABLE_ID + 3);
+
+	public static final VariableKind CLASS = new Implementation(
+			FIRST_VARIABLE_ID + 4);
+
+	public static final VariableKind MIXIN = new Implementation(
+			FIRST_VARIABLE_ID + 5);
+
+	public static final VariableKind ARGUMENT = new Implementation(
+			FIRST_VARIABLE_ID + 6);
 
 }
