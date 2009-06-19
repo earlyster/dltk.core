b870ea14c6b709711d175fa86b87a2e6086f5bb5
apanchenk <apanchenk>
2009-06-19 05:44:23 +0000

    restore all inner classes

--- core/plugins/org.eclipse.dltk.core/ast/org/eclipse/dltk/ast/references/VariableKind.java
+++ core/plugins/org.eclipse.dltk.core/ast/org/eclipse/dltk/ast/references/VariableKind.java
@@ -35,6 +35,17 @@ public interface VariableKind {
 	}
 
 	@Deprecated
+	public class Unknown extends Implementation {
+
+		public static final int ID = FIRST_VARIABLE_ID + 0;
+
+		public Unknown() {
+			super(ID);
+		}
+
+	}
+
+	@Deprecated
 	public class Local extends Implementation {
 
 		public static final int ID = FIRST_VARIABLE_ID + 1;
@@ -56,27 +67,66 @@ public interface VariableKind {
 
 	}
 
+	@Deprecated
+	public class Instance extends Implementation {
+
+		public static final int ID = FIRST_VARIABLE_ID + 3;
+
+		public Instance() {
+			super(ID);
+		}
+
+	}
+
+	@Deprecated
+	public class Class extends Implementation {
+
+		public static final int ID = FIRST_VARIABLE_ID + 4;
+
+		public Class() {
+			super(ID);
+		}
+
+	}
+
+	@Deprecated
+	public class Mixin extends Implementation {
+
+		public static final int ID = FIRST_VARIABLE_ID + 5;
+
+		public Mixin() {
+			super(ID);
+		}
+
+	}
+
+	@Deprecated
+	public class Argument extends Implementation {
+
+		public static final int ID = FIRST_VARIABLE_ID + 6;
+
+		public Argument() {
+			super(ID);
+		}
+
+	}
+
 	public static final int LAST_CORE_VARIABLE_ID = FIRST_VARIABLE_ID + 50;
 
 	public static final int LAST_VARIABLE_ID = LAST_CORE_VARIABLE_ID + 50;
 
-	public static final VariableKind UNKNOWN = new Implementation(
-			FIRST_VARIABLE_ID + 0);
+	public static final VariableKind UNKNOWN = new Unknown();
 
 	public static final VariableKind LOCAL = new Local();
 
 	public static final VariableKind GLOBAL = new Global();
 
-	public static final VariableKind INSTANCE = new Implementation(
-			FIRST_VARIABLE_ID + 3);
+	public static final VariableKind INSTANCE = new Instance();
 
-	public static final VariableKind CLASS = new Implementation(
-			FIRST_VARIABLE_ID + 4);
+	public static final VariableKind CLASS = new Class();
 
-	public static final VariableKind MIXIN = new Implementation(
-			FIRST_VARIABLE_ID + 5);
+	public static final VariableKind MIXIN = new Mixin();
 
-	public static final VariableKind ARGUMENT = new Implementation(
-			FIRST_VARIABLE_ID + 6);
+	public static final VariableKind ARGUMENT = new Argument();
 
 }
