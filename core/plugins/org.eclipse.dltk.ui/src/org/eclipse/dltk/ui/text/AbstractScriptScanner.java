5e979c81750351fa3156f6d62417809da3ee3669
apanchenk <apanchenk>
2009-06-23 05:34:53 +0000

    generics/@Override

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/text/AbstractScriptScanner.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/text/AbstractScriptScanner.java
@@ -183,6 +183,7 @@ public abstract class AbstractScriptScanner extends BufferedRuleBasedScanner {
 		return colorKey + PreferenceConstants.EDITOR_UNDERLINE_SUFFIX;
 	}
 
+	@Override
 	public IToken nextToken() {
 		if (fNeedsLazyColorLoading)
 			resolveProxyAttributes();
