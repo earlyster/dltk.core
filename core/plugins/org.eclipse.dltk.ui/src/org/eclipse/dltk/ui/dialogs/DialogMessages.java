07f02be5a114fc9d321036b62f0125f019fc17d0
apanchenk <apanchenk>
2009-07-06 14:15:41 +0000

    MultipleInputDialog moved to dltk.ui

---
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/dialogs/DialogMessages.java
@@ -0,0 +1,15 @@
+package org.eclipse.dltk.ui.dialogs;
+
+import org.eclipse.osgi.util.NLS;
+
+public class DialogMessages extends NLS {
+	private static final String BUNDLE_NAME = "org.eclipse.dltk.ui.dialogs.dialogMessages"; //$NON-NLS-1$
+	public static String MultipleInputDialog_ignore;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, DialogMessages.class);
+	}
+
+	private DialogMessages() {
+	}
+}
