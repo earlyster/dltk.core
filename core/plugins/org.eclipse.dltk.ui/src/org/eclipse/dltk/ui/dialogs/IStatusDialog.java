6a3091e985a4afebcc4b8492504a92c4189ce31c
apanchenk <apanchenk>
2009-07-06 14:19:22 +0000

    GlobalVariableBlock moved to dltk.tcl.ui

---
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/dialogs/IStatusDialog.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2009 xored software, Inc.  
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html  
+ *
+ * Contributors:
+ *     xored software, Inc. - initial API and Implementation (Alex Panchenko)
+ *******************************************************************************/
+package org.eclipse.dltk.ui.dialogs;
+
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Shell;
+
+public interface IStatusDialog {
+
+	/**
+	 * @return
+	 */
+	Shell getShell();
+
+	/**
+	 * @param button
+	 */
+	void setButtonLayoutData(Button button);
+
+	/**
+	 * 
+	 */
+	void updateStatusLine();
+
+}
