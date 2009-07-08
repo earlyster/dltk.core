89dc2f869fde4d2a6588482aa4d4debbff8d0604
mspector <mspector>
2009-07-08 08:24:51 +0000

    fixed messages BUNDLE_NAME

--- core/plugins/org.eclipse.dltk.core/search/org/eclipse/dltk/core/search/indexing/core/Messages.java
+++ core/plugins/org.eclipse.dltk.core/search/org/eclipse/dltk/core/search/indexing/core/Messages.java
@@ -3,7 +3,7 @@ package org.eclipse.dltk.core.search.indexing.core;
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
-	private static final String BUNDLE_NAME = "org.eclipse.dltk.internal.core.mixin.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "org.eclipse.dltk.core.search.indexing.core.messages"; //$NON-NLS-1$
 	public static String MixinBuilder_buildingRuntimeModelFor;
 	public static String MixinBuilder_buildingRuntimeModelFor2;
 	public static String MixinBuilder_savingIndexFor;
