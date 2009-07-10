b5cdcc38de747f48f16b770aebeb2baca107d24d
apanchenk <apanchenk>
2009-07-10 09:11:51 +0000

    Import declaration filter

---
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/filters/ImportDeclarationFilter.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ 
+ *******************************************************************************/
+package org.eclipse.dltk.internal.ui.filters;
+
+import org.eclipse.dltk.core.IImportContainer;
+import org.eclipse.dltk.core.IImportDeclaration;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+
+/**
+ * Filters out all compilation units and class files elements.
+ */
+public class ImportDeclarationFilter extends ViewerFilter {
+
+	/**
+	 * Returns the result of this filter, when applied to the given inputs.
+	 * 
+	 * @return Returns true if element should be included in filtered set
+	 */
+	public boolean select(Viewer viewer, Object parent, Object element) {
+		if (element instanceof IImportContainer) {
+			return false;
+		}
+		if (element instanceof IImportDeclaration) {
+			return false;
+		}
+		return true;
+	}
+}
