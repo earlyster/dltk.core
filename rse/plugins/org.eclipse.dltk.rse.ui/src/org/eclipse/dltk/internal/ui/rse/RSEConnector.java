0bad2be239d3d6740283abed5c951a61a142db14
asobolev <asobolev>
2009-07-28 04:14:00 +0000

    * Connection processing update for case then display loop are not yet started and we try to correct from display thread.

--- rse/plugins/org.eclipse.dltk.rse.ui/src/org/eclipse/dltk/internal/ui/rse/RSEConnector.java
+++ rse/plugins/org.eclipse.dltk.rse.ui/src/org/eclipse/dltk/internal/ui/rse/RSEConnector.java
@@ -8,23 +8,43 @@ import org.eclipse.rse.core.model.IHost;
 import org.eclipse.rse.core.subsystems.ISubSystem;
 import org.eclipse.rse.subsystems.files.core.subsystems.IRemoteFileSubSystem;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PlatformUI;
 
 /**
  * @since 2.0
  */
 public class RSEConnector implements IConnector {
 	private static boolean running = true;
-	private Thread processingThread = new Thread("RSE connection resolver") {
-		public void run() {
-			while (running) {
-				IHost host = RSEConnectionQueryManager.getInstance()
-						.getNextHost(true);
-				connect(host);
-				RSEConnectionQueryManager.getInstance()
-						.markHostAsFinished(host);
+	private Thread processingThread = null;
+
+	private Thread createPoprocessingThread() {
+		return new Thread("RSE connection resolver") {
+			public void run() {
+				while (running) {
+					if (RSEConnectionQueryManager.getInstance().hasHosts()) {
+						Display display = PlatformUI.getWorkbench()
+								.getDisplay();
+						display.syncExec(new Runnable() {
+							public void run() {
+								IHost host = RSEConnectionQueryManager
+										.getInstance().getNextHost(false);
+								if (host != null) {
+									connect(host);
+									RSEConnectionQueryManager.getInstance()
+											.markHostAsFinished(host);
+								}
+							}
+						});
+					}
+					try {
+						Thread.sleep(200);
+					} catch (InterruptedException e) {
+						DLTKRSEPlugin.log(e);
+					}
+				}
 			}
-		}
-	};
+		};
+	}
 
 	private void connect(IHost host) {
 		ISubSystem[] subSystems = host.getSubSystems();
@@ -53,13 +73,24 @@ public class RSEConnector implements IConnector {
 	}
 
 	public void register() {
-		processingThread.start();
+		if (processingThread == null) {
+			processingThread = createPoprocessingThread();
+			processingThread.start();
+		}
 	}
 
 	public void runDisplayRunnables() {
+		// We need to interrupt processingThread if it is no executing.
 		Display current = Display.getCurrent();
 		while (RSEConnectionQueryManager.getInstance().hasHosts()
-				&& current.readAndDispatch()) {
+				&& current.readAndDispatch() && !current.isDisposed()) {
+			IHost host = RSEConnectionQueryManager.getInstance().getNextHost(
+					false);
+			if (host != null) {
+				connect(host);
+				RSEConnectionQueryManager.getInstance()
+						.markHostAsFinished(host);
+			}
 			current.sleep();
 		}
 	}
