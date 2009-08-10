141c078fc58856df9330a5876e300ecdc752c897
apanchenk <apanchenk>
2009-08-10 05:46:00 +0000

    formatting

--- core/plugins/org.eclipse.dltk.ui/core refactoring/org/eclipse/dltk/internal/corext/util/TypeInfoFilter.java
+++ core/plugins/org.eclipse.dltk.ui/core refactoring/org/eclipse/dltk/internal/corext/util/TypeInfoFilter.java
@@ -21,129 +21,133 @@ import org.eclipse.dltk.internal.ui.util.StringMatcher;
 import org.eclipse.dltk.ui.dialogs.ITypeInfoFilterExtension;
 
 public class TypeInfoFilter {
-	
-	private static final String PACKAGE_DELIM = ".";
+
+	private static final String PACKAGE_DELIM = "."; //$NON-NLS-1$
 
 	private static class PatternMatcher {
-		
+
 		private String fPattern;
 		private int fMatchKind;
 		private StringMatcher fStringMatcher;
 
-		private static final char END_SYMBOL= '<';
-		private static final char ANY_STRING= '*';
-		private static final char BLANK= ' ';
-		
+		private static final char END_SYMBOL = '<';
+		private static final char ANY_STRING = '*';
+		private static final char BLANK = ' ';
+
 		public PatternMatcher(String pattern, boolean ignoreCase) {
-			this(pattern, SearchPattern.R_EXACT_MATCH | SearchPattern.R_PREFIX_MATCH |
-				SearchPattern.R_PATTERN_MATCH | SearchPattern.R_CAMELCASE_MATCH);
+			this(pattern, SearchPattern.R_EXACT_MATCH
+					| SearchPattern.R_PREFIX_MATCH
+					| SearchPattern.R_PATTERN_MATCH
+					| SearchPattern.R_CAMELCASE_MATCH);
 		}
-		
+
 		public PatternMatcher(String pattern, int allowedModes) {
 			initializePatternAndMatchKind(pattern);
-			fMatchKind= fMatchKind & allowedModes;
+			fMatchKind = fMatchKind & allowedModes;
 			if (fMatchKind == SearchPattern.R_PATTERN_MATCH) {
-				fStringMatcher= new StringMatcher(fPattern, true, false);
+				fStringMatcher = new StringMatcher(fPattern, true, false);
 			}
 		}
-		
+
 		public String getPattern() {
 			return fPattern;
 		}
-		
+
 		public int getMatchKind() {
 			return fMatchKind;
 		}
-		
+
 		public boolean matches(String text) {
 			switch (fMatchKind) {
-				case SearchPattern.R_PATTERN_MATCH:
-					return fStringMatcher.match(text);
-				case SearchPattern.R_EXACT_MATCH:
-					return fPattern.equalsIgnoreCase(text);
-				case SearchPattern.R_CAMELCASE_MATCH:
-					if (SearchPattern.camelCaseMatch(fPattern, text)) {
-						return true;
-					}
-					// fall through to prefix match if camel case failed (bug 137244)
-				default:
-					return Strings.startsWithIgnoreCase(text, fPattern);
+			case SearchPattern.R_PATTERN_MATCH:
+				return fStringMatcher.match(text);
+			case SearchPattern.R_EXACT_MATCH:
+				return fPattern.equalsIgnoreCase(text);
+			case SearchPattern.R_CAMELCASE_MATCH:
+				if (SearchPattern.camelCaseMatch(fPattern, text)) {
+					return true;
+				}
+				// fall through to prefix match if camel case failed (bug
+				// 137244)
+			default:
+				return Strings.startsWithIgnoreCase(text, fPattern);
 			}
 		}
-		
+
 		private void initializePatternAndMatchKind(String pattern) {
-			int length= pattern.length();
+			int length = pattern.length();
 			if (length == 0) {
-				fMatchKind= SearchPattern.R_EXACT_MATCH;
-				fPattern= pattern;
+				fMatchKind = SearchPattern.R_EXACT_MATCH;
+				fPattern = pattern;
 				return;
 			}
-			char last= pattern.charAt(length - 1);
-			
+			char last = pattern.charAt(length - 1);
+
 			if (pattern.indexOf('*') != -1 || pattern.indexOf('?') != -1) {
-				fMatchKind= SearchPattern.R_PATTERN_MATCH;
+				fMatchKind = SearchPattern.R_PATTERN_MATCH;
 				switch (last) {
-					case END_SYMBOL:
-						fPattern= pattern.substring(0, length - 1);
-						break;
-					case BLANK:
-						fPattern= pattern.trim();
-						break;
-					case ANY_STRING:
-						fPattern= pattern;
-						break;
-					default:
-						fPattern= pattern + ANY_STRING;
+				case END_SYMBOL:
+					fPattern = pattern.substring(0, length - 1);
+					break;
+				case BLANK:
+					fPattern = pattern.trim();
+					break;
+				case ANY_STRING:
+					fPattern = pattern;
+					break;
+				default:
+					fPattern = pattern + ANY_STRING;
 				}
 				return;
 			}
-			
+
 			if (last == END_SYMBOL) {
-				fMatchKind= SearchPattern.R_EXACT_MATCH;
-				fPattern= pattern.substring(0, length - 1);
+				fMatchKind = SearchPattern.R_EXACT_MATCH;
+				fPattern = pattern.substring(0, length - 1);
 				return;
 			}
-			
+
 			if (last == BLANK) {
-				fMatchKind= SearchPattern.R_EXACT_MATCH;
-				fPattern= pattern.trim();
+				fMatchKind = SearchPattern.R_EXACT_MATCH;
+				fPattern = pattern.trim();
 				return;
 			}
-			
+
 			if (SearchUtils.isCamelCasePattern(pattern)) {
-				fMatchKind= SearchPattern.R_CAMELCASE_MATCH;
-				fPattern= pattern;
+				fMatchKind = SearchPattern.R_CAMELCASE_MATCH;
+				fPattern = pattern;
 				return;
 			}
-			
-			fMatchKind= SearchPattern.R_PREFIX_MATCH;
-			fPattern= pattern;
-		}		
+
+			fMatchKind = SearchPattern.R_PREFIX_MATCH;
+			fPattern = pattern;
+		}
 	}
-	
+
 	private String fText;
 	private final IDLTKSearchScope fSearchScope;
 	private boolean fIsWorkspaceScope;
 	private int fElementKind;
 	private ITypeInfoFilterExtension fFilterExtension;
-	private TypeInfoRequestorAdapter fAdapter= new TypeInfoRequestorAdapter();
+	private TypeInfoRequestorAdapter fAdapter = new TypeInfoRequestorAdapter();
 
 	private PatternMatcher fPackageMatcher;
 	private PatternMatcher fNameMatcher;
-	
-	private static final int TYPE_MODIFIERS= 0;
-	
-	public TypeInfoFilter(String text, IDLTKSearchScope scope, int elementKind, ITypeInfoFilterExtension extension) {
-		fText= text;
-		fSearchScope= scope;
+
+	private static final int TYPE_MODIFIERS = 0;
+
+	public TypeInfoFilter(String text, IDLTKSearchScope scope, int elementKind,
+			ITypeInfoFilterExtension extension) {
+		fText = text;
+		fSearchScope = scope;
 		IDLTKLanguageToolkit toolkit = scope.getLanguageToolkit();
 		fIsWorkspaceScope = fSearchScope.equals(SearchEngine
 				.createWorkspaceScope(toolkit));
-		fElementKind= elementKind;
-		fFilterExtension= extension;
-		
+		fElementKind = elementKind;
+		fFilterExtension = extension;
+
 		String packageDelimiter = getPackageDelimiter(toolkit);
-		int index = text.lastIndexOf(packageDelimiter); //$NON-NLS-1$
+		int index = text.lastIndexOf(packageDelimiter);
 		if (index == -1) {
 			fNameMatcher = new PatternMatcher(text, true);
 		} else {
@@ -155,7 +159,7 @@ public class TypeInfoFilter {
 			fNameMatcher = new PatternMatcher(name, true);
 		}
 	}
-	
+
 	/*
 	 * Transforms o.e.j to o*.e*.j*
 	 */
@@ -189,9 +193,9 @@ public class TypeInfoFilter {
 	}
 
 	public boolean isSubFilter(String text) {
-		if (! fText.startsWith(text))
+		if (!fText.startsWith(text))
 			return false;
-		
+
 		return fText.indexOf('.', text.length()) == -1;
 	}
 
@@ -212,11 +216,11 @@ public class TypeInfoFilter {
 	public int getSearchFlags() {
 		return fNameMatcher.getMatchKind();
 	}
-	
+
 	public int getPackageFlags() {
 		if (fPackageMatcher == null)
 			return SearchPattern.R_EXACT_MATCH;
-		
+
 		return fPackageMatcher.getMatchKind();
 	}
 
@@ -230,9 +234,10 @@ public class TypeInfoFilter {
 			return false;
 		return matchesName(type);
 	}
-	
+
 	public boolean matchesHistoryElement(TypeNameMatch type) {
-		if (!(matchesPackage(type) && matchesModifiers(type) && matchesScope(type) && matchesFilterExtension(type)))
+		if (!(matchesPackage(type) && matchesModifiers(type)
+				&& matchesScope(type) && matchesFilterExtension(type)))
 			return false;
 		return matchesName(type);
 	}
@@ -243,7 +248,7 @@ public class TypeInfoFilter {
 		fAdapter.setMatch(type);
 		return fFilterExtension.select(fAdapter);
 	}
-	
+
 	private boolean matchesName(TypeNameMatch type) {
 		return fNameMatcher.matches(type.getSimpleTypeName());
 	}
@@ -259,14 +264,14 @@ public class TypeInfoFilter {
 			return true;
 		return fSearchScope.encloses(type.getType());
 	}
-	
+
 	private boolean matchesModifiers(TypeNameMatch type) {
 		if (fElementKind == IDLTKSearchConstants.TYPE)
 			return true;
-		int modifiers= type.getModifiers() & TYPE_MODIFIERS;
+		int modifiers = type.getModifiers() & TYPE_MODIFIERS;
 		switch (fElementKind) {
-			case IDLTKSearchConstants.TYPE:
-				return modifiers == 0;
+		case IDLTKSearchConstants.TYPE:
+			return modifiers == 0;
 		}
 		return false;
 	}
