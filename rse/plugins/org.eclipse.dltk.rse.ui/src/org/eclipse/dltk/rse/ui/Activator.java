3f28ee7ac01c954a0ed831db74f712923efff9a5
apanchenk <apanchenk>
2009-09-01 04:33:41 +0000

    to prevent deadlock initiated from workspace refresh: on shutdown close projects with linked files

--- rse/plugins/org.eclipse.dltk.rse.ui/src/org/eclipse/dltk/rse/ui/Activator.java
+++ rse/plugins/org.eclipse.dltk.rse.ui/src/org/eclipse/dltk/rse/ui/Activator.java
@@ -1,8 +1,21 @@
 package org.eclipse.dltk.rse.ui;
 
+import java.net.URI;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.dltk.core.DLTKCore;
+import org.eclipse.dltk.core.DLTKLanguageManager;
+import org.eclipse.dltk.core.ModelException;
+import org.eclipse.dltk.core.internal.rse.RSEEnvironmentProvider;
 import org.eclipse.dltk.internal.ui.rse.RSEConnectionMonitor;
 import org.eclipse.dltk.internal.ui.rse.RSEConnector;
 import org.eclipse.rse.ui.RSEUIPlugin;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchListener;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -26,16 +39,24 @@ public class Activator extends AbstractUIPlugin {
 	/*
 	 * @see AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
 		RSEUIPlugin.getDefault();
 		RSEConnectionMonitor.start();
+		try {
+			PlatformUI.getWorkbench().addWorkbenchListener(
+					new ShutdownCloseProjectsWithLinkedFiles());
+		} catch (IllegalStateException e) {
+			// IGNORE: workbench has not been created yet.
+		}
 	}
 
 	/*
 	 * @see AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		RSEConnector.stop();
@@ -51,4 +72,57 @@ public class Activator extends AbstractUIPlugin {
 		return plugin;
 	}
 
+	private static class ShutdownCloseProjectsWithLinkedFiles implements
+			IWorkbenchListener {
+		public void postShutdown(IWorkbench workbench) {
+			// empty
+		}
+
+		public boolean preShutdown(IWorkbench workbench, boolean forced) {
+			closeProjectsWithLinkedFiles();
+			return true;
+		}
+
+		/**
+		 * At the moment RSE calls Display.syncExec() to ask password and if
+		 * it's called from RefreshJob when workspace is locked and at the same
+		 * time UI thread wants to perform a workspace operation - deadlock
+		 * occurs.
+		 * 
+		 * @throws ModelException
+		 * @throws CoreException
+		 */
+		private void closeProjectsWithLinkedFiles() {
+			final IProject[] projects = ResourcesPlugin.getWorkspace()
+					.getRoot().getProjects();
+			for (IProject project : projects) {
+				if (project.isAccessible()
+						&& DLTKLanguageManager.hasScriptNature(project)) {
+					try {
+						if (shouldBeClosed(project)) {
+							project.close(null);
+						}
+					} catch (CoreException e) {
+						if (DLTKCore.DEBUG)
+							e.printStackTrace();
+					}
+				}
+			}
+		}
+
+		private boolean shouldBeClosed(IProject project) throws CoreException {
+			for (final IResource child : project.members()) {
+				if (child.isLinked()) {
+					final URI location = child.getLocationURI();
+					if (location != null
+							&& RSEEnvironmentProvider.RSE_SCHEME
+									.equalsIgnoreCase(location.getScheme())) {
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+
+	}
 }
