8393c614d2db60fcac905361074f69186e6981f2
apanchenk <apanchenk>
2009-09-04 11:40:31 +0000

    organize imports

--- rse/plugins/org.eclipse.dltk.ssh.core/src/org/eclipse/dltk/ssh/internal/core/SshFileHandle.java
+++ rse/plugins/org.eclipse.dltk.ssh.core/src/org/eclipse/dltk/ssh/internal/core/SshFileHandle.java
@@ -1,8 +1,5 @@
 package org.eclipse.dltk.ssh.internal.core;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.HashMap;
