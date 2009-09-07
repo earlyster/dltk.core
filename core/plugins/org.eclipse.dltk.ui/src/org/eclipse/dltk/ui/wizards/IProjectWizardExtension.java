a16f6efeaf030bed76e1318a67fde50f65a78b81
asobolev <asobolev>
2009-09-07 09:18:05 +0000

    * Tcl project wizard improvements

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/wizards/IProjectWizardExtension.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/wizards/IProjectWizardExtension.java
@@ -14,11 +14,17 @@ package org.eclipse.dltk.ui.wizards;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.dltk.core.environment.IEnvironment;
+import org.eclipse.dltk.ui.dialogs.IProjectTemplate;
 
 /**
  * @since 2.0
  */
 public interface IProjectWizardExtension {
+	interface IValidationRequestor {
+		void validate();
+	}
 
 	void createControls(IProjectWizardExtensionContext context);
 
@@ -29,5 +35,10 @@ public interface IProjectWizardExtension {
 	 */
 	void postConfigure(IProject project, IProgressMonitor monitor)
 			throws CoreException;
+
+	IStatus validate(IProject handle, IEnvironment environment,
+			IProjectTemplate template);
+
+	void setValidationRequestor(IValidationRequestor requestor);
 
 }
