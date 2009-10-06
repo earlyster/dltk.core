fdf6df730bdf6019548725bcb9e0cc5bb54cc806
apanchenk <apanchenk>
2009-10-06 05:58:49 +0000

    +ProblemsLabelDecorator

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/viewsupport/DecoratingModelLabelProvider.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/viewsupport/DecoratingModelLabelProvider.java
@@ -9,19 +9,19 @@
  *******************************************************************************/
 package org.eclipse.dltk.ui.viewsupport;
 
-import org.eclipse.dltk.core.DLTKCore;
+import org.eclipse.dltk.ui.ProblemsLabelDecorator;
 import org.eclipse.jface.viewers.DecoratingLabelProvider;
 import org.eclipse.jface.viewers.DecorationContext;
 import org.eclipse.jface.viewers.IColorProvider;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.ui.PlatformUI;
 
+public class DecoratingModelLabelProvider extends DecoratingLabelProvider
+		implements IColorProvider {
 
-public class DecoratingModelLabelProvider extends DecoratingLabelProvider implements IColorProvider {
-	
 	/**
-	 * Decorating label provider for DLTK. Combines a ScriptUILabelProvider
-	 * with problem and override indicuator with the workbench decorator (label
+	 * Decorating label provider for DLTK. Combines a ScriptUILabelProvider with
+	 * problem and override indicuator with the workbench decorator (label
 	 * decorator extension point).
 	 */
 	public DecoratingModelLabelProvider(ScriptUILabelProvider labelProvider) {
@@ -29,58 +29,54 @@ public class DecoratingModelLabelProvider extends DecoratingLabelProvider implem
 	}
 
 	/**
-	 * Decorating label provider for dltk. Combines a ScriptUILabelProvider
-	 * (if enabled with problem indicator) with the workbench
-	 * decorator (label decorator extension point).
+	 * Decorating label provider for dltk. Combines a ScriptUILabelProvider (if
+	 * enabled with problem indicator) with the workbench decorator (label
+	 * decorator extension point).
 	 */
-	public DecoratingModelLabelProvider(ScriptUILabelProvider labelProvider, boolean errorTick) {
+	public DecoratingModelLabelProvider(ScriptUILabelProvider labelProvider,
+			boolean errorTick) {
 		this(labelProvider, errorTick, true);
 	}
-	
+
 	/**
-	 * Decorating label provider for dltk. Combines a ScriptUILabelProvider
-	 * (if enabled with problem indicator) with the workbench
-	 * decorator (label decorator extension point).
+	 * Decorating label provider for dltk. Combines a ScriptUILabelProvider (if
+	 * enabled with problem indicator) with the workbench decorator (label
+	 * decorator extension point).
 	 */
-	public DecoratingModelLabelProvider(ScriptUILabelProvider labelProvider, boolean errorTick, boolean flatPackageMode) {
-		super(labelProvider, PlatformUI.getWorkbench().getDecoratorManager().getLabelDecorator());
-		
-		if (DLTKCore.DEBUG) {
-			System.err.println("TODO: DecoratingModelLabelProvider: Add ProblemsLabelDecorator"); //$NON-NLS-1$
+	public DecoratingModelLabelProvider(ScriptUILabelProvider labelProvider,
+			boolean errorTick, boolean flatPackageMode) {
+		super(labelProvider, PlatformUI.getWorkbench().getDecoratorManager()
+				.getLabelDecorator());
+
+		if (errorTick) {
+			labelProvider.addLabelDecorator(new ProblemsLabelDecorator(null));
 		}
-//		if (errorTick) {
-//			labelProvider.addLabelDecorator(new ProblemsLabelDecorator(null));
-//		}
 		setFlatPackageMode(flatPackageMode);
 	}
-	
+
 	/**
-	 * Tells the label decorator if the view presents packages flat or hierarchical.
-	 * @param enable If set, packages are presented in flat mode.
+	 * Tells the label decorator if the view presents packages flat or
+	 * hierarchical.
+	 * 
+	 * @param enable
+	 *            If set, packages are presented in flat mode.
 	 */
 	public void setFlatPackageMode(boolean enable) {
 		if (enable) {
 			setDecorationContext(DecorationContext.DEFAULT_CONTEXT);
-		} 
-		if (DLTKCore.DEBUG) {
-			System.err.println("TODO: DecoratingModelLabelProvider: Add HierarchicalDecorationContext"); //$NON-NLS-1$
+		} else {
+			setDecorationContext(DecorationContext.DEFAULT_CONTEXT);
+			// TODO setDecorationContext(HierarchicalDecorationContext.CONTEXT);
 		}
-//		else {
-//			setDecorationContext(HierarchicalDecorationContext.CONTEXT);
-//		}
 	}
-	
-	/* (non-Javadoc)
-	 * @see org.eclipse.jface.viewers.IColorProvider#getForeground(java.lang.Object)
-	 */
+
+	@Override
 	public Color getForeground(Object element) {
 		// label provider is a ScriptUILabelProvider
 		return ((IColorProvider) getLabelProvider()).getForeground(element);
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.jface.viewers.IColorProvider#getBackground(java.lang.Object)
-	 */
+	@Override
 	public Color getBackground(Object element) {
 		// label provider is a ScriptUILabelProvider
 		return ((IColorProvider) getLabelProvider()).getBackground(element);
