fa4e8b0d64146ae78698c5614b2135b287d6388b
apanchenk <apanchenk>
2009-10-12 11:35:06 +0000

    formatting

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/wizards/IBuildpathContainerPageExtension2.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/wizards/IBuildpathContainerPageExtension2.java
@@ -11,25 +11,25 @@ package org.eclipse.dltk.internal.ui.wizards;
 
 import org.eclipse.dltk.core.IBuildpathEntry;
 
-
 /**
- * Buildpath container pages that implement {@link IBuildpathContainerPage} can 
+ * Buildpath container pages that implement {@link IBuildpathContainerPage} can
  * optionally implement {@link IBuildpathContainerPageExtension2} to return more
- * than one element when creating new containers. If implemented, the method {@link #getNewContainers()}
- * is used instead of the method {@link IBuildpathContainerPage#getSelection() } to get the
- * newly selected containers. {@link IBuildpathContainerPage#getSelection() } is still used
- * for edited elements.
- *
-	 *
+ * than one element when creating new containers. If implemented, the method
+ * {@link #getNewContainers()} is used instead of the method
+ * {@link IBuildpathContainerPage#getSelection() } to get the newly selected
+ * containers. {@link IBuildpathContainerPage#getSelection() } is still used for
+ * edited elements.
  */
 public interface IBuildpathContainerPageExtension2 {
-	
+
 	/**
-	 * Method {@link #getNewContainers()} is called instead of {@link IBuildpathContainerPage#getSelection() }
-	 * to get the newly added containers. {@link IBuildpathContainerPage#getSelection() } is still used
+	 * Method {@link #getNewContainers()} is called instead of
+	 * {@link IBuildpathContainerPage#getSelection() } to get the newly added
+	 * containers. {@link IBuildpathContainerPage#getSelection() } is still used
 	 * to get the edited elements.
-	 * @return the buildpath entries created on the page. All returned entries must be on kind
-	 * {@link IBuildpathEntry#CPE_CONTAINER}
+	 * 
+	 * @return the buildpath entries created on the page. All returned entries
+	 *         must be on kind {@link IBuildpathEntry#CPE_CONTAINER}
 	 */
 	public IBuildpathEntry[] getNewContainers();
 
