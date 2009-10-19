50c7ffaf2c2061322adf2ebd847b00e2f8cd27fd
apanchenk <apanchenk>
2009-10-19 11:14:47 +0000

    static for inner class + class header

--- core/plugins/org.eclipse.dltk.core/model/org/eclipse/dltk/core/ProgressMonitoringJob.java
+++ core/plugins/org.eclipse.dltk.core/model/org/eclipse/dltk/core/ProgressMonitoringJob.java
@@ -1,6 +1,14 @@
-/**
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2009 xored software, Inc.  
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html  
+ *
+ * Contributors:
+ *     xored software, Inc. - initial API and Implementation (Andrei Sobolev)
+ *******************************************************************************/
 package org.eclipse.dltk.core;
 
 import java.util.LinkedList;
@@ -16,10 +24,9 @@ import org.eclipse.core.runtime.jobs.Job;
  * 
  * @author asobolev
  * @since 2.0
- * 
  */
 public class ProgressMonitoringJob extends Job implements IProgressMonitor {
-	private class WorkNode {
+	private static class WorkNode {
 		int type;
 		String message;
 		double worked;
