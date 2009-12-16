85ccb9eefd504b02c08153d98d70b4d3b3812168
apanchenk <apanchenk>
2009-12-16 07:08:25 +0000

    support selecting filters (e.g. "My Home") as a folder

--- rse/plugins/org.eclipse.dltk.rse.ui/src/org/eclipse/dltk/internal/ui/rse/RSEEnvironmentUI.java
+++ rse/plugins/org.eclipse.dltk.rse.ui/src/org/eclipse/dltk/internal/ui/rse/RSEEnvironmentUI.java
@@ -5,13 +5,14 @@ import org.eclipse.dltk.core.internal.rse.DLTKRSEPlugin;
 import org.eclipse.dltk.core.internal.rse.RSEEnvironment;
 import org.eclipse.dltk.ui.environment.IEnvironmentUI;
 import org.eclipse.jface.window.Window;
+import org.eclipse.rse.core.filters.ISystemFilter;
+import org.eclipse.rse.core.filters.ISystemFilterReference;
 import org.eclipse.rse.files.ui.dialogs.SystemRemoteFileDialog;
 import org.eclipse.rse.files.ui.dialogs.SystemRemoteFolderDialog;
 import org.eclipse.rse.services.clientserver.messages.SystemMessageException;
 import org.eclipse.rse.subsystems.files.core.model.RemoteFileUtility;
 import org.eclipse.rse.subsystems.files.core.subsystems.IRemoteFile;
 import org.eclipse.rse.subsystems.files.core.subsystems.IRemoteFileSubSystem;
-import org.eclipse.rse.subsystems.files.core.subsystems.RemoteFile;
 import org.eclipse.swt.widgets.Shell;
 
 public class RSEEnvironmentUI implements IEnvironmentUI {
@@ -31,7 +32,7 @@ public class RSEEnvironmentUI implements IEnvironmentUI {
 		}
 		SystemRemoteFolderDialog dialog = new SystemRemoteFolderDialog(shell);
 		dialog.setDefaultSystemConnection(this.environment.getHost(), true);
-		if (initialFolder != null) {
+		if (initialFolder != null && initialFolder.length() != 0) {
 			final IRemoteFileSubSystem fs = RemoteFileUtility
 					.getFileSubSystem(environment.getHost());
 			if (fs != null) {
@@ -48,13 +49,41 @@ public class RSEEnvironmentUI implements IEnvironmentUI {
 		}
 		if (dialog.open() == Window.OK) {
 			Object selectedObject = dialog.getSelectedObject();
-			if (selectedObject instanceof RemoteFile) {
-				RemoteFile file = (RemoteFile) selectedObject;
+			if (selectedObject instanceof IRemoteFile) {
+				IRemoteFile file = (IRemoteFile) selectedObject;
 				return file.getAbsolutePath();
+			} else if (selectedObject instanceof ISystemFilterReference) {
+				final ISystemFilter filter = ((ISystemFilterReference) selectedObject)
+						.getReferencedFilter();
+				if (filter != null && filter.getFilterStringCount() == 1) {
+					final String filterString = filter.getFilterStrings()[0];
+					if (filterString.endsWith(FILTER_STD_SUFFIX)) {
+						final IRemoteFileSubSystem fs = RemoteFileUtility
+								.getFileSubSystem(environment.getHost());
+						if (fs != null) {
+							try {
+								final IRemoteFile remoteFile = fs
+										.getRemoteFileObject(filterString
+												.substring(0, filterString
+														.length()
+														- FILTER_STD_SUFFIX
+																.length()),
+												new NullProgressMonitor());
+								if (remoteFile != null) {
+									return remoteFile.getAbsolutePath();
+								}
+							} catch (SystemMessageException e) {
+								DLTKRSEPlugin.log(e);
+							}
+						}
+					}
+				}
 			}
 		}
 		return null;
 	}
+
+	private static final String FILTER_STD_SUFFIX = "/*"; //$NON-NLS-1$
 
 	public String selectFile(Shell shell, int fileType) {
 		return selectFile(shell, fileType, null);
@@ -86,8 +115,8 @@ public class RSEEnvironmentUI implements IEnvironmentUI {
 		}
 		if (dialog.open() == Window.OK) {
 			Object selectedObject = dialog.getSelectedObject();
-			if (selectedObject instanceof RemoteFile) {
-				RemoteFile file = (RemoteFile) selectedObject;
+			if (selectedObject instanceof IRemoteFile) {
+				IRemoteFile file = (IRemoteFile) selectedObject;
 				return file.getAbsolutePath();
 			}
 		}
