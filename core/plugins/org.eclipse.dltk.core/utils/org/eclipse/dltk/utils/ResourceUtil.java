772ef48e49b0dfa80ef67160c8797e2a471c5756
apanchenk <apanchenk>
2010-02-22 07:56:16 +0000

    extract ResourceUtil.addNature()

---
+++ core/plugins/org.eclipse.dltk.core/utils/org/eclipse/dltk/utils/ResourceUtil.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2010 xored software, Inc.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     xored software, Inc. - initial API and Implementation (Alex Panchenko)
+ *******************************************************************************/
+package org.eclipse.dltk.utils;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.OperationCanceledException;
+
+public class ResourceUtil {
+
+	public static void addNature(IProject project, IProgressMonitor monitor,
+			String nature) throws CoreException {
+		if (monitor != null && monitor.isCanceled()) {
+			throw new OperationCanceledException();
+		}
+		if (!project.hasNature(nature)) {
+			IProjectDescription description = project.getDescription();
+			String[] prevNatures = description.getNatureIds();
+			String[] newNatures = new String[prevNatures.length + 1];
+			System.arraycopy(prevNatures, 0, newNatures, 0, prevNatures.length);
+			newNatures[prevNatures.length] = nature;
+			description.setNatureIds(newNatures);
+			project.setDescription(description, monitor);
+		} else {
+			if (monitor != null) {
+				monitor.worked(1);
+			}
+		}
+	}
+
+}
