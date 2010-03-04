d5d633bc02ce2091ff6cb46d157eead4d0745cef
apanchenk <apanchenk>
2010-03-04 09:37:47 +0000

    formatting

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/viewsupport/AppearanceAwareLabelProvider.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/viewsupport/AppearanceAwareLabelProvider.java
@@ -17,22 +17,21 @@ import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.jface.viewers.LabelProviderChangedEvent;
 
-
-public class AppearanceAwareLabelProvider extends ScriptUILabelProvider implements IPropertyChangeListener {
-	public final static long DEFAULT_TEXTFLAGS = 
-		ScriptElementLabels.ROOT_VARIABLE | 
-		ScriptElementLabels.M_PARAMETER_NAMES|				
-		ScriptElementLabels.M_PARAMETER_TYPES |
-		ScriptElementLabels.M_APP_TYPE_PARAMETERS | 
-		ScriptElementLabels.M_APP_RETURNTYPE |
-		ScriptElementLabels.M_PARAMETER_INITIALIZERS;
-		//|ScriptElementLabels.REFERENCED_ROOT_POST_QUALIFIED;
+public class AppearanceAwareLabelProvider extends ScriptUILabelProvider
+		implements IPropertyChangeListener {
+	public final static long DEFAULT_TEXTFLAGS = ScriptElementLabels.ROOT_VARIABLE
+			| ScriptElementLabels.M_PARAMETER_NAMES
+			| ScriptElementLabels.M_PARAMETER_TYPES
+			| ScriptElementLabels.M_APP_TYPE_PARAMETERS
+			| ScriptElementLabels.M_APP_RETURNTYPE
+			| ScriptElementLabels.M_PARAMETER_INITIALIZERS;
+	// |ScriptElementLabels.REFERENCED_ROOT_POST_QUALIFIED;
 	public final static int DEFAULT_IMAGEFLAGS = ScriptElementImageProvider.OVERLAY_ICONS;
 
 	private long fTextFlagMask;
-	
+
 	private int fImageFlagMask;
-	
+
 	private final IPreferenceStore fStore;
 
 	/**
@@ -41,13 +40,15 @@ public class AppearanceAwareLabelProvider extends ScriptUILabelProvider implemen
 	 * 
 	 * @param preferenceStore
 	 */
-	public AppearanceAwareLabelProvider(long textFlags, int imageFlags, IPreferenceStore store) {
+	public AppearanceAwareLabelProvider(long textFlags, int imageFlags,
+			IPreferenceStore store) {
 		super(textFlags, imageFlags);
 		fStore = store;
 		if (store == null) {
-			throw new AssertionError(Messages.AppearanceAwareLabelProvider_storeMustNotBeNull);
+			throw new AssertionError(
+					Messages.AppearanceAwareLabelProvider_storeMustNotBeNull);
 		}
-		initMasks();		
+		initMasks();
 		fStore.addPropertyChangeListener(this);
 	}
 
@@ -60,13 +61,16 @@ public class AppearanceAwareLabelProvider extends ScriptUILabelProvider implemen
 
 	private void initMasks() {
 		fTextFlagMask = -1;
-		if (!fStore.getBoolean(PreferenceConstants.APPEARANCE_METHOD_RETURNTYPE)) {
+		if (!fStore
+				.getBoolean(PreferenceConstants.APPEARANCE_METHOD_RETURNTYPE)) {
 			fTextFlagMask ^= ScriptElementLabels.M_APP_RETURNTYPE;
 		}
-		if (!fStore.getBoolean(PreferenceConstants.APPEARANCE_METHOD_TYPEPARAMETERS)) {
+		if (!fStore
+				.getBoolean(PreferenceConstants.APPEARANCE_METHOD_TYPEPARAMETERS)) {
 			fTextFlagMask ^= ScriptElementLabels.M_APP_TYPE_PARAMETERS;
 		}
-		if (!fStore.getBoolean(PreferenceConstants.APPEARANCE_COMPRESS_PACKAGE_NAMES)) {
+		if (!fStore
+				.getBoolean(PreferenceConstants.APPEARANCE_COMPRESS_PACKAGE_NAMES)) {
 			fTextFlagMask ^= ScriptElementLabels.P_COMPRESSED;
 		}
 		fImageFlagMask = -1;
@@ -78,34 +82,32 @@ public class AppearanceAwareLabelProvider extends ScriptUILabelProvider implemen
 	public void propertyChange(PropertyChangeEvent event) {
 		String property = event.getProperty();
 		if (property.equals(PreferenceConstants.APPEARANCE_METHOD_RETURNTYPE)
-				|| property.equals(PreferenceConstants.APPEARANCE_METHOD_TYPEPARAMETERS)
-				|| property.equals(PreferenceConstants.APPEARANCE_PKG_NAME_PATTERN_FOR_PKG_VIEW)
-				|| property.equals(PreferenceConstants.APPEARANCE_COMPRESS_PACKAGE_NAMES)) {
+				|| property
+						.equals(PreferenceConstants.APPEARANCE_METHOD_TYPEPARAMETERS)
+				|| property
+						.equals(PreferenceConstants.APPEARANCE_PKG_NAME_PATTERN_FOR_PKG_VIEW)
+				|| property
+						.equals(PreferenceConstants.APPEARANCE_COMPRESS_PACKAGE_NAMES)) {
 			initMasks();
-			LabelProviderChangedEvent lpEvent = new LabelProviderChangedEvent(this, null); // refresh
+			LabelProviderChangedEvent lpEvent = new LabelProviderChangedEvent(
+					this, null); // refresh
 			// all
 			fireLabelProviderChanged(lpEvent);
 		}
 	}
 
-	/*
-	 * @see IBaseLabelProvider#dispose()
-	 */
+	@Override
 	public void dispose() {
 		fStore.removePropertyChangeListener(this);
 		super.dispose();
 	}
 
-	/*
-	 * @see ScriptUILabelProvider#evaluateImageFlags()
-	 */
+	@Override
 	protected int evaluateImageFlags(Object element) {
 		return getImageFlags() & fImageFlagMask;
 	}
 
-	/*
-	 * @see ScriptUILabelProvider#evaluateTextFlags()
-	 */
+	@Override
 	protected long evaluateTextFlags(Object element) {
 		return getTextFlags() & fTextFlagMask;
 	}
