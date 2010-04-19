d17982f65952fb20cc664d1ac231681c16c9ca22
apanchenk <apanchenk>
2010-04-19 09:06:32 +0000

    fix pattern for IMethod search

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/search/PatternStrings.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/search/PatternStrings.java
@@ -9,32 +9,70 @@
  *******************************************************************************/
 package org.eclipse.dltk.internal.ui.search;
 
+import org.eclipse.dltk.core.DLTKLanguageManager;
+import org.eclipse.dltk.core.IDLTKLanguageToolkit;
 import org.eclipse.dltk.core.IField;
 import org.eclipse.dltk.core.IMethod;
+import org.eclipse.dltk.core.ISearchPatternProcessor;
 import org.eclipse.dltk.core.IType;
+import org.eclipse.dltk.core.ModelException;
 import org.eclipse.dltk.ui.ScriptElementLabels;
 
 public class PatternStrings {
 	public static String getMethodSignature(IMethod method) {
-		StringBuffer buffer= new StringBuffer();
-		IType type = method.getDeclaringType();
-		if( type != null ) {
-			buffer.append(ScriptElementLabels.getDefault().getElementLabel( type, ScriptElementLabels.T_FULLY_QUALIFIED | ScriptElementLabels.USE_RESOLVED));		
-			boolean isConstructor= method.getElementName().equals(type.getElementName());
-			if (!isConstructor) {
-				buffer.append('.');
+		final StringBuilder buffer = new StringBuilder();
+		final IDLTKLanguageToolkit toolkit = DLTKLanguageManager
+				.getLanguageToolkit(method);
+		if (toolkit != null) {
+			final ISearchPatternProcessor patternProcessor = DLTKLanguageManager
+					.getSearchPatternProcessor(toolkit);
+			final IType type = method.getDeclaringType();
+			if (type != null) {
+				if (patternProcessor != null) {
+					buffer.append(type.getTypeQualifiedName(patternProcessor
+							.getDelimiterReplacementString()));
+				} else {
+					buffer.append(type.getTypeQualifiedName());
+				}
+				if (patternProcessor != null) {
+					buffer.append(patternProcessor
+							.getDelimiterReplacementString());
+				} else {
+					buffer.append('.');
+				}
+				// TODO for constructor call method name could be optional
 			}
-		}				
+		}
+		buffer.append(method.getElementName());
+		buffer.append("(");
+		if (method.exists()) {
+			try {
+				int count = 0;
+				for (String param : method.getParameterNames()) {
+					if (count != 0) {
+						buffer.append(",");
+					}
+					++count;
+					buffer.append(param);
+				}
+			} catch (ModelException e) {
+				//
+			}
+		}
+		buffer.append(")");
 		return buffer.toString();
 	}
 
-	
 	public static String getTypeSignature(IType field) {
-		return ScriptElementLabels.getDefault().getElementLabel(field, 
-			ScriptElementLabels.T_FULLY_QUALIFIED | ScriptElementLabels.T_TYPE_PARAMETERS | ScriptElementLabels.USE_RESOLVED);
-	}	
-	
+		return ScriptElementLabels.getDefault().getElementLabel(
+				field,
+				ScriptElementLabels.T_FULLY_QUALIFIED
+						| ScriptElementLabels.T_TYPE_PARAMETERS
+						| ScriptElementLabels.USE_RESOLVED);
+	}
+
 	public static String getFieldSignature(IField field) {
-		return ScriptElementLabels.getDefault().getElementLabel(field, ScriptElementLabels.F_FULLY_QUALIFIED);
-	}	
+		return ScriptElementLabels.getDefault().getElementLabel(field,
+				ScriptElementLabels.F_FULLY_QUALIFIED);
+	}
 }
