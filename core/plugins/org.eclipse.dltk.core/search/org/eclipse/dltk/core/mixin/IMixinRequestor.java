6ef4a03605ab47ac2fcd5acd2eaf97b0e4c47e1c
apanchenk <apanchenk>
2010-05-18 11:04:47 +0000

    formatting

--- core/plugins/org.eclipse.dltk.core/search/org/eclipse/dltk/core/mixin/IMixinRequestor.java
+++ core/plugins/org.eclipse.dltk.core/search/org/eclipse/dltk/core/mixin/IMixinRequestor.java
@@ -11,20 +11,24 @@ package org.eclipse.dltk.core.mixin;
 
 public interface IMixinRequestor {
 	public final static String MIXIN_NAME_SEPARATOR = MixinModel.SEPARATOR;
+
 	public static class ElementInfo {
 		/**
-		 * Could be seperated by MIXIN_NAME_SEPARATOR.
-		 * If it is separated, then it added by splitting. 
-		 * Then user ask for parent, it will contain this element.
+		 * Could be separated by MIXIN_NAME_SEPARATOR. If it is separated, then
+		 * it added by splitting. Then user ask for parent, it will contain this
+		 * element.
 		 */
 		public String key;
 		/**
 		 * All possible user object.
 		 */
 		public Object object;
+
+		@Override
 		public String toString() {
 			return key + " : " + object; //$NON-NLS-1$
 		}
 	}
-	void reportElement( ElementInfo info );
+
+	void reportElement(ElementInfo info);
 }
