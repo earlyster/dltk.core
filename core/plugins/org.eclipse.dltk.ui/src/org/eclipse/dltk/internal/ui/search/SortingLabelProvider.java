bdc59ab171c20bd27da04d1695943dd33eccddd1
rganor <rganor>
2010-05-19 16:52:38 +0000

    Bug 255050 - [regression] Relative file path is not shown in Search view in case of "Show as list"
    contributed by Zhao, thanks!

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/search/SortingLabelProvider.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/search/SortingLabelProvider.java
@@ -11,55 +11,104 @@ package org.eclipse.dltk.internal.ui.search;
 
 import org.eclipse.core.resources.IResource;
 import org.eclipse.dltk.core.IModelElement;
+import org.eclipse.dltk.core.ISourceModule;
+import org.eclipse.dltk.core.ModelException;
+import org.eclipse.dltk.internal.core.ExternalSourceModule;
+import org.eclipse.dltk.ui.DLTKUIPlugin;
 import org.eclipse.dltk.ui.ScriptElementLabels;
 import org.eclipse.jface.viewers.IColorProvider;
 import org.eclipse.swt.graphics.Image;
 
+public class SortingLabelProvider extends SearchLabelProvider implements
+		IColorProvider {
+	public static final int SHOW_ELEMENT_CONTAINER = 1; // default
+	public static final int SHOW_CONTAINER_ELEMENT = 2;
+	public static final int SHOW_PATH = 3;
 
-public class SortingLabelProvider extends SearchLabelProvider implements IColorProvider {
-	public static final int SHOW_ELEMENT_CONTAINER= 1; // default
-	public static final int SHOW_CONTAINER_ELEMENT= 2;
-	public static final int SHOW_PATH= 3;
-	
 	public SortingLabelProvider(DLTKSearchResultPage page) {
 		super(page);
-	}	
+	}
 
 	public Image getImage(Object element) {
-		Image image= null;
+		Image image = null;
 		if (element instanceof IModelElement || element instanceof IResource)
-			image= super.getImage(element);
+			image = super.getImage(element);
 		if (image != null)
 			return image;
 		return getParticipantImage(element);
 	}
-		
+
 	public final String getText(Object element) {
-		return getLabelWithCounts(element, internalGetText(element));
+		return getLabelWithCounts(element, appendRalitivePath(element,
+				internalGetText(element)));
+	}
+
+	private String appendRalitivePath(Object element, String internalGetText) {
+		StringBuffer result = new StringBuffer(internalGetText);
+		if (element instanceof IModelElement) {
+			IModelElement modelElement = (IModelElement) element;
+			try {
+				String path = null;
+				ISourceModule sm = (ISourceModule) modelElement
+						.getAncestor(IModelElement.SOURCE_MODULE);
+				if (sm instanceof ExternalSourceModule) {
+					ExternalSourceModule esm = (ExternalSourceModule) sm;
+					path = esm.getStorage().getFullPath().toString();
+				} else {
+					IResource resource = modelElement.getUnderlyingResource();
+					path = resource.getFullPath().toString();
+				}
+
+				if (path != null) {
+					result.append(" - ").append(path);
+				}
+			} catch (ModelException e) {
+				DLTKUIPlugin.log(e);
+			}
+		}
+		return result.toString();
 	}
 
 	private String internalGetText(Object o) {
-//		if (o instanceof IImportDeclaration)
-//			o= ((IImportDeclaration)o).getParent().getParent();
+		// if (o instanceof IImportDeclaration)
+		// o= ((IImportDeclaration)o).getParent().getParent();
 
-		String text= super.getText(o);
+		String text = super.getText(o);
 		if (text != null && (text.length() > 0))
 			return text;
-		return getParticipantText(o);	
+		return getParticipantText(o);
 	}
 
 	public void setOrder(int orderFlag) {
-		long flags= DEFAULT_SEARCH_TEXTFLAGS;
+		long flags = DEFAULT_SEARCH_TEXTFLAGS;
 		if (orderFlag == SHOW_ELEMENT_CONTAINER)
-			flags |= ScriptElementLabels.F_POST_QUALIFIED | ScriptElementLabels.M_POST_QUALIFIED | ScriptElementLabels.I_POST_QUALIFIED | ScriptElementLabels.M_PARAMETER_TYPES
-							| ScriptElementLabels.T_POST_QUALIFIED | ScriptElementLabels.D_POST_QUALIFIED | ScriptElementLabels.CF_POST_QUALIFIED  | ScriptElementLabels.CU_POST_QUALIFIED;
-			
+			flags |= ScriptElementLabels.F_POST_QUALIFIED
+					| ScriptElementLabels.M_POST_QUALIFIED
+					| ScriptElementLabels.I_POST_QUALIFIED
+					| ScriptElementLabels.M_PARAMETER_TYPES
+					| ScriptElementLabels.T_POST_QUALIFIED
+					| ScriptElementLabels.D_POST_QUALIFIED
+					| ScriptElementLabels.CF_POST_QUALIFIED
+					| ScriptElementLabels.CU_POST_QUALIFIED;
+
 		else if (orderFlag == SHOW_CONTAINER_ELEMENT)
-			flags |= ScriptElementLabels.F_FULLY_QUALIFIED | ScriptElementLabels.M_FULLY_QUALIFIED | ScriptElementLabels.I_FULLY_QUALIFIED | ScriptElementLabels.M_PARAMETER_TYPES
-				| ScriptElementLabels.T_FULLY_QUALIFIED | ScriptElementLabels.D_QUALIFIED | ScriptElementLabels.CF_QUALIFIED  | ScriptElementLabels.CU_QUALIFIED;
+			flags |= ScriptElementLabels.F_FULLY_QUALIFIED
+					| ScriptElementLabels.M_FULLY_QUALIFIED
+					| ScriptElementLabels.I_FULLY_QUALIFIED
+					| ScriptElementLabels.M_PARAMETER_TYPES
+					| ScriptElementLabels.T_FULLY_QUALIFIED
+					| ScriptElementLabels.D_QUALIFIED
+					| ScriptElementLabels.CF_QUALIFIED
+					| ScriptElementLabels.CU_QUALIFIED;
 		else if (orderFlag == SHOW_PATH) {
-			flags |= ScriptElementLabels.F_FULLY_QUALIFIED | ScriptElementLabels.M_FULLY_QUALIFIED | ScriptElementLabels.I_FULLY_QUALIFIED | ScriptElementLabels.M_PARAMETER_TYPES
-				| ScriptElementLabels.T_FULLY_QUALIFIED | ScriptElementLabels.D_QUALIFIED | ScriptElementLabels.CF_QUALIFIED  | ScriptElementLabels.CU_QUALIFIED;
+			flags |= ScriptElementLabels.F_FULLY_QUALIFIED
+					| ScriptElementLabels.M_FULLY_QUALIFIED
+					| ScriptElementLabels.I_FULLY_QUALIFIED
+					| ScriptElementLabels.M_PARAMETER_TYPES
+					| ScriptElementLabels.T_FULLY_QUALIFIED
+					| ScriptElementLabels.D_QUALIFIED
+					| ScriptElementLabels.CF_QUALIFIED
+					| ScriptElementLabels.CU_QUALIFIED;
 			flags |= ScriptElementLabels.PREPEND_ROOT_PATH;
 		}
 		setTextFlags(flags);
