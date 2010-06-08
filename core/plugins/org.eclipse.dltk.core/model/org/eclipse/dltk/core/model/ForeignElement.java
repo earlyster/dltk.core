8f71e9d7489a220398622e47b25600fada049752
apanchenk <apanchenk>
2010-06-08 09:20:09 +0000

    IForeignElement implementation

---
+++ core/plugins/org.eclipse.dltk.core/model/org/eclipse/dltk/core/model/ForeignElement.java
@@ -0,0 +1,87 @@
+package org.eclipse.dltk.core.model;
+
+import java.util.HashMap;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.dltk.core.IForeignElement;
+import org.eclipse.dltk.core.IModelElement;
+import org.eclipse.dltk.core.ModelException;
+import org.eclipse.dltk.core.WorkingCopyOwner;
+import org.eclipse.dltk.internal.core.ModelElement;
+import org.eclipse.dltk.internal.core.util.MementoTokenizer;
+
+public abstract class ForeignElement extends ModelElement implements
+		IForeignElement {
+
+	protected ForeignElement(IModelElement parent)
+			throws IllegalArgumentException {
+		super((ModelElement) parent);
+	}
+
+	public IResource getResource() {
+		return null;
+	}
+
+	public IPath getPath() {
+		IResource r = getResource();
+		if (r != null)
+			return r.getFullPath();
+		return Path.EMPTY;
+	}
+
+	public IResource getUnderlyingResource() throws ModelException {
+		return getResource();
+	}
+
+	public IResource getCorrespondingResource() throws ModelException {
+		return getResource();
+	}
+
+	public boolean isStructureKnown() throws ModelException {
+		return true;
+	}
+
+	@Override
+	public boolean exists() {
+		return true;
+	}
+
+	@Override
+	public void close() throws ModelException {
+	}
+
+	@Override
+	protected void closing(Object info) throws ModelException {
+	}
+
+	private static class ForeignInfo {
+	}
+
+	@Override
+	protected Object createElementInfo() {
+		return new ForeignInfo();
+	}
+
+	@Override
+	protected void generateInfos(Object info, HashMap newElements,
+			IProgressMonitor pm) throws ModelException {
+		newElements.put(this, info);
+	}
+
+	@Override
+	public IModelElement getHandleFromMemento(String token,
+			MementoTokenizer memento, WorkingCopyOwner owner) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected char getHandleMementoDelimiter() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
