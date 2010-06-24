b4e48527cb76013d1dc2ca8b7cfefc3541c275fe
apanchenk <apanchenk>
2010-06-24 11:12:44 +0000

    generics

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/filters/FilterDescriptor.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/filters/FilterDescriptor.java
@@ -83,7 +83,8 @@ public class FilterDescriptor implements Comparable, IPluginContribution {
 	 */
 	public static FilterDescriptor[] getFilterDescriptors(String targetId) {
 		FilterDescriptor[] filterDescs= FilterDescriptor.getFilterDescriptors();
-		List result= new ArrayList(filterDescs.length);
+		List<FilterDescriptor> result = new ArrayList<FilterDescriptor>(
+				filterDescs.length);
 		for (int i= 0; i < filterDescs.length; i++) {
 			String tid= filterDescs[i].getTargetId();
 			if (WorkbenchActivityHelper.filterItem(filterDescs[i]))
@@ -245,8 +246,8 @@ public class FilterDescriptor implements Comparable, IPluginContribution {
 	 * Creates the filter descriptors.
 	 */
 	private static FilterDescriptor[] createFilterDescriptors(IConfigurationElement[] elements) {
-		List result= new ArrayList(5);
-		Set descIds= new HashSet(5);
+		List<FilterDescriptor> result = new ArrayList<FilterDescriptor>(5);
+		Set<String> descIds = new HashSet<String>(5);
 		for (int i= 0; i < elements.length; i++) {
 			final IConfigurationElement element= elements[i];
 			if (FILTER_TAG.equals(element.getName())) {
