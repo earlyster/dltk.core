1f9e48664d080a086ffebfc09c353f5d3c1d1873
apanchenk <apanchenk>
2010-07-02 19:23:42 +0000

    simplify coloring preferences

---
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/coloring/ColoringPreferences.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2010 xored software, Inc.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     xored software, Inc. - initial API and Implementation (Alex Panchenko)
+ *******************************************************************************/
+package org.eclipse.dltk.ui.coloring;
+
+import org.eclipse.dltk.internal.ui.coloring.DefaultPreferenceRequestor;
+import org.eclipse.dltk.ui.DLTKUIPlugin;
+import org.eclipse.dltk.utils.NatureExtensionManager;
+import org.eclipse.jface.preference.IPreferenceStore;
+
+public class ColoringPreferences {
+
+	public static void initializeDefaults(IPreferenceStore store,
+			String natureId) {
+		final IColoringPreferenceProvider[] providers = (IColoringPreferenceProvider[]) new NatureExtensionManager(
+				DLTKUIPlugin.PLUGIN_ID + ".coloring",
+				IColoringPreferenceProvider.class).getInstances(natureId);
+		if (providers != null) {
+			final IColoringPreferenceRequestor requestor = new DefaultPreferenceRequestor(
+					store);
+			for (IColoringPreferenceProvider provider : providers) {
+				provider.providePreferences(requestor);
+			}
+		}
+	}
+}
