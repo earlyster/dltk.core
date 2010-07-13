90373d50a7b2b751d826d16be7288fda094f376e
apanchenk <apanchenk>
2010-07-13 13:26:19 +0000

    documentation

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/text/folding/IFoldingBlockKind.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/text/folding/IFoldingBlockKind.java
@@ -11,6 +11,27 @@
  *******************************************************************************/
 package org.eclipse.dltk.ui.text.folding;
 
+/**
+ * <p>
+ * The kind of the folding block - class, method, comment, etc. The instances
+ * are compared using <b>==</b>, so make sure you pass the same instance every
+ * time.
+ * </p>
+ * 
+ * <p>
+ * The recommended way to implement is java enumeration, for example:
+ * </p>
+ * 
+ * <pre>
+ * public enum FoldingBlockKind implements IFoldingBlockKind {
+ * 	CLASS, FUNCTION, COMMENT;
+ * 
+ * 	public boolean isComment() {
+ * 		return this == COMMENT;
+ * 	}
+ * }
+ * </pre>
+ */
 public interface IFoldingBlockKind {
 
 	boolean isComment();
