c8b82831c0a7a48085d803de47c587a9288441d4
apanchenk <apanchenk>
2010-11-02 15:54:53 +0000

    createMarkers() extracted and moved to ProblemCollector

--- core/plugins/org.eclipse.dltk.core/compiler/org/eclipse/dltk/compiler/problem/ProblemCollector.java
+++ core/plugins/org.eclipse.dltk.core/compiler/org/eclipse/dltk/compiler/problem/ProblemCollector.java
@@ -15,7 +15,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.dltk.compiler.task.ITaskReporter;
+import org.eclipse.dltk.core.IScriptModelMarker;
+import org.eclipse.dltk.internal.core.util.Util;
 
 public class ProblemCollector extends AbstractProblemReporter implements
 		ITaskReporter {
@@ -197,6 +202,53 @@ public class ProblemCollector extends AbstractProblemReporter implements
 			}
 		}
 		return result;
+	}
+
+	/**
+	 * @param resource
+	 * @param problemFactory
+	 * @throws CoreException
+	 * @since 3.0
+	 */
+	public void createMarkers(IResource resource, IProblemFactory problemFactory)
+			throws CoreException {
+		for (final IProblem problem : problems) {
+			final IMarker m = problemFactory.createMarker(resource, problem);
+			if (problem.getSourceLineNumber() >= 0) {
+				m.setAttribute(IMarker.LINE_NUMBER,
+						problem.getSourceLineNumber() + 1);
+			}
+			m.setAttribute(IMarker.MESSAGE, problem.getMessage());
+			if (problem.getSourceStart() >= 0) {
+				m.setAttribute(IMarker.CHAR_START, problem.getSourceStart());
+			}
+			if (problem.getSourceEnd() >= 0) {
+				m.setAttribute(IMarker.CHAR_END, problem.getSourceEnd());
+			}
+			if (!problem.isTask()) {
+				int severity = IMarker.SEVERITY_INFO;
+				if (problem.isError()) {
+					severity = IMarker.SEVERITY_ERROR;
+				} else if (problem.isWarning()) {
+					severity = IMarker.SEVERITY_WARNING;
+				}
+				m.setAttribute(IMarker.SEVERITY, severity);
+			} else {
+				m.setAttribute(IMarker.USER_EDITABLE, Boolean.FALSE);
+				if (problem instanceof TaskInfo) {
+					m.setAttribute(IMarker.PRIORITY,
+							((TaskInfo) problem).getPriority());
+				}
+			}
+			if (problem.getID() != 0) {
+				m.setAttribute(IScriptModelMarker.ID, problem.getID());
+			}
+			final String[] arguments = problem.getArguments();
+			if (arguments != null && arguments.length != 0) {
+				m.setAttribute(IScriptModelMarker.ARGUMENTS,
+						Util.getProblemArgumentsForMarker(arguments));
+			}
+		}
 	}
 
 }
--- core/plugins/org.eclipse.dltk.core/model/org/eclipse/dltk/internal/core/builder/BuildProblemReporter.java
+++ core/plugins/org.eclipse.dltk.core/model/org/eclipse/dltk/internal/core/builder/BuildProblemReporter.java
@@ -11,15 +11,11 @@
  *******************************************************************************/
 package org.eclipse.dltk.internal.core.builder;
 
-import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.dltk.compiler.problem.IProblem;
 import org.eclipse.dltk.compiler.problem.IProblemFactory;
 import org.eclipse.dltk.compiler.problem.ProblemCollector;
 import org.eclipse.dltk.core.DLTKCore;
-import org.eclipse.dltk.core.IScriptModelMarker;
-import org.eclipse.dltk.internal.core.util.Util;
 
 public class BuildProblemReporter extends ProblemCollector {
 
@@ -42,44 +38,7 @@ public class BuildProblemReporter extends ProblemCollector {
 				oldMarkersDeleted = true;
 				problemFactory.deleteMarkers(resource);
 			}
-			for (final IProblem problem : problems) {
-				final IMarker m = problemFactory
-						.createMarker(resource, problem);
-				if (problem.getSourceLineNumber() >= 0) {
-					m.setAttribute(IMarker.LINE_NUMBER,
-							problem.getSourceLineNumber() + 1);
-				}
-				m.setAttribute(IMarker.MESSAGE, problem.getMessage());
-				if (problem.getSourceStart() >= 0) {
-					m.setAttribute(IMarker.CHAR_START, problem.getSourceStart());
-				}
-				if (problem.getSourceEnd() >= 0) {
-					m.setAttribute(IMarker.CHAR_END, problem.getSourceEnd());
-				}
-				if (!problem.isTask()) {
-					int severity = IMarker.SEVERITY_INFO;
-					if (problem.isError()) {
-						severity = IMarker.SEVERITY_ERROR;
-					} else if (problem.isWarning()) {
-						severity = IMarker.SEVERITY_WARNING;
-					}
-					m.setAttribute(IMarker.SEVERITY, severity);
-				} else {
-					m.setAttribute(IMarker.USER_EDITABLE, Boolean.FALSE);
-					if (problem instanceof TaskInfo) {
-						m.setAttribute(IMarker.PRIORITY,
-								((TaskInfo) problem).getPriority());
-					}
-				}
-				if (problem.getID() != 0) {
-					m.setAttribute(IScriptModelMarker.ID, problem.getID());
-				}
-				final String[] arguments = problem.getArguments();
-				if (arguments != null && arguments.length != 0) {
-					m.setAttribute(IScriptModelMarker.ARGUMENTS,
-							Util.getProblemArgumentsForMarker(arguments));
-				}
-			}
+			createMarkers(resource, problemFactory);
 			problems.clear();
 		} catch (CoreException e) {
 			DLTKCore.error(Messages.BuildProblemReporter_errorUpdatingMarkers,
