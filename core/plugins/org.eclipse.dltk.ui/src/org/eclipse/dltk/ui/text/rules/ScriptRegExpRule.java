ff475bfb5e116aecca7375193cfedbc24081cf39
apanchenk <apanchenk>
2011-01-29 11:04:12 +0000

    generics

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/text/rules/ScriptRegExpRule.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/text/rules/ScriptRegExpRule.java
@@ -27,9 +27,9 @@ public abstract class ScriptRegExpRule extends PatternRule {
 	 * @since 3.1
 	 */
 	private static class DecreasingCharArrayLengthComparator implements
-			Comparator {
-		public int compare(Object o1, Object o2) {
-			return ((char[]) o2).length - ((char[]) o1).length;
+			Comparator<char[]> {
+		public int compare(char[] o1, char[] o2) {
+			return o2.length - o1.length;
 		}
 	}
 
@@ -39,7 +39,7 @@ public abstract class ScriptRegExpRule extends PatternRule {
 	 * 
 	 * @since 3.1
 	 */
-	private Comparator fLineDelimiterComparator = new DecreasingCharArrayLengthComparator();
+	private Comparator<char[]> fLineDelimiterComparator = new DecreasingCharArrayLengthComparator();
 
 	/**
 	 * Cached line delimiters.
