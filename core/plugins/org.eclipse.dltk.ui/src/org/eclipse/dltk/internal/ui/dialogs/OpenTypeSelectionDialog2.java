5329b3ad65e482eb0690e9a48e2835aca1a3489d
apanchenk <apanchenk>
2011-03-07 18:11:53 +0000

    formatting

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/dialogs/OpenTypeSelectionDialog2.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/dialogs/OpenTypeSelectionDialog2.java
@@ -7,7 +7,7 @@
  *
  
  *******************************************************************************/
- package org.eclipse.dltk.internal.ui.dialogs;
+package org.eclipse.dltk.internal.ui.dialogs;
 
 import org.eclipse.dltk.core.search.IDLTKSearchScope;
 import org.eclipse.dltk.ui.DLTKUIPlugin;
@@ -28,91 +28,96 @@ public class OpenTypeSelectionDialog2 extends TypeSelectionDialog2 {
 	private Point fLocation;
 	private Point fSize;
 
-	private static final String DIALOG_SETTINGS= "org.eclipse.jdt.internal.ui.dialogs.OpenTypeSelectionDialog2"; //$NON-NLS-1$
-	private static final String WIDTH= "width"; //$NON-NLS-1$
-	private static final String HEIGHT= "height"; //$NON-NLS-1$
-	
-	public OpenTypeSelectionDialog2(Shell parent, boolean multi, IRunnableContext context, IDLTKSearchScope scope, int elementKinds, IDLTKUILanguageToolkit toolkit ) {
-		this(parent, multi, context, scope, elementKinds, null, toolkit );
+	private static final String DIALOG_SETTINGS = "org.eclipse.jdt.internal.ui.dialogs.OpenTypeSelectionDialog2"; //$NON-NLS-1$
+	private static final String WIDTH = "width"; //$NON-NLS-1$
+	private static final String HEIGHT = "height"; //$NON-NLS-1$
+
+	public OpenTypeSelectionDialog2(Shell parent, boolean multi,
+			IRunnableContext context, IDLTKSearchScope scope, int elementKinds,
+			IDLTKUILanguageToolkit toolkit) {
+		this(parent, multi, context, scope, elementKinds, null, toolkit);
 	}
-	
-	public OpenTypeSelectionDialog2(Shell parent, boolean multi, IRunnableContext context, 
-			IDLTKSearchScope scope, int elementKinds, TypeSelectionExtension extension, IDLTKUILanguageToolkit toolkit) {
-		super(parent, multi, context, scope, elementKinds, extension, toolkit );
-		IDialogSettings settings= DLTKUIPlugin.getDefault().getDialogSettings();
-		fSettings= settings.getSection(DIALOG_SETTINGS);
+
+	public OpenTypeSelectionDialog2(Shell parent, boolean multi,
+			IRunnableContext context, IDLTKSearchScope scope, int elementKinds,
+			TypeSelectionExtension extension, IDLTKUILanguageToolkit toolkit) {
+		super(parent, multi, context, scope, elementKinds, extension, toolkit);
+		IDialogSettings settings = DLTKUIPlugin.getDefault()
+				.getDialogSettings();
+		fSettings = settings.getSection(DIALOG_SETTINGS);
 		if (fSettings == null) {
-			fSettings= new DialogSettings(DIALOG_SETTINGS);
+			fSettings = new DialogSettings(DIALOG_SETTINGS);
 			settings.addSection(fSettings);
 			fSettings.put(WIDTH, 480);
 			fSettings.put(HEIGHT, 320);
 		}
 	}
-	
+
 	protected void configureShell(Shell newShell) {
 		super.configureShell(newShell);
-//		PlatformUI.getWorkbench().getHelpSystem().setHelp(newShell, IJavaHelpContextIds.OPEN_TYPE_DIALOG);
+		// PlatformUI.getWorkbench().getHelpSystem().setHelp(newShell,
+		// IJavaHelpContextIds.OPEN_TYPE_DIALOG);
 	}
 
 	protected Point getInitialSize() {
-		Point result= super.getInitialSize();
+		Point result = super.getInitialSize();
 		if (fSize != null) {
-			result.x= Math.max(result.x, fSize.x);
-			result.y= Math.max(result.y, fSize.y);
-			Rectangle display= getShell().getDisplay().getClientArea();
-			result.x= Math.min(result.x, display.width);
-			result.y= Math.min(result.y, display.height);
+			result.x = Math.max(result.x, fSize.x);
+			result.y = Math.max(result.y, fSize.y);
+			Rectangle display = getShell().getDisplay().getClientArea();
+			result.x = Math.min(result.x, display.width);
+			result.y = Math.min(result.y, display.height);
 		}
 		return result;
 	}
-	
+
 	protected Point getInitialLocation(Point initialSize) {
-		Point result= super.getInitialLocation(initialSize);
+		Point result = super.getInitialLocation(initialSize);
 		if (fLocation != null) {
-			result.x= fLocation.x;
-			result.y= fLocation.y;
-			Rectangle display= getShell().getDisplay().getClientArea();
-			int xe= result.x + initialSize.x;
+			result.x = fLocation.x;
+			result.y = fLocation.y;
+			Rectangle display = getShell().getDisplay().getClientArea();
+			int xe = result.x + initialSize.x;
 			if (xe > display.width) {
-				result.x-= xe - display.width; 
+				result.x -= xe - display.width;
 			}
-			int ye= result.y + initialSize.y;
+			int ye = result.y + initialSize.y;
 			if (ye > display.height) {
-				result.y-= ye - display.height; 
+				result.y -= ye - display.height;
 			}
 		}
 		return result;
 	}
-	
+
 	protected Control createDialogArea(Composite parent) {
 		readSettings();
 		return super.createDialogArea(parent);
 	}
-	
+
 	public boolean close() {
 		writeSettings();
 		return super.close();
 	}
-	
+
 	/**
-	 * Initializes itself from the dialog settings with the same state
-	 * as at the previous invocation.
+	 * Initializes itself from the dialog settings with the same state as at the
+	 * previous invocation.
 	 */
 	private void readSettings() {
 		try {
-			int x= fSettings.getInt("x"); //$NON-NLS-1$
-			int y= fSettings.getInt("y"); //$NON-NLS-1$
-			fLocation= new Point(x, y);
+			int x = fSettings.getInt("x"); //$NON-NLS-1$
+			int y = fSettings.getInt("y"); //$NON-NLS-1$
+			fLocation = new Point(x, y);
 		} catch (NumberFormatException e) {
-			fLocation= null;
+			fLocation = null;
 		}
 		try {
-			int width= fSettings.getInt("width"); //$NON-NLS-1$
-			int height= fSettings.getInt("height"); //$NON-NLS-1$
-			fSize= new Point(width, height);
+			int width = fSettings.getInt("width"); //$NON-NLS-1$
+			int height = fSettings.getInt("height"); //$NON-NLS-1$
+			fSize = new Point(width, height);
 
 		} catch (NumberFormatException e) {
-			fSize= null;
+			fSize = null;
 		}
 	}
 
@@ -120,12 +125,12 @@ public class OpenTypeSelectionDialog2 extends TypeSelectionDialog2 {
 	 * Stores it current configuration in the dialog store.
 	 */
 	private void writeSettings() {
-		Point location= getShell().getLocation();
+		Point location = getShell().getLocation();
 		fSettings.put("x", location.x); //$NON-NLS-1$
 		fSettings.put("y", location.y); //$NON-NLS-1$
 
-		Point size= getShell().getSize();
+		Point size = getShell().getSize();
 		fSettings.put("width", size.x); //$NON-NLS-1$
 		fSettings.put("height", size.y); //$NON-NLS-1$
-	}	
+	}
 }
