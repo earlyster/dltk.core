6e505843458ab6fe008a56e1d60a1a660fc8aa90
apanchenk <apanchenk>
2011-03-21 13:59:46 +0000

    optional parameter to run project build

--- core/tests/org.eclipse.dltk.core.tests/src/org/eclipse/dltk/core/tests/BundledProjectSetup.java
+++ core/tests/org.eclipse.dltk.core.tests/src/org/eclipse/dltk/core/tests/BundledProjectSetup.java
@@ -14,6 +14,8 @@ package org.eclipse.dltk.core.tests;
 import junit.extensions.TestSetup;
 import junit.framework.Test;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.dltk.core.tests.model.AbstractModelTests;
 
 /**
@@ -40,11 +42,18 @@ public class BundledProjectSetup extends TestSetup {
 
 	private final Helper helper;
 	private final String projectName;
+	private final boolean build;
 
 	public BundledProjectSetup(String bundleName, String projectName, Test test) {
+		this(bundleName, projectName, test, false);
+	}
+
+	public BundledProjectSetup(String bundleName, String projectName,
+			Test test, boolean build) {
 		super(test);
 		this.helper = new Helper(bundleName);
 		this.projectName = projectName;
+		this.build = build;
 	}
 
 	@Override
@@ -52,12 +61,19 @@ public class BundledProjectSetup extends TestSetup {
 		super.setUp();
 		AbstractModelTests.disableAutoBulid();
 		helper.setUpProject(projectName);
+		if (build) {
+			getProject().build(IncrementalProjectBuilder.FULL_BUILD, null);
+		}
 	}
 
 	@Override
 	protected void tearDown() throws Exception {
 		helper.deleteProject(projectName);
 		super.tearDown();
+	}
+
+	protected IProject getProject() {
+		return AbstractModelTests.getProject(projectName);
 	}
 
 }
