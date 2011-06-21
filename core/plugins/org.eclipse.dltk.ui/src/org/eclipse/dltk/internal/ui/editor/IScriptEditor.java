05d226de1af1436399412f5bedd9ae3f8b59ebb1
apanchenk <apanchenk>
2011-06-21 17:17:49 +0000

    extends IScriptLanguageProvider

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/editor/IScriptEditor.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/editor/IScriptEditor.java
@@ -2,13 +2,14 @@ package org.eclipse.dltk.internal.ui.editor;
 
 import org.eclipse.dltk.core.IDLTKLanguageToolkit;
 import org.eclipse.dltk.core.IModelElement;
+import org.eclipse.dltk.core.IScriptLanguageProvider;
 import org.eclipse.dltk.core.ISourceReference;
 import org.eclipse.jface.action.IAction;
 
 /**
  * @since 2.0
  */
-public interface IScriptEditor {
+public interface IScriptEditor extends IScriptLanguageProvider {
 
 	public ISourceReference computeHighlightRangeSourceReference();
 
