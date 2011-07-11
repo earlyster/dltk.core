37fb9b2fa0a7d289e948f0a9406f4db72978394c
rganor <rganor>
2011-07-11 07:02:53 +0000

    Bug 315403 - Error marker does not show in parent source folder if a error is deep inside in a offspring file

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/ProblemsLabelDecorator.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/ui/ProblemsLabelDecorator.java
@@ -187,9 +187,9 @@ public class ProblemsLabelDecorator implements ILabelDecorator,
 				case IModelElement.SCRIPT_MODEL:
 				case IModelElement.SCRIPT_PROJECT:
 				case IModelElement.PROJECT_FRAGMENT:
+				case IModelElement.SCRIPT_FOLDER:
 					return getErrorTicksFromMarkers(element.getResource(),
 							IResource.DEPTH_INFINITE, null);
-				case IModelElement.SCRIPT_FOLDER:
 				case IModelElement.SOURCE_MODULE:
 					return getErrorTicksFromMarkers(element.getResource(),
 							IResource.DEPTH_ONE, null);
