ce8bdd42e0450853d56dfe1593a02cafbff6124e
apanchenk <apanchenk>
2011-08-11 05:52:21 +0000

    generics,formatting

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/typehierarchy/TypeHierarchyLifeCycle.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/typehierarchy/TypeHierarchyLifeCycle.java
@@ -16,92 +16,97 @@ import java.util.List;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.dltk.core.DLTKCore;
-import org.eclipse.dltk.core.ScriptModelUtil;
 import org.eclipse.dltk.core.ElementChangedEvent;
-import org.eclipse.dltk.core.IScriptProject;
 import org.eclipse.dltk.core.IElementChangedListener;
 import org.eclipse.dltk.core.IModelElement;
 import org.eclipse.dltk.core.IModelElementDelta;
 import org.eclipse.dltk.core.IProjectFragment;
 import org.eclipse.dltk.core.IRegion;
 import org.eclipse.dltk.core.IScriptFolder;
+import org.eclipse.dltk.core.IScriptProject;
 import org.eclipse.dltk.core.ISourceModule;
 import org.eclipse.dltk.core.IType;
 import org.eclipse.dltk.core.ITypeHierarchy;
 import org.eclipse.dltk.core.ITypeHierarchyChangedListener;
 import org.eclipse.dltk.core.ModelException;
+import org.eclipse.dltk.core.ScriptModelUtil;
 import org.eclipse.dltk.ui.DLTKUIPlugin;
 import org.eclipse.jface.operation.IRunnableContext;
 import org.eclipse.jface.operation.IRunnableWithProgress;
+
 /**
  * Manages a type hierarchy, to keep it refreshed, and to allow it to be shared.
  */
-public class TypeHierarchyLifeCycle implements ITypeHierarchyChangedListener, IElementChangedListener {
-	
+public class TypeHierarchyLifeCycle implements ITypeHierarchyChangedListener,
+		IElementChangedListener {
+
 	private boolean fHierarchyRefreshNeeded;
 	private ITypeHierarchy fHierarchy;
 	private IModelElement fInputElement;
 	private boolean fIsSuperTypesOnly;
-	
-	private List fChangeListeners;
-	
+
+	private List<ITypeHierarchyLifeCycleListener> fChangeListeners;
+
 	public TypeHierarchyLifeCycle() {
 		this(false);
-	}	
-	
-	public TypeHierarchyLifeCycle(boolean isSuperTypesOnly) {
-		fHierarchy= null;
-		fInputElement= null;
-		fIsSuperTypesOnly= isSuperTypesOnly;
-		fChangeListeners= new ArrayList(2);
 	}
-	
+
+	public TypeHierarchyLifeCycle(boolean isSuperTypesOnly) {
+		fHierarchy = null;
+		fInputElement = null;
+		fIsSuperTypesOnly = isSuperTypesOnly;
+		fChangeListeners = new ArrayList<ITypeHierarchyLifeCycleListener>(2);
+	}
+
 	public ITypeHierarchy getHierarchy() {
 		return fHierarchy;
 	}
-	
+
 	public IModelElement getInputElement() {
 		return fInputElement;
 	}
-	
-	
+
 	public void freeHierarchy() {
 		if (fHierarchy != null) {
 			fHierarchy.removeTypeHierarchyChangedListener(this);
 			DLTKCore.removeElementChangedListener(this);
-			fHierarchy= null;
-			fInputElement= null;
+			fHierarchy = null;
+			fInputElement = null;
 		}
 	}
-	
+
 	public void removeChangedListener(ITypeHierarchyLifeCycleListener listener) {
 		fChangeListeners.remove(listener);
 	}
-	
+
 	public void addChangedListener(ITypeHierarchyLifeCycleListener listener) {
 		if (!fChangeListeners.contains(listener)) {
 			fChangeListeners.add(listener);
 		}
 	}
-	
+
 	private void fireChange(IType[] changedTypes) {
-		for (int i= fChangeListeners.size()-1; i>=0; i--) {
-			ITypeHierarchyLifeCycleListener curr= (ITypeHierarchyLifeCycleListener) fChangeListeners.get(i);
+		for (int i = fChangeListeners.size() - 1; i >= 0; i--) {
+			ITypeHierarchyLifeCycleListener curr = fChangeListeners.get(i);
 			curr.typeHierarchyChanged(this, changedTypes);
 		}
 	}
-			
-	public void ensureRefreshedTypeHierarchy(final IModelElement element, IRunnableContext context) throws InvocationTargetException, InterruptedException {
+
+	public void ensureRefreshedTypeHierarchy(final IModelElement element,
+			IRunnableContext context) throws InvocationTargetException,
+			InterruptedException {
 		if (element == null || !element.exists()) {
 			freeHierarchy();
 			return;
 		}
-		boolean hierachyCreationNeeded= (fHierarchy == null || !element.equals(fInputElement));
-		
+		boolean hierachyCreationNeeded = (fHierarchy == null || !element
+				.equals(fInputElement));
+
 		if (hierachyCreationNeeded || fHierarchyRefreshNeeded) {
-			
-			IRunnableWithProgress op= new IRunnableWithProgress() {
-				public void run(IProgressMonitor pm) throws InvocationTargetException, InterruptedException {
+
+			IRunnableWithProgress op = new IRunnableWithProgress() {
+				public void run(IProgressMonitor pm)
+						throws InvocationTargetException, InterruptedException {
 					try {
 						doHierarchyRefresh(element, pm);
 					} catch (ModelException e) {
@@ -111,35 +116,38 @@ public class TypeHierarchyLifeCycle implements ITypeHierarchyChangedListener, IE
 					}
 				}
 			};
-			fHierarchyRefreshNeeded= true;
+			fHierarchyRefreshNeeded = true;
 			context.run(true, true, op);
-			fHierarchyRefreshNeeded= false;
+			fHierarchyRefreshNeeded = false;
 		}
 	}
-	
-	private ITypeHierarchy createTypeHierarchy(IModelElement element, IProgressMonitor pm) throws ModelException {
+
+	private ITypeHierarchy createTypeHierarchy(IModelElement element,
+			IProgressMonitor pm) throws ModelException {
 		if (element.getElementType() == IModelElement.TYPE) {
-			IType type= (IType) element;
+			IType type = (IType) element;
 			if (fIsSuperTypesOnly) {
 				return type.newSupertypeHierarchy(pm);
 			} else {
 				return type.newTypeHierarchy(pm);
 			}
 		} else {
-			IRegion region= DLTKCore.newRegion();
+			IRegion region = DLTKCore.newRegion();
 			if (element.getElementType() == IModelElement.SCRIPT_PROJECT) {
 				// for projects only add the contained source folders
-				IProjectFragment[] roots= ((IScriptProject) element).getProjectFragments();
-				for (int i= 0; i < roots.length; i++) {
+				IProjectFragment[] roots = ((IScriptProject) element)
+						.getProjectFragments();
+				for (int i = 0; i < roots.length; i++) {
 					if (!roots[i].isExternal()) {
 						region.add(roots[i]);
 					}
 				}
 			} else if (element.getElementType() == IModelElement.PROJECT_FRAGMENT) {
-				IProjectFragment[] roots= element.getScriptProject().getProjectFragments();
-				String name= element.getElementName();
-				for (int i= 0; i < roots.length; i++) {
-					IScriptFolder pack= roots[i].getScriptFolder(name);
+				IProjectFragment[] roots = element.getScriptProject()
+						.getProjectFragments();
+				String name = element.getElementName();
+				for (int i = 0; i < roots.length; i++) {
+					IScriptFolder pack = roots[i].getScriptFolder(name);
 					if (pack.exists()) {
 						region.add(pack);
 					}
@@ -147,41 +155,43 @@ public class TypeHierarchyLifeCycle implements ITypeHierarchyChangedListener, IE
 			} else {
 				region.add(element);
 			}
-			IScriptProject jproject= element.getScriptProject();
+			IScriptProject jproject = element.getScriptProject();
 			return jproject.newTypeHierarchy(region, pm);
 		}
 	}
-	
-	
-	public synchronized void doHierarchyRefresh(IModelElement element, IProgressMonitor pm) throws ModelException {
-		boolean hierachyCreationNeeded= (fHierarchy == null || !element.equals(fInputElement));
-		// to ensure the order of the two listeners always remove / add listeners on operations
+
+	public synchronized void doHierarchyRefresh(IModelElement element,
+			IProgressMonitor pm) throws ModelException {
+		boolean hierachyCreationNeeded = (fHierarchy == null || !element
+				.equals(fInputElement));
+		// to ensure the order of the two listeners always remove / add
+		// listeners on operations
 		// on type hierarchies
 		if (fHierarchy != null) {
 			fHierarchy.removeTypeHierarchyChangedListener(this);
 			DLTKCore.removeElementChangedListener(this);
 		}
 		if (hierachyCreationNeeded) {
-			fHierarchy= createTypeHierarchy(element, pm);
+			fHierarchy = createTypeHierarchy(element, pm);
 			if (pm != null && pm.isCanceled()) {
 				throw new OperationCanceledException();
 			}
-			fInputElement= element;
+			fInputElement = element;
 		} else {
 			fHierarchy.refresh(pm);
 		}
 		fHierarchy.addTypeHierarchyChangedListener(this);
 		DLTKCore.addElementChangedListener(this);
-		fHierarchyRefreshNeeded= false;
-	}		
-	
+		fHierarchyRefreshNeeded = false;
+	}
+
 	/*
 	 * @see ITypeHierarchyChangedListener#typeHierarchyChanged
 	 */
 	public void typeHierarchyChanged(ITypeHierarchy typeHierarchy) {
-	 	fHierarchyRefreshNeeded= true;
- 		fireChange(null);
-	}		
+		fHierarchyRefreshNeeded = true;
+		fireChange(null);
+	}
 
 	/*
 	 * @see IElementChangedListener#elementChanged(ElementChangedEvent)
@@ -190,73 +200,75 @@ public class TypeHierarchyLifeCycle implements ITypeHierarchyChangedListener, IE
 		if (fChangeListeners.isEmpty()) {
 			return;
 		}
-		
+
 		if (fHierarchyRefreshNeeded) {
 			return;
 		} else {
-			ArrayList changedTypes= new ArrayList();
+			ArrayList<IType> changedTypes = new ArrayList<IType>();
 			processDelta(event.getDelta(), changedTypes);
 			if (changedTypes.size() > 0) {
-				fireChange((IType[]) changedTypes.toArray(new IType[changedTypes.size()]));
+				fireChange(changedTypes.toArray(new IType[changedTypes.size()]));
 			}
 		}
 	}
-	
+
 	/*
 	 * Assume that the hierarchy is intact (no refresh needed)
-	 */					
-	private void processDelta(IModelElementDelta delta, ArrayList changedTypes) {
-		IModelElement element= delta.getElement();
+	 */
+	private void processDelta(IModelElementDelta delta,
+			ArrayList<IType> changedTypes) {
+		IModelElement element = delta.getElement();
 		switch (element.getElementType()) {
-			case IModelElement.TYPE:
-				processTypeDelta((IType) element, changedTypes);
-				processChildrenDelta(delta, changedTypes); // (inner types)
-				break;
-			case IModelElement.SCRIPT_MODEL:
-			case IModelElement.SCRIPT_PROJECT:
-			case IModelElement.PROJECT_FRAGMENT:
-				processChildrenDelta(delta, changedTypes);
-				break;
-			case IModelElement.SOURCE_MODULE:
-				ISourceModule cu= (ISourceModule)element;
-				if (!ScriptModelUtil.isPrimary(cu)) {
-					return;
-				}
-				
-				if (delta.getKind() == IModelElementDelta.CHANGED && isPossibleStructuralChange(delta.getFlags())) {
-					try {
-						if (cu.exists()) {
-							IType[] types= cu.getAllTypes();
-							for (int i= 0; i < types.length; i++) {
-								processTypeDelta(types[i], changedTypes);
-							}
+		case IModelElement.TYPE:
+			processTypeDelta((IType) element, changedTypes);
+			processChildrenDelta(delta, changedTypes); // (inner types)
+			break;
+		case IModelElement.SCRIPT_MODEL:
+		case IModelElement.SCRIPT_PROJECT:
+		case IModelElement.PROJECT_FRAGMENT:
+			processChildrenDelta(delta, changedTypes);
+			break;
+		case IModelElement.SOURCE_MODULE:
+			ISourceModule cu = (ISourceModule) element;
+			if (!ScriptModelUtil.isPrimary(cu)) {
+				return;
+			}
+
+			if (delta.getKind() == IModelElementDelta.CHANGED
+					&& isPossibleStructuralChange(delta.getFlags())) {
+				try {
+					if (cu.exists()) {
+						IType[] types = cu.getAllTypes();
+						for (int i = 0; i < types.length; i++) {
+							processTypeDelta(types[i], changedTypes);
 						}
-					} catch (ModelException e) {
-						DLTKUIPlugin.log(e);
 					}
-				} else {
-					processChildrenDelta(delta, changedTypes);
+				} catch (ModelException e) {
+					DLTKUIPlugin.log(e);
 				}
-				break;			
+			} else {
+				processChildrenDelta(delta, changedTypes);
+			}
+			break;
 		}
 	}
-	
+
 	private boolean isPossibleStructuralChange(int flags) {
 		return (flags & (IModelElementDelta.F_CONTENT | IModelElementDelta.F_FINE_GRAINED)) == IModelElementDelta.F_CONTENT;
 	}
-	
-	private void processTypeDelta(IType type, ArrayList changedTypes) {
+
+	private void processTypeDelta(IType type, ArrayList<IType> changedTypes) {
 		if (getHierarchy().contains(type)) {
 			changedTypes.add(type);
 		}
 	}
-	
-	private void processChildrenDelta(IModelElementDelta delta, ArrayList changedTypes) {
-		IModelElementDelta[] children= delta.getAffectedChildren();
-		for (int i= 0; i < children.length; i++) {
+
+	private void processChildrenDelta(IModelElementDelta delta,
+			ArrayList<IType> changedTypes) {
+		IModelElementDelta[] children = delta.getAffectedChildren();
+		for (int i = 0; i < children.length; i++) {
 			processDelta(children[i], changedTypes); // recursive
 		}
 	}
-	
 
 }
