a31e62b7b684334c705475a5f47cebf758c854a4
Alex Panchenko <alex.panchenko@gmail.com>
2012-10-02 00:20:37 +0700

    @Internal annotation

---
+++ core/plugins/org.eclipse.dltk.annotations/src/org/eclipse/dltk/annotations/Internal.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2012 NumberFour AG
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     NumberFour AG - initial API and Implementation (Alex Panchenko)
+ *******************************************************************************/
+package org.eclipse.dltk.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Marker annotation for members which visibility was increased to avoid having
+ * synthetic accessor methods generated.
+ */
+@Retention(RetentionPolicy.SOURCE)
+@Target({ ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR,
+		ElementType.FIELD })
+public @interface Internal {
+}
